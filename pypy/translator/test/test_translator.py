c0e9ae5e8cb548822fd1ed6221eeb28320f72a60
Anders Lehmann <serendipity-soft@get2net.dk>
2004-06-03 16:26:33 +0000

    Some more test (factorial,factorial2,sieve_of_erastothenes)

--- pypy/translator/test/test_translator.py
+++ pypy/translator/test/test_translator.py
@@ -19,11 +19,31 @@ class TranslatorTestCase(testit.IntTestCase):
         inheritance2 = t.compile()
         self.assertEquals(inheritance2(), ((-12, -12), (3, "world")))
 
+    def test_factorial2(self):
+        t = Translator(snippet.factorial2)
+        t.annotate([int])
+        factorial2 = t.compile()
+        self.assertEquals(factorial2(5), 120)
+
+    def test_factorial(self):
+        t = Translator(snippet.factorial)
+        t.annotate([int])
+        factorial = t.compile()
+        self.assertEquals(factorial(5), 120)
+
     def test_simple_method(self):
         t = Translator(snippet.simple_method)
         t.annotate([int]).simplify()
         simple_method = t.compile()
         self.assertEquals(simple_method(55), 55)
+
+    def test_sieve_of_eratosthenes(self):
+        t = Translator(snippet.sieve_of_eratosthenes)
+        t.simplify()
+        t.annotate([]).simplify()
+        #t.view()
+        sieve_of_eratosthenes = t.compile()
+        self.assertEquals(sieve_of_eratosthenes(), 1028)
 
 if __name__ == '__main__':
     testit.main()
