d9b9215fe19e38729ba339e3e7968d13cea04b4b
Laura Creighton <lac@openend.se>
2004-06-16 16:43:23 +0000

    Much smaller now.  But I am not done yet with shrinking this ...

--- pypy/tool/testconverter.py
+++ pypy/tool/testconverter.py
@@ -24,13 +24,26 @@ def process_block(s, old, new):
             left, right = body[:p], body[p+1:]
             arglist.append((left, right))
 
-        r = find_comma(arglist)
+        try:
+            r = find_comma(arglist)
+            left, right = find_comma(arglist)
 
-        if r is None:
-            print 'malformed block %s cannot be converted' % s
+            if right.rstrip()[-1] != ')':
+                # if the last printing char of the string is not ')',
+                # keep the parens for now.
+                return new + left + ') == (' + right
+            else:  # see if we can drop one set of parens
+                #stripped = right.rstrip()[0:-1]
+                l = new + ' ' + left[1:] + ' == ' + right.rstrip()[0:-1] + '\n'
+                try:
+                    compile(l, '', 'exec')
+                    return l
+                except SyntaxError: # too bad, needed them
+                    return new + left + ') == (' + right
+
+        except SyntaxError:
+            print 'malformed block %s cannot be converted' % s.rstrip()
             return s
-        else:
-            return new + r[0] + ') == (' + r[1]
         
 def find_comma(tuplelist):
     import parser
@@ -40,14 +53,13 @@ def find_comma(tuplelist):
     
     for l, r in tuplelist:
         try:
-            left = l + ')'
-            right = '(' + r
-            parser.expr(left)
-            parser.expr(right)
-            return l , r  # Great!  Both sides are expressions!
+            
+            parser.expr(l + ')')
+            parser.expr('(' + r)
+            return l , r    # Great!  Both sides are expressions!
         except SyntaxError: # It wasn't that comma
             pass
-    return None
+    raise SyntaxError       # We never found anything that worked.
     
 def pos_finder(s, char=','):
     # returns the list of string positions where the char 'char' was found
@@ -63,7 +75,7 @@ def blocksplitter(filename):
     blockstring = ''
     filestring = ''
     was_interesting = False
-    indentation = ''
+    indent = ''
     
     for line in fp:
 
@@ -74,23 +86,19 @@ def blocksplitter(filename):
 
         if interesting :
             # we have found the beginning of a new interesting block.
-            # finish up your business with your last block, and
-            # reset everything
+            # finish up your business with your last block, if 
+            # necessary and reset everything
 
             if was_interesting:
-                filestring += indentation + process_block(blockstring,
-                                                          old_fname, new_fname)
-            else:
-                filestring += line
-
+                filestring += indent + process_block(blockstring,
+                                                     old_fname, new_fname)
             blockstring = line # reset the block
-            indentation = ls.group(1)
+            indent = ls.group(1)
             was_interesting = True
 
         elif not was_interesting and not interesting :
             # the last line was not interesting and this one isn't either.
             # just copy it out.
-
             filestring  += line
 
         else:
@@ -101,18 +109,17 @@ def blocksplitter(filename):
             try:
                 compile(blockstring.lstrip(), '', 'exec')
                 # We were done.  This is a boring old follower
-
-                filestring += indentation + process_block(blockstring, old_fname, new_fname)
-                blockstring = line
+                
+                filestring += indent + process_block(blockstring, old_fname, new_fname)
+                filestring += line
+                #blockstring = ''
                 was_interesting = False
 
             except SyntaxError:  # we haven't got enough yet.
                 blockstring += line
 
     if was_interesting :
-        filestring += indentation + process_block(blockstring, old_fname, new_fname)
-    else:
-        filestring += line
+        filestring += indent + process_block(blockstring, old_fname, new_fname)
     
     print filestring
 
