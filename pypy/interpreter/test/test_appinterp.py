0a131971521f5e2524de43a80455c994ec93dcdf
Holger Krekel <holger.krekel@gmail.com>
2005-02-10 18:56:17 +0000

    added a another way to do applevel the new way:
        app = gateway.appdef("app(x,y)", """
            return x + y
        """)
        w_result = app(space, space.wrap(41), space.wrap(1))
    
    now the question is whether we want to support
    default args :-)

--- pypy/interpreter/test/test_appinterp.py
+++ pypy/interpreter/test/test_appinterp.py
@@ -1,5 +1,6 @@
 
 import py
+from pypy.interpreter.gateway import appdef 
 
 def test_execwith_novars(space): 
     val = space.appexec([], """ 
@@ -23,4 +24,10 @@ def test_execwith_compile_error(space):
     """)
     assert str(excinfo).find('y y') != -1 
 
-    
+def test_simple_applevel(space):
+    app = appdef("app(x,y)", """
+        return x + y
+    """)
+    assert app.func_name == 'app'
+    w_result = app(space, space.wrap(41), space.wrap(1))
+    assert space.eq_w(w_result, space.wrap(42))
