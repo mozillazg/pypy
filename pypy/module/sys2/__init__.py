429ebe18c627e86e6e4b6cfe972e27e132cce297
Holger Krekel <holger.krekel@gmail.com>
2005-02-17 09:49:00 +0000

    a careful port (preserving history as feasible) of the module/sys*.py module parts to the new module definition structure.
    Currently, when you startup "py.py" then you
    you will have 'builtin' and 'sys2' automatically
    in your namespace so that you can play a bit
    with the two converted modules.  accessing
    their dict completely un-lazifies them.

---
+++ pypy/module/sys2/__init__.py
@@ -0,0 +1,54 @@
+from pypy.interpreter.newmodule import ExtModule
+
+class Module(ExtModule):
+    """Sys Builtin Module. """
+    interpleveldefs = {
+        'pypy_objspaceclass'    : '(space.wrap(space.__class__.__name__))', 
+
+        '__name__'              : '(space.wrap("sys"))', 
+        '__doc__'               : '(space.wrap("PyPy sys module"))', 
+
+        'platform'              : 'space.wrap(sys.platform)', 
+        'maxint'                : 'space.wrap(sys.maxint)', 
+        'byteorder'             : 'space.wrap(sys.byteorder)', 
+        'exec_prefix'           : 'space.wrap(sys.exec_prefix)', 
+        'prefix'                : 'space.wrap(sys.prefix)', 
+        'maxunicode'            : 'space.wrap(sys.maxunicode)',
+        'maxint'                : 'space.wrap(sys.maxint)',
+        'stdin'                 : 'space.wrap(sys.stdin)',
+        'stdout'                : 'space.wrap(sys.stdout)',
+        'stderr'                : 'space.wrap(sys.stderr)', 
+        'pypy_objspaceclass'    : 'space.wrap(space.__class__.__name__)', 
+
+        'path'                  : 'state.get(space).w_path', 
+        'modules'               : 'state.get(space).w_modules', 
+        'argv'                  : 'state.get(space).w_argv', 
+        'warnoptions'           : 'state.get(space).w_warnoptions', 
+        'builtin_module_names'  : 'state.get(space).w_builtin_module_names', 
+        'pypy_getudir'          : 'state.pypy_getudir', 
+
+        'getdefaultencoding'    : 'space.wrap(sys.getdefaultencoding())', 
+        'getrefcount'           : 'vm.getrefcount', 
+        '_getframe'             : 'vm._getframe', 
+        'setrecursionlimit'     : 'vm.setrecursionlimit', 
+        'getrecursionlimit'     : 'vm.getrecursionlimit', 
+        'setcheckinterval'      : 'vm.setcheckinterval', 
+        'getcheckinterval'      : 'vm.getcheckinterval', 
+        'exc_info'              : 'vm.exc_info', 
+        'exc_clear'             : 'vm.exc_clear', 
+
+        'executable'            : 'space.wrap("py.py")', 
+        'copyright'             : 'space.wrap("MIT-License")', 
+        'version_info'          : 'space.wrap((2,3,4, "pypy1"))', 
+        'version'               : 'space.wrap("2.3.4 (pypy1 build)")', 
+        'hexversion'            : 'space.wrap(0x020304a0)', 
+        'ps1'                   : 'space.wrap(">>>>")', 
+        'ps2'                   : 'space.wrap("....")', 
+    }
+    appleveldefs = {
+        'displayhook'           : 'app.displayhook', 
+        '__displayhook__'       : 'app.__displayhook__', 
+        'excepthook'            : 'app.excepthook', 
+        '__excepthook__'        : 'app.__excepthook__', 
+        'exit'                  : 'app.exit', 
+    }
