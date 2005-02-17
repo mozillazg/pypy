eef5eda38113bd6bc55e1be7e58545aeaffe4fbb
Holger Krekel <holger.krekel@gmail.com>
2005-02-17 18:16:47 +0000

    move the displayhook to use one layer of applevel code less

--- pypy/module/sys2/hook.py
+++ pypy/module/sys2/hook.py
@@ -1,13 +1,11 @@
+from pypy.interpreter.pyopcode import print_item_to, sys_stdout 
 
 def displayhook(space, w_obj):
     if not space.is_w(w_obj, space.w_None): 
         space.setitem(space.builtin.w_dict, space.wrap('_'), w_obj)
         # NB. this is slightly more complicated in CPython,
         # see e.g. the difference with  >>> print 5,; 8
-        space.appexec([w_obj], """
-            (obj): 
-                print `obj` 
-        """)
+        print_item_to(space, w_obj, sys_stdout(space))
 
 __displayhook__ = displayhook  # this is exactly like in CPython
 
