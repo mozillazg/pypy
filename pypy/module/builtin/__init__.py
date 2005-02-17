c7102377791c6a4071e92f7edbb962fe700dbbe8
Samuele Pedroni <pedronis@lucediurna.net>
2005-02-17 22:33:28 +0000

    find a home for lookup builtin logic such that we don't have circular import problems.
    The other placements would require moving the imports after the W_Root etc class defs in baseobjspace

--- pypy/module/builtin/__init__.py
+++ pypy/module/builtin/__init__.py
@@ -1,3 +1,5 @@
+from pypy.interpreter.error import OperationError
+from pypy.interpreter import module
 from pypy.interpreter.newmodule import ExtModule
 
 class Module(ExtModule):
@@ -102,3 +104,24 @@ Noteworthy: None is the `nil' object; Ellipsis represents `...' in slices.
 
         '__import__'    : 'importing.importhook',
     }
+
+    def pick_builtin(self, w_globals):
+       "Look up the builtin module to use from the __builtins__ global"
+       space = self.space
+       try:
+           w_builtin = space.getitem(w_globals, space.wrap('__builtins__'))
+       except OperationError, e:
+           if not e.match(space, space.w_KeyError):
+               raise
+       else:
+           if w_builtin is space.builtin:   # common case
+               return space.builtin
+           if space.is_true(space.isinstance(w_builtin, space.w_dict)):
+                return module.Module(space, None, w_builtin)
+           builtin = space.interpclass_w(w_builtin)
+           if isinstance(builtin, module.Module):
+               return builtin   
+       # no builtin! make a default one.  Given them None, at least.
+       builtin = module.Module(space, None)
+       space.setitem(builtin.w_dict, space.wrap('None'), space.w_None)
+       return builtin
