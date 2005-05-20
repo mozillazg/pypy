9f0f1a2974f2a53077e21bd00c779ec5855f0326
Anders Chrigstrom <anders@chrigstrom.se>
2005-05-20 06:12:07 +0000

    Slightly improve the unicode string formating.

--- pypy/objspace/std/unicodeobject.py
+++ pypy/objspace/std/unicodeobject.py
@@ -766,8 +766,20 @@ def unicode_translate__Unicode_ANY(self, table):
                 raise TypeError("character mapping must return integer, None or unicode")
     return ''.join(result)
 
+def unicode_to_str(val):
+    if isinstance(val, unicode):
+        return val.encode("utf-8")
+    return val
+
 def mod__Unicode_ANY(format, values):
-    return unicode(format.encode("utf-8") % values, "utf-8")
+    format = format.encode("utf-8")
+    if isinstance(values, tuple):
+        values = tuple([unicode_to_str(val) for val in values])
+    elif hasattr(values, 'keys'):
+        values = dict([(key, unicode_to_str(val)) for key, val in values.iteritems()])
+    else:
+        values = unicode_to_str(values)
+    return unicode(format % values, "utf-8")
 
 ''')
 unicode_expandtabs__Unicode_ANY = app.interphook('unicode_expandtabs__Unicode_ANY')
