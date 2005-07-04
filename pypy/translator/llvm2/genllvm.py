5796cfb215e5999afb454b3d60b365226d5ba41a
Eric van Riet Paap <ericvrp@gmail.com>
2005-07-04 18:57:52 +0000

    Fixed import problem that occured when tested functions have identical names in different tests. In this case only the first test was used. Fixed some typos

--- pypy/translator/llvm2/genllvm.py
+++ pypy/translator/llvm2/genllvm.py
@@ -13,6 +13,8 @@ from pypy.tool.udir import udir
 from pypy.translator.llvm2.codewriter import CodeWriter
 from pypy.translator.backendoptimization import remove_void
 
+function_count = {}
+
 def genllvm(translator):
     remove_void(translator)
     func = translator.entrypoint
@@ -51,17 +53,24 @@ def genllvm(translator):
         typ_decl.writeimpl(codewriter)
 
     comment("End of file") ; nl()
+
+    if func.func_name in function_count:
+        postfix = '_%d' % function_count[func.func_name]
+        function_count[func.func_name] += 1
+    else:
+        postfix = ''
+        function_count[func.func_name] = 1
     
     targetdir = udir
-    llvmsource = targetdir.join(func.func_name).new(ext='.ll')
+    llvmsource = targetdir.join(func.func_name+postfix).new(ext='.ll')
     content = str(codewriter) 
     llvmsource.write(content) 
     log.source(content)
   
     if not llvm_is_on_path(): 
         py.test.skip("llvm not found")  # XXX not good to call py.test.skip here
-         
-    pyxsource = llvmsource.new(basename=llvmsource.purebasename+'_wrapper'+'.pyx')
+
+    pyxsource = llvmsource.new(basename=llvmsource.purebasename+'_wrapper'+postfix+'.pyx')
     write_pyx_wrapper(entrynode, pyxsource)    
     
     return build_llvm_module.make_module_from_llvm(llvmsource, pyxsource)
