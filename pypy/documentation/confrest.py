2c057e0f6ed15b70e7b7a928728140745b131b5f
Holger Krekel <holger.krekel@gmail.com>
2005-08-22 14:49:59 +0000

    more reshufflings towards a flatter doc hierarchy

--- pypy/documentation/confrest.py
+++ pypy/documentation/confrest.py
@@ -7,12 +7,12 @@ class PyPyPage(Page):
             html.a("news", href="news.html", class_="menu"), " ",
             html.a("doc", href="index.html", class_="menu"), " ",
             html.a("contact", href="contact.html", class_="menu"), " ", 
-            html.a("getting-started", href="getting_started.html", class_="menu"), " ",
+            html.a("getting-started", 
+                   href="getting_started.html", class_="menu"), " ",
             html.a("issue", 
                    href="https://codespeak.net/issue/pypy-dev/", 
                    class_="menu"), 
             " ", id="menubar")
-    
 
 class Project(Project): 
     title = "PyPy" 
@@ -21,8 +21,9 @@ class Project(Project):
     prefix_title = "PyPy"
     logo = html.div(
         html.a(
-            html.img(alt="PyPy", height="110", id="pyimg", 
-                     src="/pypy/img/py-web1.png", width="149")))
+            html.img(alt="PyPy", id="pyimg", 
+                     src="http://codespeak.net/pypy/img/py-web1.png", 
+                     height=110, width=149)))
     Page = PyPyPage 
 
 
