1ac36baa55c89e565cc50277260063a079cfa2ea
Anders Lehmann <serendipity-soft@get2net.dk>
2005-08-28 09:08:11 +0000

    Make it possible to run genreportdata without svn access

--- pypy/tool/pytest/genreportdata.py
+++ pypy/tool/pytest/genreportdata.py
@@ -1,5 +1,6 @@
 import autopath
 import py
+import sys
 mydir = py.magic.autopath().dirpath().realpath()
 from pypy.tool.pytest import htmlreport 
 from pypy.tool.pytest import confpath 
@@ -7,11 +8,14 @@ from pypy.tool.pytest import confpath
 if __name__ == '__main__': 
     testresultdir = confpath.testresultdir 
     assert testresultdir.check(dir=1)
-
-    resultwc = py.path.svnwc(testresultdir)
-
-    print "updating", resultwc
-    resultwc.update()
+    try:
+        resultwc = py.path.svnwc(testresultdir)
+        print "updating", resultwc
+        resultwc.update()
+    except KeyboardInterrupt, RuntimeError:
+        raise
+    except Exception,e: #py.process.ExecutionFailed,e:
+        print >> sys.stderr, "Warning: ",e #Subversion update failed"
 
     print "traversing", mydir 
     rep = htmlreport.HtmlReport()
