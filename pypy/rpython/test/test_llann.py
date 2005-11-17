e6f3e1a09d8d3d45b037384e8cf1e25106730774
Samuele Pedroni <pedronis@lucediurna.net>
2005-11-17 15:35:10 +0000

    (arigo, pedronis)
    fixed annlowlevel.py and its basic tests in test_llann.py
    
    now annotator.build_types delegate to the function desc to possibly apply a required specialisation

--- pypy/rpython/test/test_llann.py
+++ pypy/rpython/test/test_llann.py
@@ -29,7 +29,7 @@ class TestLowLevelAnnotateTestCase:
             s = malloc(S)
             return s.v
         a = self.RPythonAnnotator()
-        s, dontcare = annotate_lowlevel_helper(a, llf, [])
+        s = annotate_lowlevel_helper(a, llf, [])
         assert s.knowntype == int
 
     def test_simple2(self):
@@ -39,7 +39,7 @@ class TestLowLevelAnnotateTestCase:
             s = malloc(S2)
             return s.a.v+s.b.v
         a = self.RPythonAnnotator()
-        s, dontcare = annotate_lowlevel_helper(a, llf, [])
+        s = annotate_lowlevel_helper(a, llf, [])
         assert s.knowntype == int
 
     def test_array(self):
@@ -48,7 +48,7 @@ class TestLowLevelAnnotateTestCase:
             a = malloc(A, 1)
             return a[0].v
         a = self.RPythonAnnotator()
-        s, dontcare = annotate_lowlevel_helper(a, llf, [])
+        s = annotate_lowlevel_helper(a, llf, [])
         assert s.knowntype == int
 
     def test_prim_array(self):
@@ -57,7 +57,7 @@ class TestLowLevelAnnotateTestCase:
             a = malloc(A, 1)
             return a[0]
         a = self.RPythonAnnotator()
-        s, dontcare = annotate_lowlevel_helper(a, llf, [])
+        s = annotate_lowlevel_helper(a, llf, [])
         assert s.knowntype == int
 
     def test_prim_array_setitem(self):
@@ -67,7 +67,7 @@ class TestLowLevelAnnotateTestCase:
             a[0] = 3
             return a[0]
         a = self.RPythonAnnotator()
-        s, dontcare = annotate_lowlevel_helper(a, llf, [])
+        s = annotate_lowlevel_helper(a, llf, [])
         assert s.knowntype == int        
         
     def test_cast_simple_widening(self):
@@ -80,7 +80,7 @@ class TestLowLevelAnnotateTestCase:
             p3 = cast_pointer(PS1, p2)
             return p3
         a = self.RPythonAnnotator()
-        s, dontcare = annotate_lowlevel_helper(a, llf, [annmodel.SomePtr(PS1)])
+        s = annotate_lowlevel_helper(a, llf, [annmodel.SomePtr(PS1)])
         assert isinstance(s, annmodel.SomePtr)
         assert s.ll_ptrtype == PS1
 
@@ -94,7 +94,7 @@ class TestLowLevelAnnotateTestCase:
             p3 = cast_pointer(PS1, p2)
             return p3
         a = self.RPythonAnnotator()
-        s, dontcare = annotate_lowlevel_helper(a, llf, [])
+        s = annotate_lowlevel_helper(a, llf, [])
         assert isinstance(s, annmodel.SomePtr)
         assert s.ll_ptrtype == PS1
 
@@ -105,8 +105,6 @@ class TestLowLevelAnnotateTestCase:
         PS1 = Ptr(S1)
         PS2 = Ptr(S2)
         PS3 = Ptr(S3)
-        def llwitness(p12, p13, p21, p23, p31, p32):
-            pass
         def llf():
             p1 = malloc(S1)
             p2 = p1.sub
@@ -117,17 +115,10 @@ class TestLowLevelAnnotateTestCase:
             p23 = cast_pointer(PS2, p3)
             p31 = cast_pointer(PS3, p1)
             p32 = cast_pointer(PS3, p2)
-            llwitness(p12, p13, p21, p23, p31, p32)
+            return p12, p13, p21, p23, p31, p32
         a = self.RPythonAnnotator()
-        s, dontcare = annotate_lowlevel_helper(a, llf, [])
-        
-        spec_llwitness = None
-        for call in annotated_calls(a):
-            spec_llwitness = derived(call, 'llwitness')
-
-        g = a.translator.flowgraphs[spec_llwitness]
-        bindings = [a.binding(v) for v in g.getargs()]
-        assert [x.ll_ptrtype for x in bindings] == [PS1, PS1, PS2, PS2, PS3, PS3]
+        s = annotate_lowlevel_helper(a, llf, [])
+        assert [x.ll_ptrtype for x in s.items] == [PS1, PS1, PS2, PS2, PS3, PS3]
             
 
     def test_array_length(self):
@@ -136,7 +127,7 @@ class TestLowLevelAnnotateTestCase:
             a = malloc(A, 1)
             return len(a)
         a = self.RPythonAnnotator()
-        s, dontcare = annotate_lowlevel_helper(a, llf, [])
+        s = annotate_lowlevel_helper(a, llf, [])
         assert s.knowntype == int
 
     def test_funcptr(self):
@@ -145,7 +136,7 @@ class TestLowLevelAnnotateTestCase:
         def llf(p):
             return p(0)
         a = self.RPythonAnnotator()
-        s, dontcare = annotate_lowlevel_helper(a, llf, [annmodel.SomePtr(PF)])
+        s = annotate_lowlevel_helper(a, llf, [annmodel.SomePtr(PF)])
         assert s.knowntype == int
  
 
@@ -169,33 +160,32 @@ class TestLowLevelAnnotateTestCase:
             a2[0] = 2.0
             return ll_get(A, a2, 1)
         a = self.RPythonAnnotator()
-        s, llf2 = annotate_lowlevel_helper(a, llf, [])
-        assert llf2 is llf
+        s = annotate_lowlevel_helper(a, llf, [])
         assert s == annmodel.SomeFloat()
-        g = a.translator.getflowgraph(llf)
-        for_ = {}
+        vTs = []
+        ngraphs = len(a.translator.graphs)
         for call in annotated_calls(a):
             if derived(call, "ll_"):
-                    for_[tuple([x.value for x in call.args[0:2]])] = True
-        assert len(for_) == 4
-        vTs = []
-        for func, T in for_.keys():
-            g = a.translator.getflowgraph(func)
-            args = g.getargs()
-            rv = g.getreturnvar()
-            if len(args) == 2:
-                vT, vn = args
-                vTs.append(vT)
-                assert a.binding(vT) == annmodel.SomePBC({T: True})
-                assert a.binding(vn).knowntype == int
-                assert a.binding(rv).ll_ptrtype.TO == T
-            else:
-                vT, vp, vi = args
-                vTs.append(vT)
-                assert a.binding(vT) == annmodel.SomePBC({T: True})
-                assert a.binding(vi).knowntype == int
-                assert a.binding(vp).ll_ptrtype.TO == T
-                assert a.binding(rv) == annmodel.lltype_to_annotation(T.OF)
+                func = call.args[0].value
+                T = call.args[1].value
+                inputcells = [a.binding(v) for v in call.args[1:]]
+                g = a.bookkeeper.getdesc(func).specialize(inputcells)
+                args = g.getargs()
+                rv = g.getreturnvar()
+                if len(args) == 2:
+                    vT, vn = args
+                    vTs.append(vT)
+                    assert a.binding(vT) == a.bookkeeper.immutablevalue(T)
+                    assert a.binding(vn).knowntype == int
+                    assert a.binding(rv).ll_ptrtype.TO == T
+                else:
+                    vT, vp, vi = args
+                    vTs.append(vT)
+                    assert a.binding(vT) == a.bookkeeper.immutablevalue(T)
+                    assert a.binding(vi).knowntype == int
+                    assert a.binding(vp).ll_ptrtype.TO == T
+                    assert a.binding(rv) == annmodel.lltype_to_annotation(T.OF)
+        assert len(a.translator.graphs) == ngraphs
         return a, vTs
  
     def test_ll_calling_ll2(self):
@@ -220,45 +210,44 @@ class TestLowLevelAnnotateTestCase:
             a2[0] = 2.0
             return ll_get(a2, 1)
         a = self.RPythonAnnotator()
-        s, llf2 = annotate_lowlevel_helper(a, llf, [])
-        assert llf2 is llf
+        s = annotate_lowlevel_helper(a, llf, [])
         assert s == annmodel.SomeFloat()
-        g = a.translator.getflowgraph(llf)
-        for_ = {}
+
         def q(v):
             s = a.binding(v)
             if s.is_constant():
                 return s.const
             else:
                 return s.ll_ptrtype
-
+                
+        vTs = []
+        ngraphs = len(a.translator.graphs)
         for call in annotated_calls(a):
             if derived(call, "ll_") or derived(call, "makelen4"):
-                for_[tuple([q(x) for x in call.args[0:2]])] = True
-                
-        assert len(for_) == 5
-        vTs = []
-        for func, T in for_.keys():
-            g = a.translator.getflowgraph(func)
-            args = g.getargs()
-            rv = g.getreturnvar()
-            if isinstance(T, ContainerType):
-                if len(args) == 2:
-                    vT, vn = args
-                    vTs.append(vT)
-                    assert a.binding(vT) == annmodel.SomePBC({T: True})
-                    assert a.binding(vn).knowntype == int
-                    assert a.binding(rv).ll_ptrtype.TO == T
+                func = call.args[0].value
+                T = q(call.args[1])
+                inputcells = [a.binding(v) for v in call.args[1:]]
+                g = a.bookkeeper.getdesc(func).specialize(inputcells)
+                args = g.getargs()
+                rv = g.getreturnvar()
+                if isinstance(T, ContainerType):
+                    if len(args) == 2:
+                        vT, vn = args
+                        vTs.append(vT)
+                        assert a.binding(vT) == a.bookkeeper.immutablevalue(T)
+                        assert a.binding(vn).knowntype == int
+                        assert a.binding(rv).ll_ptrtype.TO == T
+                    else:
+                        vT, = args
+                        vTs.append(vT)
+                        assert a.binding(vT) == a.bookkeeper.immutablevalue(T)
+                        assert a.binding(rv).ll_ptrtype.TO == T
                 else:
-                    vT, = args
-                    vTs.append(vT)
-                    assert a.binding(vT) == annmodel.SomePBC({T: True})
-                    assert a.binding(rv).ll_ptrtype.TO == T
-            else:
-                vp, vi = args
-                assert a.binding(vi).knowntype == int
-                assert a.binding(vp).ll_ptrtype == T
-                assert a.binding(rv) == annmodel.lltype_to_annotation(T.TO.OF)
+                    vp, vi = args
+                    assert a.binding(vi).knowntype == int
+                    assert a.binding(vp).ll_ptrtype == T
+                    assert a.binding(rv) == annmodel.lltype_to_annotation(T.TO.OF)
+        assert len(a.translator.graphs) == ngraphs
         return a, vTs
 
     def test_getRuntimeTypeInfo(self):
@@ -267,7 +256,7 @@ class TestLowLevelAnnotateTestCase:
         def llf():
             return getRuntimeTypeInfo(S)
         a = self.RPythonAnnotator()
-        s, dontcare = annotate_lowlevel_helper(a, llf, [])
+        s = annotate_lowlevel_helper(a, llf, [])
         assert isinstance(s, annmodel.SomePtr)
         assert s.ll_ptrtype == Ptr(RuntimeTypeInfo)
         assert s.const == getRuntimeTypeInfo(S)
@@ -278,7 +267,7 @@ class TestLowLevelAnnotateTestCase:
         def llf(p):
             return runtime_type_info(p)
         a = self.RPythonAnnotator()
-        s, dontcare = annotate_lowlevel_helper(a, llf, [annmodel.SomePtr(Ptr(S))])
+        s = annotate_lowlevel_helper(a, llf, [annmodel.SomePtr(Ptr(S))])
         assert isinstance(s, annmodel.SomePtr)
         assert s.ll_ptrtype == Ptr(RuntimeTypeInfo)
         
