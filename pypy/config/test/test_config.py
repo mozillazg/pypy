fe26d2164e354bd30ac8ec5d5cb4bd19cd1a143e
Carl Friedrich Bolz <cfbolz@gmx.de>
2006-09-07 16:05:24 +0000

    make test actually check something

--- pypy/config/test/test_config.py
+++ pypy/config/test/test_config.py
@@ -208,9 +208,10 @@ def test_getpaths():
     descr = make_description()
     config = Config(descr)
     
-    assert config.getpaths(), ['gc.name', 'gc.dummy', 'gc.float', 'bool', 
-                                'objspace', 'wantref', 'int']
-    assert config.gc.getpaths(), ['name', 'dummy', 'float']
+    assert set(config.getpaths()) == set(
+        ['gc.name', 'gc.dummy', 'gc.float', 'bool',
+         'objspace', 'wantref', 'int'])
+    assert set(config.gc.getpaths()) == set(['name', 'dummy', 'float'])
 
 def test_none():
     dummy1 = BoolOption('dummy1', 'doc dummy', default=False, cmdline=None)
