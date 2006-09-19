2cd0ea38db2454b0ec00de7860bac4955c893d61
Michael Hudson <micahel@gmail.com>
2006-09-19 11:22:38 +0000

    attempt number #12214321 at climbing out of the rctypes hole. this one adds a local_raw_malloc operation and a step in the gctransformer to get rid of them again.  doesn't work, checking in to get onto a local machine and use --view.

--- pypy/rpython/lltypesystem/lloperation.py
+++ pypy/rpython/lltypesystem/lloperation.py
@@ -329,6 +329,7 @@ LL_OPERATIONS = {
     # __________ address operations __________
 
     'raw_malloc':           LLOp(canraise=(MemoryError,)),
+    'local_raw_malloc':     LLOp(canraise=(MemoryError,)),
     'raw_malloc_usage':     LLOp(sideeffects=False),
     'raw_free':             LLOp(),
     'raw_memclear':         LLOp(),
