5ebe1d17859eeeefc8d8957b44a36f62778b5644
Maciej Fijalkowski <fijall@gmail.com>
2006-11-04 16:58:08 +0000

    (pedronis, fijal) - Basics of traceback now works.

--- pypy/objspace/std/test/test_proxy_internals.py
+++ pypy/objspace/std/test/test_proxy_internals.py
@@ -27,7 +27,7 @@ class AppTestProxyInternals(object):
         assert tb.tb_frame is e[2].tb_frame
 
     def test_traceback_reraise(self):
-        py.test.skip("Not implemented yet")
+        #skip("Not implemented yet")
         try:
             1/0
         except:
@@ -37,3 +37,5 @@ class AppTestProxyInternals(object):
         tb = self.get_proxy(e[2])
         raises(ZeroDivisionError, "raise e[0], e[1], tb")
         raises(ZeroDivisionError, "raise e[0], self.get_proxy(e[1]), tb")
+        import traceback
+        assert len(traceback.format_tb(tb)) == 1
