f59958cfc6dbeec628719655315909bac85a6266
Maciej Fijalkowski <fijall@gmail.com>
2006-11-04 17:21:29 +0000

    (samuele, fijal) - Test + PyFrame & PyTraceback

--- pypy/objspace/std/test/test_proxy_internals.py
+++ pypy/objspace/std/test/test_proxy_internals.py
@@ -39,3 +39,58 @@ class AppTestProxyInternals(object):
         raises(ZeroDivisionError, "raise e[0], self.get_proxy(e[1]), tb")
         import traceback
         assert len(traceback.format_tb(tb)) == 1
+
+    def test_simple_frame(self):
+        import sys
+        frame = sys._getframe(0)
+        fp = self.get_proxy(frame)
+        assert fp.f_locals == frame.f_locals
+
+class AppTestProxyTracebackController(object):
+    def test_controller(self):
+        import types
+        import sys
+        import traceback
+        
+        def get_proxy(f):
+            return proxy(type(f), Controller(f).perform)
+        
+        class FakeTb(object):
+            def __init__(self, tb):
+                self.tb_lasti = tb.tb_lasti
+                self.tb_lineno = tb.tb_lineno
+                if tb.tb_next:
+                    self.tb_next = FakeTb(tb.tb_next)
+                else:
+                    self.tb_next = None
+                self.tb_frame = get_proxy(tb.tb_frame)
+        
+        class Controller(object):
+            def __init__(self, tb):
+                if isinstance(tb, types.TracebackType):
+                    self.obj = FakeTb(tb)
+                else:
+                    self.obj = tb
+            
+            def perform(self, name, *args, **kwargs):
+                return getattr(self.obj, name)(*args, **kwargs)
+        
+        def f():
+            1/0
+        
+        def g():
+            f()
+        
+        try:
+            g()
+        except:
+            e = sys.exc_info()
+        
+        last_tb = e[2]
+        tb = get_proxy(e[2])
+        try:
+            raise e[0], e[1], tb
+        except:
+            e = sys.exc_info()
+        
+        assert traceback.format_tb(last_tb) == traceback.format_tb(e[2])
