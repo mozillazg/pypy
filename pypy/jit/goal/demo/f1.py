929cd6b06328288a03293fd192d3dc9c9ad00d61
Armin Rigo <arigo@tunes.org>
2007-02-07 15:08:50 +0000

    Demo scripts whose machine code can be looked at (run with PYPYJITLOG=filename).

---
+++ pypy/jit/goal/demo/f1.py
@@ -0,0 +1,30 @@
+import time
+
+
+ZERO = 0
+
+def f1(n):
+    "Arbitrary test function."
+    i = 0
+    x = 1
+    while i<n:
+        j = ZERO
+        while j<=i:
+            j = j + 1
+            x = x + (i&j)
+        i = i + 1
+    return x
+
+
+import pypyjit
+pypyjit.enable(f1.func_code)
+
+res = f1(2117)
+print res
+N = 5
+start = time.time()
+for i in range(N):
+    assert f1(2117) == res
+end = time.time()
+
+print '%d iterations, time per iteration: %s' % (N, (end-start)/N)
