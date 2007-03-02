bf9a39bc31472af8d8c5c504a7d2508c13b22936
Carl Friedrich Bolz <cfbolz@gmx.de>
2007-03-02 16:58:59 +0000

    fix a bug, thanks armin for spotting

--- pypy/objspace/std/test/test_ropeobject.py
+++ pypy/objspace/std/test/test_ropeobject.py
@@ -8,6 +8,10 @@ class AppTestRopeObject(test_stringobject.AppTestStringObject):
     def setup_class(cls):
         cls.space = gettestobjspace(**{"objspace.std.withrope": True})
 
+    def test_mul_overflow(self):
+        import sys
+        raises(OverflowError, '"abcdefg" * (sys.maxint // 2)')
+
 class AppTestRopeUnicode(object):
 
     def setup_class(cls):
