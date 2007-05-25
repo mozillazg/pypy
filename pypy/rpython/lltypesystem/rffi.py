d54781ae959cec5a95d7a697287d258e03880b06
Maciej Fijalkowski <fijall@gmail.com>
2007-05-25 12:40:27 +0000

    Next test (involving char**, null terminated) passes. This is just enough to write down execv

--- pypy/rpython/lltypesystem/rffi.py
+++ pypy/rpython/lltypesystem/rffi.py
@@ -1,5 +1,6 @@
 
 from pypy.rpython.lltypesystem import lltype
+from pypy.rpython.lltypesystem.lloperation import llop
 
 def llexternal(name, args, result, sources=[], includes=[]):
     ext_type = lltype.FuncType(args, result)
@@ -15,5 +16,24 @@ def str2charp(s):
     array = lltype.malloc(CCHARP, len(s) + 1, flavor='raw')
     for i in range(len(s)):
         array[i] = s[i]
-    array[len(s)] = chr(0)
+    array[len(s)] = '\x00'
     return array
+
+CCHARPP = lltype.Array(lltype.Ptr(CCHARP), hints={'nolength': True})
+# list[str] -> char**, NULL terminated
+def liststr2charpp(l):
+    array = lltype.malloc(CCHARPP, len(l) + 1, flavor='raw')
+    for i in range(len(l)):
+        array[i] = str2charp(l[i])
+    array[len(l)] = lltype.nullptr(CCHARP)
+    return array
+
+# frees list of char**
+def free_charpp(ref):
+    next = ref
+    i = 0
+    while next[i]:
+        lltype.free(next[i], flavor='raw')
+        i += 1
+    lltype.free(ref, flavor='raw')
+
