bf83f5358a1ef07ddf16a1b944d6938b4a888571
Maciej Fijalkowski <fijall@gmail.com>
2007-05-25 12:52:12 +0000

    Move comments to docstrings

--- pypy/rpython/lltypesystem/rffi.py
+++ pypy/rpython/lltypesystem/rffi.py
@@ -8,28 +8,34 @@ def llexternal(name, args, result, sources=[], includes=[]):
                               sources=tuple(sources),
                               includes=tuple(includes))
 
+# char *
 CCHARP = lltype.Array(lltype.Char, hints={'nolength': True})
 
 # various type mapping
-# str -> char*
 def str2charp(s):
+    """ str -> char*
+    """
     array = lltype.malloc(CCHARP, len(s) + 1, flavor='raw')
     for i in range(len(s)):
         array[i] = s[i]
     array[len(s)] = '\x00'
     return array
 
+# char**
 CCHARPP = lltype.Array(lltype.Ptr(CCHARP), hints={'nolength': True})
-# list[str] -> char**, NULL terminated
+
 def liststr2charpp(l):
+    """ list[str] -> char**, NULL terminated
+    """
     array = lltype.malloc(CCHARPP, len(l) + 1, flavor='raw')
     for i in range(len(l)):
         array[i] = str2charp(l[i])
     array[len(l)] = lltype.nullptr(CCHARP)
     return array
 
-# frees list of char**
 def free_charpp(ref):
+    """ frees list of char**, NULL terminated
+    """
     next = ref
     i = 0
     while next[i]:
