d4cd46c4cc974538e7376f959ece66a64d3f707a
Maciej Fijalkowski <fijall@gmail.com>
2007-05-27 22:20:22 +0000

    Add caching mechanism

--- pypy/rpython/lltypesystem/test/test_rfficache.py
+++ pypy/rpython/lltypesystem/test/test_rfficache.py
@@ -1,6 +1,21 @@
 
-from pypy.rpython.lltypesystem.rfficache import sizeof_c_type
+from pypy.rpython.lltypesystem.rfficache import *
+from pypy.tool.udir import udir
 
 def test_sizeof_c_type():
     sizeofchar = sizeof_c_type('char')
     assert sizeofchar == 8
+
+def test_gettypesizes():
+    tmpfile = udir.join("somecrappyfile.py")
+    assert get_type_sizes(tmpfile)['char'] == 8
+    # this should not invoke a compiler
+    assert get_type_sizes(tmpfile, compiler_exe='xxx')['char'] == 8
+
+def test_gettypesizes_platforms():
+    tmpfile = udir.join("plat.py")
+    tmpfile.write(py.code.Source("""
+    platforms = {'xxx':{'char':4}}
+    """))
+    assert get_type_sizes(tmpfile)['char'] == 8
+    assert get_type_sizes(tmpfile, platform_key='xxx', compiler_exe='xxx')['char'] == 4
