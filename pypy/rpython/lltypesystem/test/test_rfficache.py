619fce67ddd7b293b144ca06f430b39b43477b83
Maciej Fijalkowski <fijall@gmail.com>
2007-05-27 22:58:55 +0000

    Add those type to be available

--- pypy/rpython/lltypesystem/test/test_rfficache.py
+++ pypy/rpython/lltypesystem/test/test_rfficache.py
@@ -1,5 +1,6 @@
 
 from pypy.rpython.lltypesystem.rfficache import *
+from pypy.rpython.lltypesystem import rffi
 from pypy.tool.udir import udir
 
 def test_sizeof_c_type():
@@ -19,3 +20,12 @@ def test_gettypesizes_platforms():
     """))
     assert get_type_sizes(tmpfile)['char'] == 8
     assert get_type_sizes(tmpfile, platform_key='xxx', compiler_exe='xxx')['char'] == 4
+
+def test_types_present():
+    for name in TYPES:
+        if name.startswith('unsigned'):
+            name = 'u' + name[9:]
+        name = name.replace(' ', '')
+        assert hasattr(rffi, 'r_' + name)
+        assert hasattr(rffi, name.upper())
+
