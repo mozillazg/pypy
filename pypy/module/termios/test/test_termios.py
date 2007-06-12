57ed491ceefaf171abb54e96b8768cfb62fc93ab
Maciej Fijalkowski <fijall@gmail.com>
2007-06-12 21:23:47 +0000

    Fix the termios module

--- pypy/module/termios/test/test_termios.py
+++ pypy/module/termios/test/test_termios.py
@@ -84,6 +84,25 @@ class TestTermios(object):
         child = self.spawn(['--withmod-termios', '--withmod-fcntl', str(f)])
         child.expect('ok!')
 
+    def test_icanon(self):
+        source = py.code.Source("""
+        import termios
+        import fcntl
+        import termios
+        f = termios.tcgetattr(2)
+        f[3] |= termios.ICANON
+        termios.tcsetattr(2, termios.TCSANOW, f)
+        f = termios.tcgetattr(2)
+        assert len([i for i in f[-1] if isinstance(i, int)]) == 2
+        assert isinstance(f[-1][termios.VMIN], int)
+        assert isinstance(f[-1][termios.VTIME], int)
+        print 'ok!'
+        """)
+        f = udir.join("test_ioctl_termios.py")
+        f.write(source)
+        child = self.spawn(['--withmod-termios', '--withmod-fcntl', str(f)])
+        child.expect('ok!')
+
 class AppTestTermios(object):
     def setup_class(cls):
         cls.space = gettestobjspace(usemodules=['termios'])
