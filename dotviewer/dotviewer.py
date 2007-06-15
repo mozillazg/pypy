f36dfc608bf46de3b0e2591fc6c2e919de462e1a
Armin Rigo <arigo@tunes.org>
2007-06-15 08:18:21 +0000

    Support for specifying an interface to listen on.  Useful to avoid opening an externally-visible tcp port.

--- dotviewer/dotviewer.py
+++ dotviewer/dotviewer.py
@@ -4,7 +4,7 @@ Command-line interface for a dot file viewer.
 
     dotviewer.py filename.dot
     dotviewer.py filename.plain
-    dotviewer.py --server port
+    dotviewer.py --server [interface:]port
 
 In the first form, show the graph contained in a .dot file.
 In the second form, the graph was already compiled to a .plain file.
@@ -17,22 +17,22 @@ import sys
 def main(args = sys.argv[1:]):
     import getopt
     options, args = getopt.getopt(args, 's:h', ['server=', 'help'])
-    server_port = None
+    server_addr = None
     for option, value in options:
         if option in ('-h', '--help'):
             print >> sys.stderr, __doc__
             sys.exit(2)
         if option in ('-s', '--server'):
-            server_port = int(value)
-    if not args and server_port is None:
+            server_addr = value
+    if not args and server_addr is None:
         print >> sys.stderr, __doc__
         sys.exit(2)
     for filename in args:
         import graphclient
         graphclient.display_dot_file(filename)
-    if server_port is not None:
+    if server_addr is not None:
         import graphserver
-        graphserver.listen_server(('', server_port))
+        graphserver.listen_server(server_addr)
 
 if __name__ == '__main__':
     main()
