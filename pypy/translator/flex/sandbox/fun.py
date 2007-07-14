15bc7ba957bdbee08424b9993b94ed499fb3c7c8
Alejandro J. Cura <alecu@codespeak.net>
2007-07-14 12:15:11 +0000

    merge lucio's changes

--- pypy/translator/flex/sandbox/fun.py
+++ pypy/translator/flex/sandbox/fun.py
@@ -1,17 +1,14 @@
 from pypy.translator.flex.modules.flex import *
 
-
+class Foo:
+    def __init__(self, msg):
+        self.msg = msg
+    
 def flash_main(a=1):
-	#abutton = Button()
-        #abutton.label = "I'm a button!"
-        #addChild(abutton)
-
-        for x in range(20):
-            abutton = Button()
-            abutton.label = "I'm button :" + str(x)
-            addChild(abutton)
-            
-	return a+1
-
-
+    f = Foo("hello")
+    for x in [1,2,3]:
+        abutton = Button()
+        abutton.label = "I'm button :" + str(x)
+        addChild(abutton)
+    return f.msg
 
