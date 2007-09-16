a1e3418282870dbf6f81d2a9495404968c11c05b
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2007-09-16 19:55:56 +0000

    try to simplify (by adding bugs) to get it to work (no dice); replace _hints hack at least, and give __new__s unwrap_specs

--- pypy/module/zlib/__init__.py
+++ pypy/module/zlib/__init__.py
@@ -3,12 +3,7 @@
 Mixed-module definition for the zlib module.
 """
 
-import zlib
-
 from pypy.interpreter.mixedmodule import MixedModule
-
-def constant(value):
-    return 'space.wrap(%s)' % (value,)
 
 
 class Module(MixedModule):
@@ -18,16 +13,6 @@ class Module(MixedModule):
         'Compress': 'interp_zlib.Compress',
         'Decompress': 'interp_zlib.Decompress',
         }
-
-#     # Constants exposed by zlib.h
-#     interpleveldefs.update((
-#             (name, constant(getattr(zlib, name)))
-#             for name
-#             in ['DEFLATED', 'DEF_MEM_LEVEL', 'MAX_WBITS',
-#                 'Z_BEST_COMPRESSION', 'Z_BEST_SPEED',
-#                 'Z_DEFAULT_COMPRESSION', 'Z_DEFAULT_STRATEGY',
-#                 'Z_FILTERED', 'Z_FINISH', 'Z_FULL_FLUSH',
-#                 'Z_HUFFMAN_ONLY', 'Z_NO_FLUSH', 'Z_SYNC_FLUSH']))
 
     appleveldefs = {
         'error': 'app_zlib.error',
