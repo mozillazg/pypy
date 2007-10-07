d5ef8407660e18e4331dc89ddceb22c52a92b01b
Armin Rigo <arigo@tunes.org>
2007-10-07 10:17:15 +0000

    Experimental.  Arenas that allow objects to be put anywhere in them, in any order.  When run directly (or via the llinterp), they do best-effort checks against overflows and overlaps by using the raw_malloc_usage() estimate.  The advantage is that can be directly use address arithmetic to handle the arenas, as the existing GCs do.  A drawback is that the implementation looks a little bit like the memory simulator, but at least it tries to be reasonably integrated with the rest of lltype and llmemory.
    For now I'll make arena_malloc, arena_free and arena_reset new
    lloperations, instead of using raw_malloc with a symbolic arena size.  I
    can imagine situations where it's more reasonable in the backend to use
    a different implementation to allocate small objects and whole arenas
    (e.g. mmap).  Similarly, arena_reset can optionally fill the arena with
    zero, which for very large amounts of memory can be implemented in more
    efficient ways than just memset().

---
+++ pypy/rpython/lltypesystem/test/test_llarena.py
@@ -0,0 +1,47 @@
+import py
+from pypy.rpython.lltypesystem import lltype, llmemory
+from pypy.rpython.lltypesystem.llmemory import cast_adr_to_ptr
+from pypy.rpython.lltypesystem.llarena import arena_malloc, arena_reset
+from pypy.rpython.lltypesystem.llarena import ArenaError
+
+def test_arena():
+    S = lltype.Struct('S', ('x',lltype.Signed))
+    SPTR = lltype.Ptr(S)
+    ssize = llmemory.raw_malloc_usage(llmemory.sizeof(S))
+    a = arena_malloc(2*ssize+1, False)
+
+    s1_ptr1 = cast_adr_to_ptr(a, SPTR)
+    s1_ptr1.x = 1
+    s1_ptr2 = cast_adr_to_ptr(a, SPTR)
+    assert s1_ptr2.x == 1
+    assert s1_ptr1 == s1_ptr2
+
+    s2_ptr1 = cast_adr_to_ptr(a + ssize + 1, SPTR)
+    py.test.raises(lltype.UninitializedMemoryAccess, 's2_ptr1.x')
+    s2_ptr1.x = 2
+    s2_ptr2 = cast_adr_to_ptr(a + ssize + 1, SPTR)
+    assert s2_ptr2.x == 2
+    assert s2_ptr1 == s2_ptr2
+    assert s1_ptr1 != s2_ptr1
+    assert not (s2_ptr2 == s1_ptr2)
+    assert s1_ptr1 == cast_adr_to_ptr(a, SPTR)
+
+    S2 = lltype.Struct('S2', ('y',lltype.Char))
+    S2PTR = lltype.Ptr(S2)
+    py.test.raises(TypeError, cast_adr_to_ptr, a, S2PTR)
+    py.test.raises(ArenaError, cast_adr_to_ptr, a+1, SPTR)
+    py.test.raises(ArenaError, cast_adr_to_ptr, a+ssize, SPTR)
+    py.test.raises(ArenaError, cast_adr_to_ptr, a+2*ssize, SPTR)
+    py.test.raises(ArenaError, cast_adr_to_ptr, a+2*ssize+1, SPTR)
+
+    arena_reset(a, True)
+    s1_ptr1 = cast_adr_to_ptr(a, SPTR)
+    assert s1_ptr1.x == 0
+    s1_ptr1.x = 5
+
+    s2_ptr1 = cast_adr_to_ptr(a + ssize, S2PTR)
+    assert s2_ptr1.y == '\x00'
+    s2_ptr1.y = 'X'
+
+    assert cast_adr_to_ptr(a + 0, SPTR).x == 5
+    assert cast_adr_to_ptr((a + ssize + 1) - 1, S2PTR).y == 'X'
