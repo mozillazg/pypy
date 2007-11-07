94a380db27b056a4d7ed8db4576391b2110222ca
Amit Regmi <regmee@codespeak.net>
2007-11-07 23:26:05 +0000

    few more lines to test System.Math

--- pypy/module/clr/test/test_importer.py
+++ pypy/module/clr/test/test_importer.py
@@ -10,5 +10,9 @@ class AppTestDotnet:
     def test_import_hook_simple(self):
         import clr
         import System.Math
+
+        print " abs(-5) = %s"%System.Math.Abs(-5)
+        print " 2^5     = %s"%System.Math.Pow(2,5)
+
         # import System.Math ...
 
