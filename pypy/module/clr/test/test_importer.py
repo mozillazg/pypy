a41b347f0917c85b78182c486fde4c9e29ec3d00
Antonio Cuni <anto.cuni@gmail.com>
2007-11-08 09:42:28 +0000

    - Abs(-5) is 5, not -5 :-)
    - Add an assert to check that Math is loaded only one time
    
    - Add a failing test for non existent modules

--- pypy/module/clr/test/test_importer.py
+++ pypy/module/clr/test/test_importer.py
@@ -9,8 +9,15 @@ class AppTestDotnet:
         import clr
         import System.Math
 
-        assert System.Math.Abs(-5) == -5
+        assert System.Math.Abs(-5) == 5
         assert System.Math.Pow(2, 5) == 2**5
 
-        # import System.Math ...
+        Math = clr.load_cli_class('System', 'Math')
+        assert Math is System.Math
 
+    def test_ImportError(self):
+        skip('Fixme!')
+        def fn():
+            import non_existent_module
+        raises(ImportError, fn())
+        
