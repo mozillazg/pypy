ee651b413e9244fd342f0607dd9ac900738542e9
Maciej Fijalkowski <fijall@gmail.com>
2007-11-10 11:43:38 +0000

    Another type :-/

--- pypy/objspace/std/unicodetype.py
+++ pypy/objspace/std/unicodetype.py
@@ -159,7 +159,7 @@ def unicode_from_encoded_object(space, w_obj, encoding, errors):
             space.w_TypeError,
             space.wrap(
                 "decoder did not return an unicode object (type=%s)" %
-                        space.type(retval).getname('?')))
+                        space.type(w_retval).getname('?')))
     return w_retval
 
 
