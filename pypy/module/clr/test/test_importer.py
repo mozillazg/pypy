93080972ee2fbbff5a08823359e6975737c8bd2e
Amit Regmi <regmee@codespeak.net>
2007-12-06 18:52:49 +0000

    import hooks implemented. More test cases added. Hardcoded .NET modules names for the check to be resolved using reflection

--- pypy/module/clr/test/test_importer.py
+++ pypy/module/clr/test/test_importer.py
@@ -15,6 +15,19 @@ class AppTestDotnet:
         Math = clr.load_cli_class('System', 'Math')
         assert Math is System.Math
 
+        import System.Collections.Stack
+        a = System.Collections.Stack()
+        a.Push(3)
+        a.Push(44)
+        sum = 0
+        for i in a:
+           sum += i
+        assert sum == 3+44
+
+        import System.Collections.ArrayList
+        ArrayList = clr.load_cli_class('System.Collections', 'ArrayList')
+        assert ArrayList is System.Collections.ArrayList
+
     def test_ImportError(self):
         skip('Fixme!')
         def fn():
