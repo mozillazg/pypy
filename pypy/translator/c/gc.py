d3afcb5a291c2f6380f655f75e2b20d815b30b52
Armin Rigo <arigo@tunes.org>
2007-12-22 15:18:43 +0000

    In-progress: the llvmgcroot GC transformer, with its custom StackRootIterator that walks the tables produced by the llvm GC plugin.

--- pypy/translator/c/gc.py
+++ pypy/translator/c/gc.py
@@ -5,7 +5,7 @@ from pypy.rpython.lltypesystem.lltype import \
      typeOf, Ptr, ContainerType, RttiStruct, \
      RuntimeTypeInfo, getRuntimeTypeInfo, top_container
 from pypy.rpython.memory.gctransform import \
-     refcounting, boehm, framework, stacklessframework
+     refcounting, boehm, framework, stacklessframework, llvmgcroot
 from pypy.rpython.lltypesystem import lltype, llmemory
 
 class BasicGcPolicy(object):
@@ -305,6 +305,9 @@ class StacklessFrameworkGcPolicy(FrameworkGcPolicy):
     transformerclass = stacklessframework.StacklessFrameworkGCTransformer
     requires_stackless = True
 
+class LLVMGcRootFrameworkGcPolicy(FrameworkGcPolicy):
+    transformerclass = llvmgcroot.LLVMGcRootFrameworkGCTransformer
+
 
 name_to_gcpolicy = {
     'boehm': BoehmGcPolicy,
@@ -312,6 +315,7 @@ name_to_gcpolicy = {
     'none': NoneGcPolicy,
     'framework': FrameworkGcPolicy,
     'framework+stacklessgc': StacklessFrameworkGcPolicy,
+    'framework+llvmgcroot': LLVMGcRootFrameworkGcPolicy,
 }
 
 
