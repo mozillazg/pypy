36991da3516cc257e933f39fbad3563c9496b301
Pavel Vinogradov <vinogradov@codespeak.net>
2007-12-23 08:21:15 +0000

    Implement methods of RopeString to pass construction test

--- pypy/rlib/ropewrapper.py
+++ pypy/rlib/ropewrapper.py
@@ -1,7 +1,17 @@
 from pypy.rlib import rope
 
 class RopeString(object):
-    pass
+    def __init__ (self, str):
+        self._node = rope.LiteralStringNode(str)
+    
+    def __len__ (self):
+        return self._node.length()
+
+    def __getitem__ (self, index):
+        return self._node.getchar(index)
+    
+    def __eq__ (self, str):
+        return rope.eq (self._node, rope.LiteralStringNode(str))
 
 class RopeUnicode(object):
     pass
