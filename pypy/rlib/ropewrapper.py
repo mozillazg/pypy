427ca54e0efb868b9a1ab5b28a0957358cc76edd
Pavel Vinogradov <vinogradov@codespeak.net>
2007-12-23 18:51:08 +0000

    Remove spaces before () according to PEP 8

--- pypy/rlib/ropewrapper.py
+++ pypy/rlib/ropewrapper.py
@@ -1,42 +1,42 @@
 from pypy.rlib import rope
 
 class RopeString(object):
-    def __init__ (self, s):
+    def __init__(self, s):
         self._node = rope.LiteralStringNode(s)
     
-    def __len__ (self):
+    def __len__(self):
         return self._node.length()
 
-    def __getitem__ (self, index):
+    def __getitem__(self, index):
         return self._node.getchar(index)
     
-    def __eq__ (self, other):
-        return rope.eq (self._node, rope.LiteralStringNode(other))
+    def __eq__(self, other):
+        return rope.eq(self._node, rope.LiteralStringNode(other))
     
-    def __add__ (self, other):
+    def __add__(self, other):
         result = RopeString('')
         result._node = self._node + other._node
 	return result
     
-    def __mul__ (self, n):
+    def __mul__(self, n):
         result = RopeString('')
         result._node = rope.multiply(self._node, n)
         return result
     
-    def __rmul__ (self, n):
+    def __rmul__(self, n):
         return self * n
 
 class RopeUnicode(object):
-    def __init__ (self, s):
-        if isinstance (s, str):
+    def __init__(self, s):
+        if isinstance(s, str):
             s = unicode(s)
         self._node = rope.LiteralUnicodeNode(unicode(s))
     
-    def __len__ (self):
+    def __len__(self):
         return self._node.length()
     
-    def __getitem__ (self, index):
+    def __getitem__(self, index):
         return self._node.getunichar(index)
     
-    def __eq__ (self, other):
-        return rope.eq (self._node, rope.LiteralUnicodeNode(other))
+    def __eq__(self, other):
+        return rope.eq(self._node, rope.LiteralUnicodeNode(other))
