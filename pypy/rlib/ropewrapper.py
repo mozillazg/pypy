5bb1045980d4b4c1c1adbd9861059de3dc97455d
Pavel Vinogradov <vinogradov@codespeak.net>
2007-12-31 17:49:39 +0000

    Update RopeString and RopeUnicode class to pass slice, add coerction test. Some work on encode/decode support

--- pypy/rlib/ropewrapper.py
+++ pypy/rlib/ropewrapper.py
@@ -23,8 +23,28 @@ class RopeBaseString(object):
         return self.__class__(rope.multiply(self._node, n))
     
     def __add__(self, other):
-        return self.__class__(self._node + other._node)
+        if isinstance(self, RopeUnicode) or isinstance(other, RopeUnicode):
+            return RopeUnicode(self._node + other._node)
+        else:
+            return self.__class__(self._node + other._node)
     
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            return self.getchar(index)
+        if isinstance(index, slice):
+            start, stop, step = index.start, index.stop, index.step
+            start = start or 0
+            stop = (stop and (stop < 0 and len(self) + stop or stop)) or len(self)
+            step = step or 1
+            return self.__class__(rope.getslice(self._node, start, stop, step))
+    
+    def getchar(self,index):
+        if isinstance(self, RopeString):
+            return self._node.getchar(index)
+        if isinstance(self, RopeUnicode):
+            return self._node.getunichar(index)
+        raise NotImplementedError("Index type not known.")
+        
 class RopeStringIterator(object):
     def __init__(self, node):
         self._iter = rope.ItemIterator(node)
@@ -42,9 +62,6 @@ class RopeString(RopeBaseString):
 	elif isinstance(s, rope.StringNode):
             self._node = s
     
-    def __getitem__(self, index):
-        return self._node.getchar(index)
-    
     def __eq__(self, other):
         if isinstance(other, RopeBaseString):
             return (rope.eq(self._node, other._node))
@@ -53,6 +70,16 @@ class RopeString(RopeBaseString):
     
     def __iter__(self):
         return RopeStringIterator(self._node)
+    
+    def decode(self, codepage):
+        if codepage == "utf-8":
+            return RopeUnicode(rope.str_decode_utf8(self._node))
+        if codepage == "latin1":
+            #Need rewrite
+            return RopeUnicode(rope.str_decode_latin1(self._node))
+        if codepage == "ascii":
+            #Need rewrite
+            return RopeUnicode(rope.str_decode_ascii(self._node))
 
 class RopeUnicodeIterator(object):
     def __init__(self, node):
@@ -73,9 +100,6 @@ class RopeUnicode(RopeBaseString):
         if isinstance(s, rope.StringNode):
             self._node = s
     
-    def __getitem__(self, index):
-        return self._node.getunichar(index)
-    
     def __eq__(self, other):
         if isinstance (other, RopeBaseString):
             return rope.eq(self._node, other._node)
@@ -84,3 +108,21 @@ class RopeUnicode(RopeBaseString):
    
     def __iter__(self):
         return RopeUnicodeIterator(self._node)
+    
+    def encode(self, codepage):
+        if codepage == "utf-8":
+            return RopeString(rope.unicode_encode_utf8(self._node))
+        if codepage == "utf-16":
+            raise NotImplemented("How i can encode utf-16 string?")
+        if codepage == "latin-1":
+            result = rope.unicode_encode_latin1(self._node)
+            if result:
+                return RopeString(result)
+            else:
+                raise NotImplementedError("Do i need implement such latin-1 encoding?")
+        if codepage == "ascii":
+            result = rope.unicode_encode_ascii(self._node)
+            if result:
+                return RopeString(result)
+            else:
+                raise NotImplementedError("Do i need implement such ascii encoding?")
