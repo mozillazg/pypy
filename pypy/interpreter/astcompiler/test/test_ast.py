c0d94ae1bfc079e48f6fdaadf9b90042390899e9
Armin Rigo <arigo@tunes.org>
2008-01-05 20:18:25 +0000

    Finally figured out how I'm supposed to use mutate(). The trick is that the default() method of the visitors is not doing the right thing at all in this case, leading to completely bogus performance.

--- pypy/interpreter/astcompiler/test/test_ast.py
+++ pypy/interpreter/astcompiler/test/test_ast.py
@@ -3,6 +3,8 @@ from pypy.module.recparser.pyparser import source2ast
 from pypy.interpreter.pyparser.test.test_astbuilder import FakeSpace
 
 class BaseVisitor(ast.ASTVisitor):
+    def default(self, node):
+        return node
     def visitAdd(self, node):
         return ast.Const(3)
 
