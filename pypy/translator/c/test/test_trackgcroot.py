52394e456eb09b4f1dc7543af32227268f1a43e7
Armin Rigo <arigo@tunes.org>
2008-01-20 16:01:48 +0000

    Starting unittests for trackgcroot.py.

---
+++ pypy/translator/c/test/test_trackgcroot.py
@@ -0,0 +1,29 @@
+import py
+import sys
+from pypy.translator.c.trackgcroot import GcRootTracker, FunctionGcRootTracker
+from StringIO import StringIO
+
+
+def test_find_functions():
+    source = """\
+\t.p2align 4,,15
+.globl pypy_g_make_tree
+\t.type\tpypy_g_make_tree, @function
+\tFOO
+\t.size\tpypy_g_make_tree, .-pypy_g_make_tree
+
+\t.p2align 4,,15
+.globl pypy_fn2
+\t.type\tpypy_fn2, @function
+\tBAR
+\t.size\tpypy_fn2, .-pypy_fn2
+\tMORE STUFF
+"""
+    lines = source.splitlines(True)
+    parts = list(GcRootTracker().find_functions(iter(lines)))
+    assert len(parts) == 5
+    assert parts[0] == (False, lines[:2])
+    assert parts[1] == (True,  lines[2:5])
+    assert parts[2] == (False, lines[5:8])
+    assert parts[3] == (True,  lines[8:11])
+    assert parts[4] == (False, lines[11:])
