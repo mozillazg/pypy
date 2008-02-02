dbc1ad9adb47dd9b5233e44efce88896e82aa49c
Carl Friedrich Bolz <cfbolz@gmx.de>
2008-02-02 12:34:40 +0000

    some cleanups, introduce a special empty key

--- pypy/jit/timeshifter/greenkey.py
+++ pypy/jit/timeshifter/greenkey.py
@@ -7,10 +7,16 @@ from pypy.rpython.lltypesystem import lltype
 class KeyDesc(object):
     __metaclass__ = cachedtype
 
-    def __init__(self, RGenOp, *TYPES):
+    def __init__(self, RGenOp=None, *TYPES):
         self.RGenOp = RGenOp
         self.TYPES = TYPES
         TARGETTYPES = []
+
+        if RGenOp is None:
+            assert len(TYPES) == 0
+            self.hash = lambda self: 0
+            self.compare = lambda self, other: True
+
         for TYPE in TYPES:
             # XXX more cases?
             TARGET = lltype.Signed
@@ -20,7 +26,7 @@ class KeyDesc(object):
 
         iterator = unrolling_iterable(enumerate(TARGETTYPES))
         length = len(TYPES)
-        def greenhash(self, rgenop):
+        def greenhash(self):
             retval = 0x345678
             mult = 1000003
             for i, TARGET in iterator:
@@ -30,7 +36,7 @@ class KeyDesc(object):
                 mult = mult + 82520 + 2*length
             return retval
         self.hash = greenhash
-        def greencompare(self, other, rgenop):
+        def greencompare(self, other):
             for i, TARGET in iterator:
                 genconst = self.values[i]
                 item_self = genconst.revealconst(TARGET)
@@ -46,10 +52,9 @@ class KeyDesc(object):
 
 
 class GreenKey(object):
-    def __init__(self, values, desc, rgenop):
+    def __init__(self, values, desc):
         self.desc = desc
         self.values = values
-        self.rgenop = rgenop
 
     def __eq__(self, other):
         raise TypeError("don't store GreenKeys in a normal dict")
@@ -61,17 +66,16 @@ class GreenKey(object):
         raise TypeError("not hashable")
 
 def greenkey_eq(self, other):
-    assert self.rgenop is other.rgenop
     if self is other:
         return True
     if self.desc is not other.desc:
         return False
-    return self.desc.compare(self, other, self.rgenop)
+    return self.desc.compare(self, other)
 
 def greenkey_hash(self):
-    return self.desc.hash(self, self.rgenop)
+    return self.desc.hash(self)
 
 def newgreendict():
     return r_dict(greenkey_eq, greenkey_hash)
 
-
+empty_key = GreenKey([], KeyDesc())
--- pypy/jit/timeshifter/test/test_greenkey.py
+++ pypy/jit/timeshifter/test/test_greenkey.py
@@ -1,5 +1,5 @@
 from pypy.jit.timeshifter.greenkey import GreenKey, KeyDesc, newgreendict
-from pypy.jit.timeshifter.greenkey import greenkey_hash, greenkey_eq
+from pypy.jit.timeshifter.greenkey import greenkey_hash, greenkey_eq, empty_key
 from pypy.jit.codegen.llgraph.rgenop import RGenOp
 from pypy.rpython.lltypesystem import lltype
 
@@ -8,7 +8,7 @@ rgenop = RGenOp()
 class TestGreenKeys(object):
     def newkey(self, *values):
         desc = KeyDesc(RGenOp, *[lltype.typeOf(val) for val in values])
-        return GreenKey([rgenop.genconst(val) for val in values], desc, rgenop)
+        return GreenKey([rgenop.genconst(val) for val in values], desc)
 
     def test_simple(self):
         d = newgreendict()
@@ -23,4 +23,11 @@ class TestGreenKeys(object):
     def test_check_lengths(self):
         d = newgreendict()
         d[self.newkey(1, 2)] = 1
+        assert self.newkey(1, 2, 0) not in d
+
+    def test_empty_key(self):
+        d = newgreendict()
+        assert empty_key not in d
+        d[empty_key] = 1
+        assert d[empty_key] == 1
         assert self.newkey(1, 2, 0) not in d
