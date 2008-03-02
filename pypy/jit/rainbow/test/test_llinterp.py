975532866fc9f56a7e3dc0ad9074017f8b5fdb3b
Carl Friedrich Bolz <cfbolz@gmx.de>
2008-03-02 20:17:49 +0000

    enable the llinterp tests, but add a --quicktest option to disable them again

--- pypy/jit/rainbow/test/test_llinterp.py
+++ pypy/jit/rainbow/test/test_llinterp.py
@@ -1,5 +1,10 @@
-import py; py.test.skip("in-progress")
+import py
+from pypy.jit.conftest import option
 from pypy.jit.rainbow.test import test_portal
+
+if option.quicktest:
+    py.test.skip("slow")
+
 
 
 class TestLLInterpreted(test_portal.TestPortal):
