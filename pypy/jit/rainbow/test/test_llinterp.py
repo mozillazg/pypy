fec95fd512829eb71f2eaaa3f03831dfc415c27a
Carl Friedrich Bolz <cfbolz@gmx.de>
2008-03-05 21:45:33 +0000

    show the current pypy-c-jit problem

--- pypy/jit/rainbow/test/test_llinterp.py
+++ pypy/jit/rainbow/test/test_llinterp.py
@@ -12,3 +12,19 @@ class TestLLInterpreted(test_portal.TestPortal):
 
     # for the individual tests see
     # ====> test_portal.py
+
+
+    def test_vdict_and_vlist(self):
+        py.test.skip("XXX")
+        def ll_function():
+            dic = {}
+            lst = [12] * 3
+            lst += []
+            lst.append(13)
+            lst.reverse()
+            dic[12] = 34
+            dic[lst[0]] = 35
+            return dic[lst.pop()]
+        res = self.timeshift_from_portal(ll_function, ll_function, [])
+        assert res == ll_function()
+        self.check_insns({})
