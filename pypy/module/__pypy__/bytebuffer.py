d03e467bb15d0aca20c453e5260b8fe883c3beb1
Armin Rigo <arigo@tunes.org>
2008-03-08 18:24:32 +0000

    Just because it's easy, here is a basic concrete RWBuffer object.

---
+++ pypy/module/__pypy__/bytebuffer.py
@@ -0,0 +1,24 @@
+#
+# A convenient read-write buffer.  Located here for want of a better place.
+#
+
+from pypy.interpreter.buffer import RWBuffer
+from pypy.interpreter.gateway import ObjSpace
+
+
+class ByteBuffer(RWBuffer):
+
+    def __init__(self, len):
+        self.len = len
+        self.data = ['\x00'] * len
+
+    def getitem(self, index):
+        return self.data[index]
+
+    def setitem(self, index, char):
+        self.data[index] = char
+
+
+def bytebuffer(space, length):
+    return space.wrap(ByteBuffer(length))
+bytebuffer.unwrap_spec = [ObjSpace, int]
