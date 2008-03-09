1cde6f4c4c3cfa2289137f21989de3b7dc58c22c
Armin Rigo <arigo@tunes.org>
2008-03-09 14:14:03 +0000

    In the app-level version of struct: struct.pack_into, struct.unpack_from

--- pypy/lib/struct.py
+++ pypy/lib/struct.py
@@ -337,3 +337,15 @@ def unpack(fmt,data):
                 j += format['size']
 
     return tuple(result)
+
+def pack_into(fmt, buf, offset, *args):
+    data = pack(fmt, *args)
+    buffer(buf)[offset:offset+len(data)] = data
+
+def unpack_from(fmt, buf, offset=0):
+    size = calcsize(fmt)
+    data = buffer(buf)[offset:offset+size]
+    if len(data) != size:
+        raise error("unpack_from requires a buffer of at least %d bytes"
+                    % (size,))
+    return unpack(fmt, data)
