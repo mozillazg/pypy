42277b78af7ba08014d7366b0e76d9a614b936d6
Carl Friedrich Bolz <cfbolz@gmx.de>
2008-03-17 12:58:08 +0000

    port some tests

--- pypy/jit/rainbow/test/test_hp_vlist.py
+++ pypy/jit/rainbow/test/test_hp_vlist.py
@@ -29,37 +29,59 @@ class TestVList(test_hotpath.HotPathTest):
         self.check_insns_in_loops({'int_gt': 1, 'int_rshift': 1, 'int_add': 1})
 
     def test_enter_block(self):
-        py.test.skip("port me")
+        class MyJitDriver(JitDriver):
+            greens = []
+            reds = ['i', 'tot', 'flag']
         def ll_function(flag):
-            lst = []
-            lst.append(flag)
-            lst.append(131)
-            if flag:
-                return lst[0]
-            else:
-                return lst[1]
-        res = self.interpret(ll_function, [6], [], policy=P_OOPSPEC)
-        assert res == 6
-        self.check_insns({'int_is_true': 1})
-        res = self.interpret(ll_function, [0], [], policy=P_OOPSPEC)
-        assert res == 131
-        self.check_insns({'int_is_true': 1})
+            tot = 0
+            i = 1024
+            while i > 0:
+                i >>= 1
+                lst = []
+                lst.append(flag)
+                lst.append(131)
+                if flag:
+                    tot += lst[0]
+                else:
+                    tot += lst[1]
+                MyJitDriver.jit_merge_point(tot=tot, i=i, flag=flag)
+                MyJitDriver.can_enter_jit(tot=tot, i=i, flag=flag)
+            return tot
+        res = self.run(ll_function, [6], threshold=2)
+        assert res == ll_function(6)
+        self.check_insns_in_loops({'int_gt': 1, 'int_rshift': 1, 'int_add': 1,
+                                   'int_is_true': 1})
+        res = self.run(ll_function, [0], threshold=2)
+        assert res == ll_function(0)
+        self.check_insns_in_loops({'int_gt': 1, 'int_rshift': 1, 'int_add': 1,
+                                   'int_is_true': 1})
 
     def test_merge(self):
-        py.test.skip("port me")
+        class MyJitDriver(JitDriver):
+            greens = []
+            reds = ['i', 'tot', 'flag']
         def ll_function(flag):
-            lst = []
-            if flag:
-                lst.append(flag)
-            else:
-                lst.append(131)
-            return lst[-1]
-        res = self.interpret(ll_function, [6], [], policy=P_OOPSPEC)
-        assert res == 6
-        self.check_insns({'int_is_true': 1})
-        res = self.interpret(ll_function, [0], [], policy=P_OOPSPEC)
-        assert res == 131
-        self.check_insns({'int_is_true': 1})
+            tot = 0
+            i = 1024
+            while i > 0:
+                i >>= 1
+                lst = []
+                if flag:
+                    lst.append(flag)
+                else:
+                    lst.append(131)
+                tot += lst[-1]
+                MyJitDriver.jit_merge_point(tot=tot, i=i, flag=flag)
+                MyJitDriver.can_enter_jit(tot=tot, i=i, flag=flag)
+            return tot
+        res = self.run(ll_function, [6], threshold=2, policy=P_OOPSPEC)
+        assert res == ll_function(6)
+        self.check_insns_in_loops({'int_gt': 1, 'int_rshift': 1, 'int_add': 1,
+                                   'int_is_true': 1})
+        res = self.run(ll_function, [0], threshold=2, policy=P_OOPSPEC)
+        assert res == ll_function(0)
+        self.check_insns_in_loops({'int_gt': 1, 'int_rshift': 1, 'int_add': 1,
+                                   'int_is_true': 1})
 
     def test_replace(self):
         py.test.skip("port me")
@@ -78,56 +100,85 @@ class TestVList(test_hotpath.HotPathTest):
         self.check_insns({'int_is_true': 1})
 
     def test_force(self):
-        py.test.skip("port me")
-        def ll_function(n):
-            lst = []
-            lst.append(n)
-            if n:
-                lst.append(12)
-            return lst[-1]
-        res = self.interpret(ll_function, [6], [], policy=P_OOPSPEC)
-        assert res == 12
-        res = self.interpret(ll_function, [0], [], policy=P_OOPSPEC)
-        assert res == 0
+        class MyJitDriver(JitDriver):
+            greens = []
+            reds = ['i', 'tot', 'flag']
+        def ll_function(flag):
+            i = 1024
+            tot = 0
+            while i:
+                i >>= 1
+                lst = []
+                lst.append(flag)
+                if flag:
+                    lst.append(12)
+                tot += lst[-1]
+                MyJitDriver.jit_merge_point(tot=tot, i=i, flag=flag)
+                MyJitDriver.can_enter_jit(tot=tot, i=i, flag=flag)
+            return tot
+        res = self.run(ll_function, [6], 2, policy=P_OOPSPEC)
+        assert res == ll_function(6)
+        res = self.run(ll_function, [0], 2, policy=P_OOPSPEC)
+        assert res == ll_function(0)
 
     def test_oop_vlist(self):
-        py.test.skip("port me")
+        class MyJitDriver(JitDriver):
+            greens = []
+            reds = ['i', 'tot']
         def ll_function():
-            lst = [3, 5]
-            five = lst.pop()        # [3]
-            lst.append(len(lst))    # [3, 1]
-            lst2 = list(lst)
-            three = lst.pop(0)      # [1]
-            lst.insert(0, 8)        # [8, 1]
-            lst.insert(2, 7)        # [8, 1, 7]
-            lst.append(not lst)     # [8, 1, 7, 0]
-            lst.reverse()           # [0, 7, 1, 8]
-            lst3 = lst2 + lst       # [3, 1, 0, 7, 1, 8]
-            del lst3[1]             # [3, 0, 7, 1, 8]
-            seven = lst3.pop(2)     # [3, 0, 1, 8]
-            lst3[0] = 9             # [9, 0, 1, 8]
-            nine = lst3.pop(-4)     # [0, 1, 8]
-            return (len(lst3) * 10000000 +
-                    lst3[0]   *  1000000 +
-                    lst3[1]   *   100000 +
-                    lst3[-1]  *    10000 +
-                    five      *     1000 +
-                    three     *      100 +
-                    seven     *       10 +
-                    nine      *        1)
-        assert ll_function() == 30185379
-        res = self.interpret(ll_function, [], [], policy=P_OOPSPEC)
-        assert res == 30185379
-        self.check_insns({})
+            i = 1024
+            tot = 0
+            while i:
+                i >>= 1
+                lst = [3, 5]
+                five = lst.pop()        # [3]
+                lst.append(len(lst))    # [3, 1]
+                lst2 = list(lst)
+                three = lst.pop(0)      # [1]
+                lst.insert(0, 8)        # [8, 1]
+                lst.insert(2, 7)        # [8, 1, 7]
+                lst.append(not lst)     # [8, 1, 7, 0]
+                lst.reverse()           # [0, 7, 1, 8]
+                lst3 = lst2 + lst       # [3, 1, 0, 7, 1, 8]
+                del lst3[1]             # [3, 0, 7, 1, 8]
+                seven = lst3.pop(2)     # [3, 0, 1, 8]
+                lst3[0] = 9             # [9, 0, 1, 8]
+                nine = lst3.pop(-4)     # [0, 1, 8]
+                tot += (len(lst3) * 10000000 +
+                        lst3[0]   *  1000000 +
+                        lst3[1]   *   100000 +
+                        lst3[-1]  *    10000 +
+                        five      *     1000 +
+                        three     *      100 +
+                        seven     *       10 +
+                        nine      *        1)
+                MyJitDriver.jit_merge_point(tot=tot, i=i)
+                MyJitDriver.can_enter_jit(tot=tot, i=i)
+            return tot
+        assert ll_function() == 30185379 * 11
+        res = self.run(ll_function, [], 2, policy=P_OOPSPEC)
+        assert res == 30185379 * 11
+        self.check_insns_in_loops({'int_rshift': 1, 'int_add': 1,
+                                   'int_is_true': 1})
 
     def test_alloc_and_set(self):
-        py.test.skip("port me")
+        class MyJitDriver(JitDriver):
+            greens = []
+            reds = ['i', 'tot']
         def ll_function():
-            lst = [0] * 9
-            return len(lst)
-        res = self.interpret(ll_function, [], [], policy=P_OOPSPEC)
-        assert res == 9
-        self.check_insns({})
+            i = 1024
+            tot = 0
+            while i:
+                i >>= 1
+                lst = [0] * 9
+                tot += len(lst)
+                MyJitDriver.jit_merge_point(tot=tot, i=i)
+                MyJitDriver.can_enter_jit(tot=tot, i=i)
+            return tot
+        res = self.run(ll_function, [], 2, policy=P_OOPSPEC)
+        assert res == 9 * 11
+        self.check_insns_in_loops({'int_rshift': 1, 'int_add': 1,
+                                   'int_is_true': 1})
         
     def test_lists_deepfreeze(self):
         py.test.skip("port me")
@@ -150,33 +201,55 @@ class TestVList(test_hotpath.HotPathTest):
         self.check_insns({})
 
     def test_frozen_list(self):
-        py.test.skip("port me")
         lst = [5, 7, 9]
+        class MyJitDriver(JitDriver):
+            greens = ['x']
+            reds = ['i', 'tot']
         def ll_function(x):
-            mylist = hint(lst, deepfreeze=True)
-            z = mylist[x]
-            hint(z, concrete=True)
-            return z
+            i = 1024
+            tot = 0
+            while i:
+                i >>= 1
+                mylist = hint(lst, deepfreeze=True)
+                z = mylist[x]
+                hint(z, concrete=True)
+                tot += z
+                MyJitDriver.jit_merge_point(x=x, tot=tot, i=i)
+                MyJitDriver.can_enter_jit(x=x, tot=tot, i=i)
+            return tot
 
-        res = self.interpret(ll_function, [1], policy=P_OOPSPEC)
-        assert res == 7
-        self.check_insns({})
+        res = self.run(ll_function, [1], 2, policy=P_OOPSPEC)
+        assert res == 7 * 11
+        self.check_insns_in_loops({'int_rshift': 1, 'int_add': 1,
+                                   'int_is_true': 1})
 
     def test_frozen_list_indexerror(self):
-        py.test.skip("port me")
+        py.test.skip("fix me")
         lst = [5, 7, 9]
+        class MyJitDriver(JitDriver):
+            greens = ['x']
+            reds = ['i', 'tot']
         def ll_function(x):
-            mylist = hint(lst, deepfreeze=True)
-            try:
-                z = mylist[x]
-            except IndexError:
-                return -42
-            hint(z, concrete=True)
-            return z
+            i = 1024
+            tot = 0
+            while i:
+                i >>= 1
+                mylist = hint(lst, deepfreeze=True)
+                try:
+                    z = mylist[x]
+                except IndexError:
+                    tot += -42
+                else:
+                    hint(z, concrete=True)
+                    tot += z
+                MyJitDriver.jit_merge_point(x=x, tot=tot, i=i)
+                MyJitDriver.can_enter_jit(x=x, tot=tot, i=i)
+            return tot
 
-        res = self.interpret(ll_function, [4], policy=P_OOPSPEC)
-        assert res == -42
-        self.check_insns({})
+        res = self.run(ll_function, [4], threshold=2, policy=P_OOPSPEC)
+        assert res == -42 * 11
+        self.check_insns_in_loops({'int_rshift': 1, 'int_add': 1,
+                                   'int_is_true': 1})
 
     def test_bogus_index_while_compiling(self):
         py.test.skip("implement me")
