8afc7bfa8e8c0d8638b64a89e4946683f57d8482
Maciej Fijalkowski <fijall@gmail.com>
2008-03-30 19:17:36 +0000

    skip all tests which are not supported

--- pypy/jit/codegen/ia32/test/test_rgenop.py
+++ pypy/jit/codegen/ia32/test/test_rgenop.py
@@ -1,3 +1,4 @@
+import py
 from pypy.jit.codegen.ia32.rgenop import RI386GenOp
 from pypy.jit.codegen.test.rgenop_tests import AbstractRGenOpTestsDirect
 from pypy.jit.codegen.test.rgenop_tests import AbstractRGenOpTestsCompile
@@ -8,6 +9,31 @@ from pypy.jit.codegen.test.rgenop_tests import AbstractRGenOpTestsCompile
 class TestRI386GenopDirect(AbstractRGenOpTestsDirect):
     RGenOp = RI386GenOp
     from pypy.jit.codegen.i386.test.test_operation import RGenOpPacked
+
+    def skipped(self):
+        py.test.skip("unsupported")
+
+    # frame access related
+    test_read_frame_var_direct = skipped
+    test_genconst_from_frame_var_direct = skipped
+    test_write_frame_place_direct = skipped
+    test_write_lots_of_frame_places_direct = skipped
+    test_read_frame_place_direct = skipped
+    test_frame_vars_like_the_frontend_direct = skipped
+
+    # unsupported operations
+    test_genzeroconst = skipped
+
+    # overflow
+    test_ovfcheck_adder_direct = skipped
+    test_ovfcheck1_direct = skipped
+    test_ovfcheck2_direct = skipped
+
+    # casts
+    test_cast_direct = skipped
+
+    # void returns
+    test_void_return = skipped
 
 class TestRI386GenopCompile(AbstractRGenOpTestsCompile):
     RGenOp = RI386GenOp
