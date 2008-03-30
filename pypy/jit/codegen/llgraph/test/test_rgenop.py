b2189f69efd6a3028316cc79c026fcacec3e5d8c
Maciej Fijalkowski <fijall@gmail.com>
2008-03-30 18:04:08 +0000

    port llgraph tests

--- pypy/jit/codegen/llgraph/test/test_rgenop.py
+++ pypy/jit/codegen/llgraph/test/test_rgenop.py
@@ -2,18 +2,21 @@ import py
 from pypy.rpython.lltypesystem import lltype
 from pypy.jit.codegen.llgraph.rgenop import RGenOp
 from pypy.jit.codegen.llgraph.llimpl import testgengraph
-from pypy.jit.codegen.test.rgenop_tests import AbstractRGenOpTests
+from pypy.jit.codegen.test.rgenop_tests import AbstractRGenOpTestsDirect
+from pypy.jit.codegen.test.rgenop_tests import AbstractRGenOpTestsCompile
 from pypy.rpython.test.test_llinterp import gengraph, interpret
 
+# for the individual tests see
+# ====> ../../test/rgenop_tests.py
 
-class TestLLGraphRGenop(AbstractRGenOpTests):
+class BaseLLGraphRGenop(object):
     RGenOp = RGenOp
     RGenOpPacked = RGenOp
 
     def setup_method(self, meth):
         if 'ovfcheck' in meth.__name__:
             py.test.skip("no chance (the llinterpreter has no rtyper)")
-        AbstractRGenOpTests.setup_method(self, meth)
+        super(BaseLLGraphRGenop, self).setup_method(meth)
 
     def getcompiled(self, runner, argtypes, annotatorpolicy):
         def quasi_compiled_runner(*args):
@@ -25,10 +28,13 @@ class TestLLGraphRGenop(AbstractRGenOpTests):
         argtypes = [annmodel.lltype_to_annotation(T) for T in FUNC.TO.ARGS]
         t, rtyper, graph = gengraph(func, argtypes)
         return rtyper.getcallable(graph)
+    
 
-    # for the individual tests see
-    # ====> ../../test/rgenop_tests.py
+class TestLLGraphRGenopDirect(BaseLLGraphRGenop, AbstractRGenOpTestsDirect):
+    pass
 
+class TestLLGraphRGenopCompile(BaseLLGraphRGenop, AbstractRGenOpTestsCompile):
+    pass
 
 def test_not_calling_end_explodes():
     F1 = lltype.FuncType([lltype.Signed], lltype.Signed)
