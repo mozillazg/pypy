ebc2c7c3f74ea16440779496754972e8f6b77cbf
Maciej Fijalkowski <fijall@gmail.com>
2008-03-30 18:14:12 +0000

    port those tests

--- pypy/jit/codegen/ppc/test/test_rgenop.py
+++ pypy/jit/codegen/ppc/test/test_rgenop.py
@@ -1,8 +1,9 @@
 import py
-py.test.skip("Port this tests to new style")
 from pypy.jit.codegen.ppc.rgenop import RPPCGenOp
 from pypy.rpython.lltypesystem import lltype
-from pypy.jit.codegen.test.rgenop_tests import AbstractRGenOpTests, FUNC, FUNC2
+from pypy.jit.codegen.test.rgenop_tests import FUNC, FUNC2
+from pypy.jit.codegen.test.rgenop_tests import AbstractRGenOpTestsDirect
+from pypy.jit.codegen.test.rgenop_tests import AbstractRGenOpTestsCompile
 from ctypes import cast, c_int, c_void_p, CFUNCTYPE
 from pypy.jit.codegen.ppc import instruction as insn
 
@@ -19,15 +20,14 @@ class FewRegisters(RPPCGenOp):
 class FewRegistersAndScribble(FewRegisters):
     DEBUG_SCRIBBLE = True
 
-class TestRPPCGenop(AbstractRGenOpTests):
+class TestRPPCGenopDirect(AbstractRGenOpTestsDirect):
     RGenOp = RPPCGenOp
 
+class TestRPPCGenopCompile(AbstractRGenOpTestsCompile):
+    RGenOp = RPPCGenOp    
 
-class TestRPPCGenopNoRegs(TestRPPCGenop):
+class TestRPPCGenopNoRegs(AbstractRGenOpTestsDirect):
     RGenOp = FewRegisters
 
-    def compile(self, runner, argtypes):
-        py.test.skip("Skip compiled tests w/ restricted register allocator")
-
-class TestRPPCGenopNoRegsAndScribble(TestRPPCGenopNoRegs):
+class TestRPPCGenopNoRegsAndScribble(AbstractRGenOpTestsDirect):
     RGenOp = FewRegistersAndScribble
