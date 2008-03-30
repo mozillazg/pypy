3e65cf567e219f58fc6503e8b32e4a5a8c2ac944
Maciej Fijalkowski <fijall@gmail.com>
2008-03-30 18:22:42 +0000

    port tests to new style

--- pypy/jit/codegen/ia32/test/test_rgenop.py
+++ pypy/jit/codegen/ia32/test/test_rgenop.py
@@ -1,8 +1,17 @@
 from pypy.jit.codegen.ia32.rgenop import RI386GenOp
-from pypy.jit.codegen.test.rgenop_tests import AbstractRGenOpTests
+from pypy.jit.codegen.test.rgenop_tests import AbstractRGenOpTestsDirect
+from pypy.jit.codegen.test.rgenop_tests import AbstractRGenOpTestsCompile
 
-class TestRI386Genop(AbstractRGenOpTests):
+# for the individual tests see
+# ====> ../../test/rgenop_tests.py
+
+class TestRI386GenopDirect(AbstractRGenOpTestsDirect):
     RGenOp = RI386GenOp
+    from pypy.jit.codegen.i386.test.test_operation import RGenOpPacked
 
-    # for the individual tests see
-    # ====> ../../test/rgenop_tests.py
+class TestRI386GenopCompile(AbstractRGenOpTestsCompile):
+    RGenOp = RI386GenOp
+    from pypy.jit.codegen.i386.test.test_operation import RGenOpPacked
+
+    def setup_class(cls):
+        py.test.skip("skip compilation tests")
