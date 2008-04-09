ff12b523d682c6e5d5e7fe298732bfd6b3dd550a
Armin Rigo <arigo@tunes.org>
2008-04-09 18:17:33 +0000

    Drop some hop.exception_cannot_occur() in the branch too.

--- pypy/rlib/rgc.py
+++ pypy/rlib/rgc.py
@@ -157,6 +157,7 @@ class CollectEntry(ExtRegistryEntry):
         return annmodel.s_None
 
     def specialize_call(self, hop):
+        hop.exception_cannot_occur()
         return hop.genop('gc__collect', [], resulttype=hop.r_result)
     
 class SetMaxHeapSizeEntry(ExtRegistryEntry):
@@ -169,6 +170,7 @@ class SetMaxHeapSizeEntry(ExtRegistryEntry):
     def specialize_call(self, hop):
         from pypy.rpython.lltypesystem import lltype
         [v_nbytes] = hop.inputargs(lltype.Signed)
+        hop.exception_cannot_occur()
         return hop.genop('gc_set_max_heap_size', [v_nbytes],
                          resulttype=lltype.Void)
 def can_move(P):
@@ -184,6 +186,7 @@ class CanMoveEntry(ExtRegistryEntry):
 
     def specialize_call(self, hop):
         from pypy.rpython.lltypesystem import lltype
+        hop.exception_cannot_occur()
         return hop.genop('gc_can_move', [], resulttype=lltype.Bool)
 
 class CollectEntry(ExtRegistryEntry):
@@ -195,5 +198,5 @@ class CollectEntry(ExtRegistryEntry):
 
     def specialize_call(self, hop):
         opname = 'gc__' + self.instance.__name__
+        hop.exception_cannot_occur()
         return hop.genop(opname, [], resulttype=hop.r_result)
-
