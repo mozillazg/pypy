297166ea4b06ff14491a17039b269f3361507085
Maciej Fijalkowski <fijall@gmail.com>
2008-04-23 10:48:39 +0000

    a target showcasing how bad is our readline implementation (after translation it's roughly 2x slower than run directly cpy version)

--- pypy/translator/goal/targetnopstandalone.py
+++ pypy/translator/goal/targetnopstandalone.py
@@ -8,14 +8,26 @@ actually implementing argv of the executable.
 """
 
 import os, sys
+from pypy.rpython.lltypesystem import lltype, llmemory
+
+def fib(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
 
 def debug(msg): 
-    os.write(2, "debug: " + msg + '\n')
+    for i in range(36):
+        print "n=%d => %d" % (i, fib(i))
+
 
 # __________  Entry point  __________
 
 def entry_point(argv):
     debug("hello world")
+    print llmemory.sizeof(lltype.UniChar)
+    if len(argv) > 3:
+        raise ValueError
     return 0
 
 # _____ Define and setup target ___
