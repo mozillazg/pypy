00c6933d84b5303aa78c594334d3eb90e8aaab7a
Antonio Cuni <anto.cuni@gmail.com>
2008-04-24 12:35:01 +0000

    this test passes out of the box

--- pypy/jit/rainbow/test/test_portal.py
+++ pypy/jit/rainbow/test/test_portal.py
@@ -610,7 +610,6 @@ class TestPortalOOType(BaseTestPortal):
     def _skip(self):
         py.test.skip('in progress')
 
-    test_dfa_compile2 = _skip
     test_dfa_compile3 = _skip
     test_method_call_nonpromote = _skip
     test_method_call_promote = _skip
