2b6c64ba317d947c18cd4cedcb425fa9f6fdd100
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2008-05-09 18:27:45 +0000

    Remove _minimal_curses from --allworkingmodules on Windows
    Translation on Windows is failing with an exception ending with an ImportError for "_curses"
    from pypy/module/_minimal_curses/interp_curses.py

--- pypy/config/pypyoption.py
+++ pypy/config/pypyoption.py
@@ -34,7 +34,7 @@ if sys.platform == "win32":
     del working_modules["crypt"]
     del working_modules["termios"]
     del working_modules["_rawffi"]
-
+    del working_modules["_minimal_curses"]
 
 module_dependencies = {}
 module_suggests = {    # the reason you want _rawffi is for ctypes, which
