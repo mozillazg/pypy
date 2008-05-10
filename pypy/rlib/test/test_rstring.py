6102c59b317b337d3cd50bfd87fe6ee72f222da9
Maciej Fijalkowski <fijall@gmail.com>
2008-05-10 20:05:50 +0000

    add to string builder operation append_slice which takes a source string, start and end of slice to add

--- pypy/rlib/test/test_rstring.py
+++ pypy/rlib/test/test_rstring.py
@@ -6,12 +6,13 @@ def test_string_builder():
     s.append("a")
     s.append("abc")
     s.append("a")
-    assert s.build() == "aabca"
+    s.append_slice("abc", 1, 2)
+    assert s.build() == "aabcab"
 
 def test_unicode_builder():
     s = UnicodeBuilder()
     s.append(u'a')
     s.append(u'abc')
-    s.append(u'abcdef')
-    assert s.build() == 'aabcabcdef'
+    s.append_slice(u'abcdef', 1, 2)
+    assert s.build() == 'aabcb'
     assert isinstance(s.build(), unicode)
