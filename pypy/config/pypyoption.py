19688698abee6b0a132f6f479fba23fc0aea2a2f
Michael Schneider <bigdog@codespeak.net>
2008-05-12 16:05:38 +0000

    remove zlib until it successfully builds on buildslave.  Link issue

--- pypy/config/pypyoption.py
+++ pypy/config/pypyoption.py
@@ -39,6 +39,8 @@ if sys.platform == "win32":
     del working_modules["signal"]
     del working_modules["_rawffi"]
     # modules with broken windows support
+    del working_modules["zlib"]    # MLS - Added 5/11/08 - link issue
+
     del working_modules["mmap"]    # MLS - Added 5/11/08 - broken
     del working_modules["_socket"] # MLS - Added 5/11/08 - broken
     del working_modules["select"] # MLS - Added 5/11/08 - broken
