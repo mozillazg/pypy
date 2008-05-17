9a65baefe0cbc39326241a88320a648b7d2ba4d6
Toon Verwaest <tverwaes@codespeak.net>
2008-05-17 14:36:34 +0000

    (cfbolz, tverwaes) belongs to the last commit

--- pypy/lang/smalltalk/wrapper.py
+++ pypy/lang/smalltalk/wrapper.py
@@ -35,52 +35,39 @@ def make_getter_setter(index0):
 class LinkWrapper(Wrapper):
     next_link, store_next_link = make_getter_setter(0)
 
+class ProcessWrapper(LinkWrapper):
+    suspended_context, store_suspended_context = make_getter_setter(1)
+    priority = make_getter(2)
+    my_list, store_my_list = make_getter_setter(3)
 
-'''class LinkedListShadow(AbstractShadow):
-    def __init__(self, w_self, invalid):
-        AbstractShadow.__init__(self, w_self, invalid)
-
-    def w_firstlink(self):
-        w_v = self.w_self()._vars[constants.FIRST_LINK_INDEX]
-        assert isinstance(w_v, model.W_PointersObject)
-        return w_v
-
-    def store_w_firstlink(self, w_object):
-        self.w_self()._vars[constants.FIRST_LINK_INDEX] = w_object
-
-    def w_lastlink(self):
-        w_v = self.w_self()._vars[constants.LAST_LINK_INDEX]
-        assert isinstance(w_v, model.W_PointersObject)
-        return w_v
-
-    def store_w_lastlink(self, w_object):
-        self.w_self()._vars[constants.LAST_LINK_INDEX] = w_object
+class LinkedListWrapper(Wrapper):
+    first_link, store_first_link = make_getter_setter(0)
+    last_link, store_last_link = make_getter_setter(1)
 
     def is_empty_list(self):
         from pypy.lang.smalltalk import objtable
-        return self.w_firstlink() == objtable.w_nil
+        return self.first_link() is objtable.w_nil
 
     def add_last_link(self, w_object):
         if self.is_empty_list():
-            self.store_w_firstlink(w_object)
+            self.store_first_link(w_object)
         else:
-            self.w_lastlink().as_link_get_shadow().store_next(w_object)
-        # XXX Slang version stores list in process here...
-        self.store_w_lastlink(w_object)
+            LinkWrapper(self.last_link()).store_next_link(w_object)
+        self.store_last_link(w_object)
 
     def remove_first_link_of_list(self):
         from pypy.lang.smalltalk import objtable
-        first = self.w_firstlink()
-        last = self.w_lastlink()
-        if first == last:
-            self.store_w_firstlink(objtable.w_nil)
-            self.store_w_lastlink(objtable.w_nil)
+        w_first = self.first_link()
+        w_last = self.last_link()
+        if w_first is w_last:
+            self.store_first_link(objtable.w_nil)
+            self.store_last_link(objtable.w_nil)
         else:
-            next = first.as_process_get_shadow().next()
-            self.store_w_firstlink(next)
-        first.as_process_get_shadow().store_next(objtable.w_nil)
-        return first
-
+            w_next = LinkWrapper(w_first).next_link()
+            self.store_first_link(w_next)
+        LinkWrapper(w_first).store_next_link(objtable.w_nil)
+        return w_first
+'''
 class SemaphoreShadow(LinkedListShadow):
     """A shadow for Smalltalk objects that are semaphores
     """
