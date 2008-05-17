73073bcf5fd49c1811f2f5a612064975cd14e495
Armin Rigo <arigo@tunes.org>
2008-05-17 10:31:35 +0000

    (karlb, cami, arigo) SetVideoMode works.

--- pypy/rlib/rsdl/test/test_basic.py
+++ pypy/rlib/rsdl/test/test_basic.py
@@ -5,3 +5,9 @@ from pypy.rlib.rsdl import RSDL
 def test_sdl_init():
     assert RSDL.Init(RSDL.INIT_VIDEO) >= 0
     RSDL.Quit()
+
+def test_set_video_mode():
+    assert RSDL.Init(RSDL.INIT_VIDEO) >= 0
+    surface = RSDL.SetVideoMode(640, 480, 32, 0)
+    assert surface
+    RSDL.Quit()
