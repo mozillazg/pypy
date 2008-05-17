657ed30f9a63f675267a69842c75145cbf64d195
Armin Rigo <arigo@tunes.org>
2008-05-17 11:23:15 +0000

    Reorganize tests so that "py.test --view" is needed to run the tests that try to open a window.

--- pypy/rlib/rsdl/test/test_basic.py
+++ pypy/rlib/rsdl/test/test_basic.py
@@ -2,16 +2,11 @@ import py
 from pypy.rlib.rsdl import RSDL
 from pypy.rlib.rarithmetic import r_uint
 from pypy.rpython.lltypesystem import rffi
+from pypy import conftest
 
 
 def test_sdl_init():
     assert RSDL.Init(RSDL.INIT_VIDEO) >= 0
-    RSDL.Quit()
-
-def test_set_video_mode():
-    assert RSDL.Init(RSDL.INIT_VIDEO) >= 0
-    surface = RSDL.SetVideoMode(640, 480, 32, 0)
-    assert surface
     RSDL.Quit()
 
 def test_surface_basic():
@@ -26,3 +21,20 @@ def test_surface_basic():
     assert rffi.getintfield(surface, 'c_h') == 50
     RSDL.FreeSurface(surface)
     RSDL.Quit()
+
+
+class TestVideo:
+
+    def setup_method(self, meth):
+        if not conftest.option.view:
+            py.test.skip("'--view' not specified, "
+                         "skipping tests that open a window")
+        assert RSDL.Init(RSDL.INIT_VIDEO) >= 0
+        self.surface = RSDL.SetVideoMode(640, 480, 32, 0)
+        assert self.surface
+
+    def test_simple(self):
+        pass   # only checks that opening and closing the window works
+
+    def teardown_method(self, meth):
+        RSDL.Quit()
