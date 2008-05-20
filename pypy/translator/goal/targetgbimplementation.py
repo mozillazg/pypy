f64897660a055606c470a019061b0913334636f6
Camillo Bruni <cami@codespeak.net>
2008-05-20 15:25:50 +0000

    remove old squeakminiinagetest text in the header fixed joypaddriver translates now correctly

--- pypy/translator/goal/targetgbimplementation.py
+++ pypy/translator/goal/targetgbimplementation.py
@@ -7,28 +7,15 @@ ROM_PATH = str(py.magic.autopath().dirpath().dirpath().dirpath())+"/lang/gameboy
 EMULATION_CYCLES = 64
 
 
-# This loads the whole mini.image in advance.  At run-time,
-# it executes the tinyBenchmark.  In this way we get an RPython
-# "image" frozen into the executable, mmap'ed by the OS from
-# there and loaded lazily when needed :-)
-
-
-# XXX this only compiles if sys.recursionlimit is high enough!
-# On non-Linux platforms I don't know if there is enough stack to
-# compile...
-#sys.setrecursionlimit(100000)
-
-
 def entry_point(argv=None):
     if len(argv) > 1:
         filename = argv[1]
     else:
         filename = ROM_PATH+"/rom9/rom9.gb"
+    print "using file: ", str(filename)
     gameBoy = GameBoyImplementation()
-    #gameBoy.load_cartridge_file(ROM_PATH+"/rom4/rom4.gb")#filename)
+    gameBoy.load_cartridge_file(ROM_PATH+"/rom4/rom4.gb")
     gameBoy.emulate(EMULATION_CYCLES)
-#    #gameBoy.load_cartridge_file(str(filename))
-    
     return 0
     
 
