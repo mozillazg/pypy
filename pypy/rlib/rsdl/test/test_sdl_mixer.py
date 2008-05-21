64b0e5545a0a98cd812f6f0e483a230a3cb40dff
Karl Bartel <karlb@codespeak.net>
2008-05-21 17:32:44 +0000

    Added support for LoadWAV to RMix

--- pypy/rlib/rsdl/test/test_sdl_mixer.py
+++ pypy/rlib/rsdl/test/test_sdl_mixer.py
@@ -7,3 +7,13 @@ def test_open_mixer():
     if RMix.OpenAudio(22050, RSDL.AUDIO_S16LSB, 2, 1024) != 0:
         error = rffi.charp2str(RSDL.GetError())
         raise Exception(error)
+    RMix.CloseAudio()
+
+def test_load_wav():
+    if RMix.OpenAudio(22050, RSDL.AUDIO_S16LSB, 2, 1024) != 0:
+        error = rffi.charp2str(RSDL.GetError())
+        raise Exception(error)
+    filename = rffi.str2charp('test.wav')
+    RMix.LoadWAV(filename)
+    rffi.free_charp(filename)
+    RMix.CloseAudio()
