e833da05718fc6650e62afad970410f96bdcf52e
Antonio Cuni <anto.cuni@gmail.com>
2008-05-23 17:08:46 +0000

    implement dict with void key for gencli; this allows jit/codegen/cli/test_gencli_portal/test_main_as_portal to pass, but it seems that such dicts are not really supported by rpython, as you can see by uncommeting the test in test_dict or moving the tests to rpython/test/test_rdict :-(

--- pypy/translator/cli/test/test_dict.py
+++ pypy/translator/cli/test/test_dict.py
@@ -17,6 +17,26 @@ class TestCliDict(CliTest, oodict.BaseTestDict):
             return d[0]
         assert self.interpret(fn, [2]) is None
 
+    def test_dict_with_void_key(self):
+        def fn(flag):
+            d = {}
+            if flag:
+                d[None] = flag
+            return bool(d)
+        res = self.interpret(fn, [42])
+        assert res is True
+
+##   XXX: it fails because of a bug in the annotator, which thinks the
+##   last line always raises
+##    def test_dict_with_void_key_pbc(self):
+##        d = {}
+##        def fn(flag):
+##            if flag:
+##                d[None] = flag
+##            return d[None]
+##        res = self.interpret(fn, [42], backendopt=False)
+##        assert res == 42
+
 class TestCliEmptyDict(CliTest, oodict.BaseTestEmptyDict):
     pass
 
