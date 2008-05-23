d7bb16f095ab2f0c7c2f9089f762bcd35e46e005
Maciej Fijalkowski <fijall@gmail.com>
2008-05-23 22:08:48 +0000

    Factor out rpython-only pieces of struct module

--- pypy/rlib/rstruct/__init__.py
+++ pypy/rlib/rstruct/__init__.py
@@ -1,2 +1,14 @@
 
 from struct import pack, unpack
+from pypy.rpython.extregistry import ExtRegistryEntry
+
+class UnpackEntry(ExtRegistryEntry):
+    _about_ = unpack
+
+    def compute_result_annotation(self, s_fmt, s_s):
+        from pypy.annotation import model as annmodel
+        if not isinstance(s_s, annmodel.SomeString):
+            raise TypeError("Got %s, string expected" % (s_s,))
+        if not s_fmt.is_constant():
+            raise ValueError("Can only use struct.unpack with first argument constant in RPython")
+        
