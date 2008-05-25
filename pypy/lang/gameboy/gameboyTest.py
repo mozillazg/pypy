9d8b4cb6078a46d26a6a5db36ec5b973773dcf26
Camillo Bruni <cami@codespeak.net>
2008-05-25 17:43:33 +0000

    test setup to run roms by commandline arguments

---
+++ pypy/lang/gameboy/gameboyTest.py
@@ -0,0 +1,23 @@
+from pypy.lang.gameboy.gameboyImplementation import *
+import sys
+
+from AppKit import NSApplication
+NSApplication.sharedApplication()
+
+filename = ""
+if len(sys.argv) > 1:
+    print sys.argv
+    filename = sys.argv[1]
+else:
+    pos = str(8)
+    filename = ROM_PATH+"/rom"+pos+"/rom"+pos+".gb"
+print "loading rom: ", str(filename)
+gameBoy = GameBoyImplementation()
+try:
+    gameBoy.load_cartridge_file(str(filename))
+except:
+    print "Cartridge is Corrupted!"
+
+gameBoy.load_cartridge_file(str(filename), verify=False)
+gameBoy.mainLoop()
+#pdb.runcall(gameBoy.mainLoop)
