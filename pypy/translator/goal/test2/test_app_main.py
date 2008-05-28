6b95fdb457cbc0064da084b8953bd5a467a94274
Armin Rigo <arigo@tunes.org>
2008-05-28 20:12:21 +0000

    issue372 testing
    When stdout is not a tty, use full buffering instead of
    line buffering.

--- pypy/translator/goal/test2/test_app_main.py
+++ pypy/translator/goal/test2/test_app_main.py
@@ -418,3 +418,22 @@ class TestNonInteractive:
                                  expect_prompt=True, expect_banner=False)
         assert 'hello world\n' in data
         assert '42\n' in data
+
+    def test_non_interactive_stdout_fully_buffered(self):
+        path = getscript(r"""
+            import sys, time
+            sys.stdout.write('\x00(STDOUT)\n\x00')   # stays in buffers
+            time.sleep(1)
+            sys.stderr.write('\x00[STDERR]\n\x00')
+            time.sleep(1)
+            # stdout flushed automatically here
+            """)
+        cmdline = '%s -u "%s" %s' % (sys.executable, app_main, path)
+        print 'POPEN:', cmdline
+        child_in, child_out_err = os.popen4(cmdline)
+        data = child_out_err.read(11)
+        assert data == '\x00[STDERR]\n\x00'    # from stderr
+        child_in.close()
+        data = child_out_err.read(11)
+        assert data == '\x00(STDOUT)\n\x00'    # from stdout
+        child_out_err.close()
