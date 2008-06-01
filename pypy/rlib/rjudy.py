2fca5688f6cd2f3e4347ecf9db013e5e3ba26b3c
Maciej Fijalkowski <fijall@gmail.com>
2008-06-01 15:32:12 +0000

    Add a sample rjudy implementation start.

---
+++ pypy/rlib/rjudy.py
@@ -0,0 +1,23 @@
+
+from pypy.rpython.extregistry import ExtRegistryEntry
+from pypy.annotation import model as annmodel
+
+class JudyTree(dict):
+    pass
+
+class SomeJudyTree(annmodel.SomeObject):
+    def rtyper_makerepr(self, rtyper):
+        from pypy.rpython.rjudy import JudyRepr
+        return JudyRepr(rtyper)
+
+class JudyTreeEntry(ExtRegistryEntry):
+    """ This registers JudyTree to be special-treated by a translation
+    toolchain
+    """
+    _about_ = JudyTree
+
+    def compute_result_annotation(self):
+        return SomeJudyTree()
+
+    def specialize_call(self, hop):
+        return hop.r_result.rtype_new(hop)
