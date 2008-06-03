51b62ef703eae20891937fcd74189e2e45d0f4aa
Antonio Cuni <anto.cuni@gmail.com>
2008-06-03 15:16:27 +0000

    another test that passes

--- pypy/jit/rainbow/test/test_promotion.py
+++ pypy/jit/rainbow/test/test_promotion.py
@@ -477,12 +477,6 @@ class TestOOType(OOTypeMixin, BaseTestPromotion):
     def skip(self):
         py.test.skip('in progress')
 
-    #test_promote_after_call = skip
-    #test_promote_after_yellow_call = skip
-    #test_merge_then_promote = skip
-    #test_vstruct_unfreeze = skip
-    #test_more_promotes = skip
     test_remembers_across_mp = skip
-    test_virtual_list_copy = skip
     test_raise_result_mixup = skip
     test_raise_result_mixup_some_more = skip
