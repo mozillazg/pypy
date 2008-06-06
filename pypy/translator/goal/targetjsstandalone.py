a3b9d158e8e1924be44e21e547adb9bbf6035d68
Maciej Fijalkowski <fijall@gmail.com>
2008-06-06 23:41:08 +0000

    Adapt a bit target to work with -f

--- pypy/translator/goal/targetjsstandalone.py
+++ pypy/translator/goal/targetjsstandalone.py
@@ -1,5 +1,7 @@
 """
 A simple standalone target for the javascript interpreter.
+
+Usage: js-c [-f] jsourcefile [-f] other_source ...
 """
 
 import sys
@@ -7,17 +9,29 @@ from pypy.lang.js.interpreter import *
 
 # __________  Entry point  __________
 
+def run_file(interp, name):
+    t = load_file(name)
+    interp.run(t)    
 
 def entry_point(argv):
-    if len(argv) == 2:
-        t = load_file(argv[1])
-        interp = Interpreter()
-        interp.run(t)
-        return 0
-    else:
-        print "Usage: %s jsourcefile" % argv[0]
-        return 1
-
+    i = 1
+    interp = Interpreter()
+    while i < len(argv):
+        arg = argv[i]
+        if arg == '-f':
+            if i == len(argv) - 1:
+                print __doc__
+                return 1
+            i += 1
+            run_file(interp, argv[i])
+        elif arg.startswith('-'):
+            print "Unsupported option %s" % arg
+            print __doc__
+            return 1
+        else:
+            run_file(interp, argv[i])
+        i += 1
+    return 0
 # _____ Define and setup target ___
 
 def target(driver, args):
