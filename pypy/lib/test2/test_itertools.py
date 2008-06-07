36cb79c211808561fe5005c22707a4058cd4572f
Antonio Cuni <anto.cuni@gmail.com>
2008-06-07 10:04:01 +0000

    bad anto, this test was completely buggy

--- pypy/lib/test2/test_itertools.py
+++ pypy/lib/test2/test_itertools.py
@@ -5,7 +5,7 @@ class AppTestItertools:
         cls.space = gettestobjspace()
         cls.w_itertools = cls.space.appexec([], "(): import itertools; return itertools")
 
-    def test_chain():
-        it = itertools.chain([], [1, 2, 3])
+    def test_chain(self):
+        it = self.itertools.chain([], [1, 2, 3])
         lst = list(it)
         assert lst == [1, 2, 3]
