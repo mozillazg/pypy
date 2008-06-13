856734d263bc4e70ef9ff5122eb5beae7e962cee
Antonio Cuni <anto.cuni@gmail.com>
2008-06-13 15:15:43 +0000

    port test_tlr to ootype

--- pypy/jit/rainbow/test/test_tlr.py
+++ pypy/jit/rainbow/test/test_tlr.py
@@ -10,19 +10,24 @@ class BaseTestTLR(PortalTest):
 
     def test_tlr(self):
         bytecode = ','.join([str(ord(c)) for c in tlr.SQUARE])
-        tlr_interpret = func_with_new_name(tlr.interpret, "tlr_interpret")
         to_rstr = self.to_rstr
-        # to stick attributes on the new function object, not on tlr.interpret
+
         def build_bytecode(s):
             result = ''.join([chr(int(t)) for t in s.split(',')])
             return to_rstr(result)
-        tlr_interpret.convert_arguments = [build_bytecode, int]
 
-        res = self.timeshift_from_portal(tlr_interpret, tlr_interpret, [bytecode, 1764],
+        def ll_function(llbytecode, arg):
+            from pypy.rpython.annlowlevel import hlstr
+            # needed so that both lltype and ootype can index the string with []
+            bytecode = hlstr(llbytecode)
+            return tlr.interpret(bytecode, arg)
+        ll_function.convert_arguments = [build_bytecode, int]
+
+        res = self.timeshift_from_portal(ll_function, tlr.interpret, [bytecode, 1764],
                              policy=P_OOPSPEC)
         assert res == 3111696
 
-        res = self.timeshift_from_portal(tlr_interpret, tlr_interpret, [bytecode, 9],
+        res = self.timeshift_from_portal(ll_function, tlr.interpret, [bytecode, 9],
                              policy=P_OOPSPEC)
         assert res == 81
 
@@ -30,6 +35,6 @@ class TestLLType(BaseTestTLR):
     type_system = "lltype"
     to_rstr = staticmethod(LLSupport.to_rstr)
 
-##class TestOOType(BaseTestTLR):
-##    type_system = "ootype"
-##    to_rstr = staticmethod(OOSupport.to_rstr)
+class TestOOType(BaseTestTLR):
+    type_system = "ootype"
+    to_rstr = staticmethod(OOSupport.to_rstr)
