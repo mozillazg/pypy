9e1f01e9aeb142d5e15e6d41134d42325d140b1d
Maciej Fijalkowski <fijall@gmail.com>
2008-06-15 20:01:36 +0000

    Add some support for setting restype

--- pypy/module/_ctypes/interp_func.py
+++ pypy/module/_ctypes/interp_func.py
@@ -13,12 +13,14 @@ class W_FuncPtr(Wrappable):
         self.lib = lib
         self.name = name
         # we create initial handle with no args, returning int
-        self.handle = self._gethandle([], 'i')
+        self.restype = 'i'
+        self.argtypes = []
+        self._regen_handle()
 
-    def _gethandle(self, args, res):
-        args = [TYPEMAP[i] for i in args]
-        res = TYPEMAP[res]
-        return self.lib.getpointer(self.name, args, res)
+    def _regen_handle(self):
+        args = [TYPEMAP[i] for i in self.argtypes]
+        res = TYPEMAP[self.restype]
+        self.handle = self.lib.getpointer(self.name, args, res)
 
     def call(self, space, args):
         """ Calling routine - note that we cache handle to ll
@@ -28,7 +30,28 @@ class W_FuncPtr(Wrappable):
         return space.wrap(self.handle.call(lltype.Signed))
     call.unwrap_spec = ['self', ObjSpace, Arguments]
 
+    def getargtypes(space, self):
+        xxx
+
+    def setargtypes(space, self, w_value):
+        xxx
+
+    def getrestype(space, self):
+        return self.w_restype
+
+    def setrestype(space, self, w_value):
+        # XXX isinstance check here
+        restype = space.str_w(space.getattr(w_value, space.wrap('_type_')))
+        if len(restype) != 1 or restype[0] not in TYPEMAP:
+            raise OperationError(space.w_TypeError, space.wrap(
+                "Unknown type %d" % restype))
+        self.w_restype = w_value
+        self.restype = restype[0]
+        self._regen_handle()
+
 W_FuncPtr.typedef = TypeDef(
     '_FuncPtr',
     __call__ = interp2app(W_FuncPtr.call),
+    restype = GetSetProperty(W_FuncPtr.getrestype, W_FuncPtr.setrestype),
+    argtypes = GetSetProperty(W_FuncPtr.getargtypes, W_FuncPtr.setargtypes),
 )
--- pypy/module/_ctypes/test/test_basic.py
+++ pypy/module/_ctypes/test/test_basic.py
@@ -3,7 +3,13 @@ from pypy.module._ctypes.test.support import BasicAppTest
 
 class AppTestBasic(BasicAppTest):
     def test_int_base(self):
-        from _ctypes import CDLL
-        dll = CDLL(self.so_file)
-        assert dll.get_an_integer() == 42
+        assert self.dll.get_an_integer() == 42
 
+    def test_restype(self):
+        from _ctypes import _SimpleCData
+        class c_int(_SimpleCData):
+            _type_ = 'i'
+
+        f = self.dll.get_an_integer
+        f.restype = c_int
+        assert f() == 42
