646cbc90fa6d6056f0d86a9514260f332ab3d0d3
Armin Rigo <arigo@tunes.org>
2008-06-16 10:37:37 +0000

    Attempt to skip the rsdl tests if the sdl is not installed.

--- pypy/rlib/rsdl/RSDL.py
+++ pypy/rlib/rsdl/RSDL.py
@@ -1,35 +1,14 @@
 from pypy.rpython.lltypesystem import lltype, rffi
 from pypy.rpython.tool import rffi_platform as platform
-from pypy.translator.tool.cbuild import ExternalCompilationInfo
 from pypy.rlib.rsdl.constants import _constants
+from pypy.rlib.rsdl.eci import get_rsdl_compilation_info
 from pypy.rlib.objectmodel import we_are_translated
 import py
 import sys
 
-if sys.platform == 'darwin':
-    eci = ExternalCompilationInfo(
-        includes = ['SDL.h', 
-                    #'Init.h',
-                    #'SDLMain.m'
-                    #'SDLMain.h'*/
-                    ],
-        include_dirs = ['/Library/Frameworks/SDL.framework/Versions/A/Headers',
-                        #str(py.magic.autopath().dirpath().join('macosx-sdl-main'))
-                        ],
-        link_extra = [
-            str(py.magic.autopath().dirpath().join('macosx-sdl-main/SDLMain.m')),
-            #'macosx-sdl-main/SDLMain.m',
-            '-I', '/Library/Frameworks/SDL.framework/Versions/A/Headers',
-        ],
-        frameworks = ['SDL', 'Cocoa']
-    )
-else:
-    eci = ExternalCompilationInfo(
-        includes=['SDL.h'],
-        )
-    eci = eci.merge(ExternalCompilationInfo.from_config_tool('sdl-config'))
-
 # ------------------------------------------------------------------------------
+
+eci = get_rsdl_compilation_info()
 
 def external(name, args, result):
     return rffi.llexternal(name, args, result, compilation_info=eci)
