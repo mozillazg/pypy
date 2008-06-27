859186d76d550a803ab1ea952afdf0be7070d4c6
Maciej Fijalkowski <fijall@gmail.com>
2008-06-27 18:09:30 +0000

    Potentialy questionable commit. Implement gc.enable and gc.disable as enable/disable finalizers. It fits in one of two use cases, the other one is for performance reasons, where it does not harm.

--- pypy/module/gc/app_gc.py
+++ pypy/module/gc/app_gc.py
@@ -1,9 +1,17 @@
 
+enabled = [True]
+
 def isenabled():
-    return True
+    return enabled[0]
 
 def enable():
-    "Not implemented."
+    import gc
+    if not enabled[0]:
+        gc.enable_finalizers()
+        enabled[0] = True
 
 def disable():
-    "Not implemented."
+    import gc
+    if enabled[0]:
+        gc.disable_finalizers()
+        enabled[0] = False
