c2bb0d301d7ebbc6511b201491793b6ee474af77
Armin Rigo <arigo@tunes.org>
2008-07-03 12:34:40 +0000

    A skipped test that shows an obscure failure triggered by the builtinshortcut logic :-(

--- pypy/objspace/std/test/test_builtinshortcut.py
+++ pypy/objspace/std/test/test_builtinshortcut.py
@@ -1,7 +1,23 @@
 from pypy.objspace.std.test import test_userobject
 
+WITH_BUILTINSHORTCUT = {'objspace.std.builtinshortcut': True}
+
 class AppTestUserObject(test_userobject.AppTestUserObject):
-    OPTIONS = {'objspace.std.builtinshortcut': True}
+    OPTIONS = WITH_BUILTINSHORTCUT
 
 class AppTestWithMultiMethodVersion2(test_userobject.AppTestWithMultiMethodVersion2):
-    OPTIONS = {'objspace.std.builtinshortcut': True}
+    OPTIONS = WITH_BUILTINSHORTCUT
+
+class AppTestBug:
+    def setup_class(cls):
+        from pypy import conftest
+        cls.space = conftest.gettestobjspace(**WITH_BUILTINSHORTCUT)
+
+    def test_frozen_subtype(self):
+        skip("in-progress")
+        class S(set): pass
+        assert S("abc") == set("abc")
+        assert set("abc") == S("abc")
+        class F(frozenset): pass
+        assert F("abc") == frozenset("abc")
+        assert frozenset("abc") == F("abc")
