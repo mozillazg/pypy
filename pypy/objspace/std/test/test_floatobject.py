cd8e9f94323780e1ed7495863af0f7211f25de76
Armin Rigo <arigo@tunes.org>
2008-07-12 16:45:54 +0000

    issue384 in-progress
    (pedronis, arigo) A test that fails if
    declare_new_int_float_comparison() is enabled.

--- pypy/objspace/std/test/test_floatobject.py
+++ pypy/objspace/std/test/test_floatobject.py
@@ -184,3 +184,10 @@ class AppTestAppFloatTest:
         assert 13 < 13.01
         assert 13 <= 13.0
         assert 13 <= 13.01
+
+    def test_multimethod_slice(self):
+        assert 5 .__add__(3.14) is NotImplemented
+        assert 3.25 .__add__(5) == 8.25
+        if hasattr(int, '__eq__'):  # for py.test -A: CPython is inconsistent
+            assert 5 .__eq__(3.14) is NotImplemented
+            assert 3.14 .__eq__(5) is False
