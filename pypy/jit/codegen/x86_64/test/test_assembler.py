4786708d8c7b1a2cfc8a944925f801389f3fdfe9
John Witulski <witulski@codespeak.net>
2008-07-31 12:18:11 +0000

    (cfBolz, witulski)
    Implemented encoding of ADD instruction for 64 bit registers. Needs to be refactored later.

---
+++ pypy/jit/codegen/x86_64/test/test_assembler.py
@@ -0,0 +1,16 @@
+from pypy.jit.codegen.x86_64 import assembler
+
+class AsmTest(assembler.X86_64CodeBuilder):
+    def __init__(self):
+        self.data = []
+
+    def get_as_string(self):
+        return "".join(self.data)
+
+    def write(self,char):
+        self.data.append(char)
+
+def test_add():
+    mc = AsmTest()
+    mc.ADD("rax", "r11")
+    assert mc.get_as_string() == "\x49\x00\xC3"
