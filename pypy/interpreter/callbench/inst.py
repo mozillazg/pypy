f143200b34aab4d7f2fd7b18dc689ab8825deb3b
Samuele Pedroni <pedronis@lucediurna.net>
2008-08-01 23:40:12 +0000

    benchmark the case of some actual parameters being passed around

--- pypy/interpreter/callbench/inst.py
+++ pypy/interpreter/callbench/inst.py
@@ -5,6 +5,10 @@ def w(N, start):
         def __init__(self):
             pass
 
+    class B(object):
+        def __init__(self, x, y):
+            pass
+
     start()
     i = 0
     while i < N:
@@ -13,10 +17,10 @@ def w(N, start):
         A()
         A()
         A()
-        A()
-        A()
-        A()        
-        A()
+        B(1, 2)
+        B(1, 2)
+        B(1, 2)   
+        B(1, 2)
         i+=1
     
 run(w, 1000)
