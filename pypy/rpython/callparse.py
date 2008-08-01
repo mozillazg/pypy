868483548a62722aaa2735187de03d3c56db6041
Samuele Pedroni <pedronis@lucediurna.net>
2008-08-01 17:36:36 +0000

    a new branch to try to simplify our call code logic, maybe even speed things up a bit

--- pypy/rpython/callparse.py
+++ pypy/rpython/callparse.py
@@ -176,7 +176,7 @@ class RPythonCallsSpace:
             if (expected_length is not None and
                 expected_length != len(items)):
                 raise ValueError
-            return list(items)
+            return items
         raise CallPatternTooComplex, "'*' argument must be a tuple"
 
     def is_w(self, one, other):
