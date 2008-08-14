738359dee88e2a2b83db0a332aa52b0d94869fe5
Carl Friedrich Bolz <cfbolz@gmx.de>
2008-08-14 11:04:20 +0000

    some cosmetic stuff

--- pypy/jit/codegen/x86_64/objmodel.py
+++ pypy/jit/codegen/x86_64/objmodel.py
@@ -2,10 +2,12 @@ from pypy.jit.codegen import model
 # Wrapper Classes
 
 class Register64(model.GenVar):
+    _dispatchname = "_QWREG"
     def __init__(self, reg):
         self.reg = reg
 
 # TODO: support 64-bit Constants
 class Constant32(model.GenConst):
+    _dispatchname = "_IMM32"
     def __init__(self, value):
         self.value = value
