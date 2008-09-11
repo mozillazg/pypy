6133b6f4d4f44331f83f8446e95e8513f51d694e
Camillo Bruni <cami@codespeak.net>
2008-09-11 22:08:30 +0000

    refactored video adapted tests

--- pypy/lang/gameboy/test/test_video_registers.py
+++ pypy/lang/gameboy/test/test_video_registers.py
@@ -1,14 +1,23 @@
 from pypy.lang.gameboy import constants
 from pypy.lang.gameboy.video import ControlRegister
 from pypy.lang.gameboy.video import StatusRegister
+from pypy.lang.gameboy.video import Window
+from pypy.lang.gameboy.video import Background
 from pypy.lang.gameboy.test.test_video import get_video
 import py
 
 
+def get_control_register():
+    video = get_video()
+    return ControlRegister(Window(video), Background())
+
+def get_status_register():
+    return StatusRegister(get_video())
+
 # ControlRegister --------------------------------------------------------------
 
 def test_video_control_reset():
-    control = ControlRegister()
+    control = get_control_register()
     assert control.read() == 0x91
     control.write(0xFF)
     assert control.read() == 0xFF
@@ -17,42 +26,17 @@ def test_video_control_reset():
     
     
 def test_video_control_read_write_properties():
-    control   = ControlRegister()
-    properties = ["lcd_enabled",  
-                  "window_upper_tile_map_selected", 
-                  "window_enabled", 
-                  "background_and_window_lower_tile_data_selected",
-                  "background_upper_tile_map_selected",
-                  "big_sprite_size_selected",
-                  "sprite_display_enabled",
-                  "background_enabled"]
-    properties.reverse()
-    for index in range(8):
-        property = properties[index];
-        control.write(0x00)
-        assert control.read() == 0x00
-        assert control.__getattribute__(property) == False
-        
-        control.write(0xFF)
-        assert control.read() == 0xFF
-        assert control.__getattribute__(property) == True
-        
-        control.write(0x00)
-        control.__setattr__(property, True)
-        assert control.__getattribute__(property) == True
-        assert control.read() & (1 << index) == (1 << index)
-        assert control.read() & (~(1 << index)) == 0
-        
-        control.write(1 << index)
-        assert control.__getattribute__(property) == True
-        assert control.read() & (1 << index) == (1 << index)
-        assert control.read() & (~(1 << index)) == 0
+    control   = get_control_register()
+    
+    for i in range(0xFF):
+        control.write(i)
+        assert control.read() == i
         
         
 # StatusRegister ---------------------------------------------------------------
 
 def test_video_status_reset():
-    status = StatusRegister(get_video())
+    status = get_status_register()
     assert status.read(extend=True) == 0x02 + 0x80
     
     status.write(0x00, write_all=True)
@@ -66,7 +50,7 @@ def test_video_status_reset():
     assert status.read(extend=True) == 0x02 + 0x80
     
 def test_video_status_mode():
-    status = StatusRegister(get_video())
+    status = get_status_register()
     assert status.get_mode() == 2
     
     for i in range(3):
