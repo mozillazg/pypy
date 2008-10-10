bb4247325cef507c3188fc3b1eade5fad3d7dfc4
Maciej Fijalkowski <fijall@gmail.com>
2008-10-10 13:28:39 +0000

    (pedronis, fijal) Nice error messages

--- pypy/translator/platform/test/test_platform.py
+++ pypy/translator/platform/test/test_platform.py
@@ -1,10 +1,11 @@
 
+import py
 from pypy.tool.udir import udir
-from pypy.translator.platform import host
+from pypy.translator.platform import host, CompilationError
 
 def test_simple_enough():
     cfile = udir.join('test_simple_enough.c')
-    tmpdir = cfile.write('''
+    cfile.write('''
     #include <stdio.h>
     int main()
     {
@@ -17,3 +18,15 @@ def test_simple_enough():
     assert res.out == '42\n'
     assert res.err == ''
     assert res.returncode == 0
+
+def test_nice_errors():
+    cfile = udir.join('test_nice_errors.c')
+    cfile.write('')
+    try:
+        executable = host.compile([cfile], None)
+    except CompilationError, e:
+        filename = cfile.dirpath().join(cfile.purebasename + '.errors')
+        assert filename.read() == e.err
+    else:
+        py.test.fail("Did not raise")
+    
