fa612ac88ab3ccaabc08c7a0ad333fbb51301a36
Maciej Fijalkowski <fijall@gmail.com>
2008-10-10 13:28:39 +0000

    (pedronis, fijal) Nice error messages

--- pypy/translator/platform/linux.py
+++ pypy/translator/platform/linux.py
@@ -3,6 +3,11 @@ import py
 from pypy.translator.platform import Platform, CompilationError, ExecutionResult
 from subprocess import PIPE, Popen
 
+import py
+from pypy.tool.ansi_print import ansi_log
+log = py.log.Producer("cbuild")
+py.log.setconsumer("cbuild", ansi_log)
+
 def _run_subprocess(args):
     pipe = Popen(args, executable=args[0],
                  stdout=PIPE, stderr=PIPE, shell=False)
@@ -19,8 +24,16 @@ class Linux(Platform):
         args = [self.cc] + [str(f) for f in cfiles]
         exe_name = cfiles[0].dirpath().join(cfiles[0].purebasename)
         args += ['-o', str(exe_name)]
+        log.execute(' '.join(args))
         returncode, stdout, stderr = _run_subprocess(args)
         if returncode != 0:
+            errorfile = exe_name.new(ext='errors')
+            errorfile.write(stderr)
+            stderrlines = stderr.splitlines()
+            for line in stderrlines[:5]:
+                log.ERROR(line)
+            if len(stderrlines) > 5:
+                log.ERROR('...')
             raise CompilationError(stdout, stderr)
         return exe_name
 
