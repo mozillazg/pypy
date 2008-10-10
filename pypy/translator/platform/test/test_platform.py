3b3cd4e4915973b118281edac135e88def30abce
Maciej Fijalkowski <fijall@gmail.com>
2008-10-10 13:41:59 +0000

    (pedronis, fijal) Start supporting eci

--- pypy/translator/platform/test/test_platform.py
+++ pypy/translator/platform/test/test_platform.py
@@ -2,6 +2,7 @@
 import py
 from pypy.tool.udir import udir
 from pypy.translator.platform import host, CompilationError
+from pypy.translator.tool.cbuild import ExternalCompilationInfo
 
 def test_simple_enough():
     cfile = udir.join('test_simple_enough.c')
@@ -13,7 +14,7 @@ def test_simple_enough():
         return 0;
     }
     ''')
-    executable = host.compile([cfile], None)
+    executable = host.compile([cfile], ExternalCompilationInfo())
     res = host.execute(executable)
     assert res.out == '42\n'
     assert res.err == ''
@@ -23,10 +24,30 @@ def test_nice_errors():
     cfile = udir.join('test_nice_errors.c')
     cfile.write('')
     try:
-        executable = host.compile([cfile], None)
+        executable = host.compile([cfile], ExternalCompilationInfo())
     except CompilationError, e:
         filename = cfile.dirpath().join(cfile.purebasename + '.errors')
         assert filename.read() == e.err
     else:
         py.test.fail("Did not raise")
     
+def test_use_eci():
+    tmpdir = udir.join('use_eci').ensure(dir=1)
+    hfile = tmpdir.join('needed.h')
+    hfile.write('#define SOMEHASHDEFINE 42\n')
+    eci = ExternalCompilationInfo(include_dirs=[tmpdir])
+    cfile = udir.join('use_eci_c.c')
+    cfile.write('''
+    #include <stdio.h>
+    #include "needed.h"
+    int main()
+    {
+        printf("%d\\n", SOMEHASHDEFINE);
+        return 0;
+    }
+    ''')
+    executable = host.compile([cfile], eci)
+    res = host.execute(executable)
+    assert res.out == '42\n'
+    assert res.err == ''
+    assert res.returncode == 0
