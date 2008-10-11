5bdb50ed3e3415fdd47f7422203055b8d9d4f5ae
Carl Friedrich Bolz <cfbolz@gmx.de>
2008-10-11 15:08:47 +0000

    (iko, cfbolz): Fix bugs in compression.

--- pypy/module/unicodedata/test/test_compression.py
+++ pypy/module/unicodedata/test/test_compression.py
@@ -5,14 +5,25 @@ from pypy.module.unicodedata import compression
 thisfile = py.magic.autopath()
 data = thisfile.read()
 lines = data.splitlines()
-codetable, codelist = compression.build_compression_table(lines)
-
-def test_tables_sanity():
-    for key, value in codetable.items():
-        assert codelist[value] == key
+codelist = compression.build_compression_table(lines)
 
 def test_roundtrip():
     for line in lines:
-        compressed = compression.compress(codetable, line)
+        compressed = compression.compress(codelist, line)
         decompressed = compression.uncompress(codelist, compressed)
         assert decompressed == line
+
+def test_simple():
+    names = ['abe', 'abcde', 'abd', 'abab']
+    codelist = compression.build_compression_table(names)
+    print codelist
+    for name in names:
+        compressed = compression.compress(codelist, name)
+        decompressed = compression.uncompress(codelist, compressed)
+        assert decompressed == name
+
+def test_simple2():
+    names = ['abc', 'abc', 'abc']
+    codelist = compression.build_compression_table(names)
+    assert set(codelist) == set(['abc', 'c', 'b', 'a'])
+    print codelist
