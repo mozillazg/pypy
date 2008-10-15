268361a90d070b3ab99d6cd5d896d96f86082719
Maciej Fijalkowski <fijall@gmail.com>
2008-10-15 22:08:11 +0000

    Hack differently. Invoke make from inside scratchbox. Looks a bit cleaner IMO

--- pypy/translator/platform/maemo.py
+++ pypy/translator/platform/maemo.py
@@ -76,54 +76,11 @@ class Maemo(Linux):
         # on the other hand, library lands in usual place...
         return []
 
-    
-    def gen_makefile(self, cfiles, eci, exe_name=None, path=None):
-        cfiles = [py.path.local(f) for f in cfiles]
-        cfiles += [py.path.local(f) for f in eci.separate_module_files]
-
-        if path is None:
-            path = cfiles[0].dirpath()
-
-        pypypath = py.path.local(autopath.pypydir)
-
-        if exe_name is None:
-            exe_name = cfiles[0].new(ext='')
-
-        m = GnuMakefile(path)
-        m.exe_name = exe_name
-        m.eci = eci
-
-        rel_cfiles = [str(cfile) for cfile in cfiles]
-        rel_ofiles = [rel_cfile[:-2]+'.o' for rel_cfile in rel_cfiles]
-        m.cfiles = rel_cfiles
-
-        rel_includedirs = [str(incldir) for incldir in
-                           self._preprocess_dirs(eci.include_dirs)]
-
-        m.comment('automatically generated makefile')
-        definitions = [
-            ('TARGET', str(exe_name)),
-            ('DEFAULT_TARGET', '$(TARGET)'),
-            ('SOURCES', rel_cfiles),
-            ('OBJECTS', rel_ofiles),
-            ('LIBS', self._libs(eci.libraries)),
-            ('LIBDIRS', self._libdirs(eci.library_dirs)),
-            ('INCLUDEDIRS', self._includedirs(rel_includedirs)),
-            ('CFLAGS', self.cflags + list(eci.compile_extra)),
-            ('LDFLAGS', self.link_flags + list(eci.link_extra)),
-            ('CC', '/scratchbox/login ' + self.cc)
-            ]
-        for args in definitions:
-            m.definition(*args)
-
-        rules = [
-            ('all', '$(DEFAULT_TARGET)', []),
-            ('$(TARGET)', '$(OBJECTS)', '$(CC) $(LDFLAGS) -o $@ $(OBJECTS) $(LIBDIRS) $(LIBS)'),
-            ('%.o', '%.c', '$(CC) $(CFLAGS) -o $@ -c $< $(INCLUDEDIRS)'),
-            ]
-
-        for rule in rules:
-            m.rule(*rule)
-
-        return m
-
+    def execute_makefile(self, path_to_makefile):
+        if isinstance(path_to_makefile, GnuMakefile):
+            path = path_to_makefile.makefile_dir
+        else:
+            path = path_to_makefile
+        log.execute('make in %s' % (path,))
+        returncode, stdout, stderr = _run_subprocess('/scratchbox/login', ['make', '-C', str(path)])
+        self._handle_error(returncode, stdout, stderr, path.join('make'))
