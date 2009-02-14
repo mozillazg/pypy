fdfe2b26d5bcf2fb6cd35673ffd5acbacd490dd8
Maciej Fijalkowski <fijall@gmail.com>
2009-02-14 09:22:42 +0000

    A test and a fix

--- pypy/jit/metainterp/test/test_dlist.py
+++ pypy/jit/metainterp/test/test_dlist.py
@@ -17,10 +17,26 @@ class ListTests:
             return l[0]
 
         res = self.meta_interp(f, [10])
-        assert res == f(10)
-        
+        assert res == f(10)        
         self.check_loops(getitem=0, setitem=1, guard_exception=0,
-                         guard_no_exception=0)                
+                         guard_no_exception=0)
+
+    def test_list_escapes(self):
+        myjitdriver = JitDriver(greens = [], reds = ['n', 'l'])
+        def f(n):
+            l = [0] * (n + 1)
+            while n > 0:
+                myjitdriver.can_enter_jit(n=n, l=l)
+                myjitdriver.jit_merge_point(n=n, l=l)
+                x = l[0]
+                l[0] = x + 1
+                l[n] = n
+                n -= 1
+            return l[3]
+
+        res = self.meta_interp(f, [10])
+        assert res == f(10)
+        self.check_loops(setitem=2)
 
 class TestLLtype(ListTests, LLJitMixin):
     pass
