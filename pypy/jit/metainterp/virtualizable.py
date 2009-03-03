d74c998a615d861327c7cc8f013ffe3889ef70d6
Maciej Fijalkowski <fijall@gmail.com>
2009-03-03 15:44:13 +0000

    allow for searching also in parent structures. I *hope* this is the correct thing to do

--- pypy/jit/metainterp/virtualizable.py
+++ pypy/jit/metainterp/virtualizable.py
@@ -5,12 +5,22 @@ from pypy.rpython.annlowlevel import llhelper, cast_base_ptr_to_instance
 from pypy.annotation.model import lltype_to_annotation
 from pypy.rlib.objectmodel import we_are_translated
 
+def get_field(cpu, STRUCT, fieldname):
+    s = STRUCT
+    while True:
+        if fieldname in s._flds:
+            return cpu.fielddescrof(s, fieldname)
+        if 'super' in s._flds:
+            s = s.super
+        else:
+            raise AttributeError("%r has no attribute %s" % (STRUCT, fieldname))
+
 class VirtualizableDesc(history.AbstractValue):
     hash = 0
 
     def __init__(self, cpu, TOPSTRUCT):
         "NOT_RPYTHON"
-        self.virtuals     = [cpu.fielddescrof(TOPSTRUCT, 'inst_' + name) for
+        self.virtuals     = [get_field(cpu, TOPSTRUCT, 'inst_' + name) for
                              name in TOPSTRUCT._hints['virtuals']]
         initialize_virtualizable(cpu, TOPSTRUCT.access)
         self.vable_base = cpu.fielddescrof(TOPSTRUCT, 'vable_base')
