2c8aa075efc22bc6d8b81773f0e1ccae23be7672
Maciej Fijalkowski <fijall@gmail.com>
2009-03-05 14:53:44 +0000

    example of bridge. working so far

--- pypy/jit/tl/pypyjit_demo.py
+++ pypy/jit/tl/pypyjit_demo.py
@@ -5,7 +5,10 @@ def f():
     i = 0
     while i < 100:
         # XXX implement inplace_add method for ints
-        i += 3
+        if i % 2:
+            i = i + 3
+        else:
+            i = i + 1
     print i        # should print 102
 
 f()
