b37c052cd295efd802c4b7fa30bd1e6f73abb9f6
Maciej Fijalkowski <fijall@gmail.com>
2009-03-05 17:18:37 +0000

    Ok, let's start to collect functions that were tried at least

--- pypy/jit/tl/pypyjit_demo.py
+++ pypy/jit/tl/pypyjit_demo.py
@@ -1,14 +1,27 @@
 
-def f():
-    print 543210
+def f0():
+    print "simple loop"
 
     i = 0
     while i < 100:
+        i = i + 3
+    print i
+
+def f1():
+    print "simple loop with inplace_add"
+
+    i = 0
+    while i < 100:
+        i += 3
+    print i
+
+def f():
+    print 543210
+
+    s = 0
+    for i in range(100):
         # XXX implement inplace_add method for ints
-        if i % 2:
-            i = i + 3
-        else:
-            i = i + 1
-    print i        # should print 102
+        s += i
+    print s        # should print 102
 
 f()
