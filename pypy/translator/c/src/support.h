f3fc5eaff9f119591f7579550b0a628876d71341
Armin Rigo <arigo@tunes.org>
2009-03-13 12:12:41 +0000

    Port r60900 and r61877 from the oo-jit branch:
        Fix debug_print of strings, broken now that the RPython strings
        are no longer zero-terminated.  Requires copying the RPython
        strings into a non-movable, zero-terminated buffer.

--- pypy/translator/c/src/support.h
+++ pypy/translator/c/src/support.h
@@ -20,15 +20,15 @@
 #define FAIL_ZER(msg) FAIL_EXCEPTION(PyExc_ZeroDivisionError, msg)
 #define CFAIL()       RPyConvertExceptionFromCPython()
 
-#define PyString_FromRPyString(rpystr) \
-	PyString_FromStringAndSize(RPyString_AsString(rpystr), RPyString_Size(rpystr))
+/* #define PyString_FromRPyString(rpystr) \ */
+/* 	PyString_FromStringAndSize(RPyString_AsString(rpystr), RPyString_Size(rpystr)) */
 
-#define PyUnicode_FromRPyUnicode(rpystr) \
-	PyUnicode_FromUnicode(RPyUnicode_AsUnicode(rpystr), RPyUnicode_Size(rpystr))
+/* #define PyUnicode_FromRPyUnicode(rpystr) \ */
+/*	PyUnicode_FromUnicode(RPyUnicode_AsUnicode(rpystr), RPyUnicode_Size(rpystr)) */
 
-#define PyString_ToRPyString(s, rpystr)                           \
-	memcpy(RPyString_AsString(rpystr), PyString_AS_STRING(s), \
-               RPyString_Size(rpystr))
+/* #define PyString_ToRPyString(s, rpystr)                           \ */
+/* 	memcpy(RPyString_AsString(rpystr), PyString_AS_STRING(s), \ */
+/*                RPyString_Size(rpystr)) */
 
 /* Extra checks can be enabled with the RPY_ASSERT or RPY_LL_ASSERT
  * macros.  They differ in the level at which the tests are made.
