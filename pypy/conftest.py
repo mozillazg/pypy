176a8d0afee61ce0fd8e8ac534dce9537d67cc6c
Holger Krekel <holger.krekel@gmail.com>
2009-03-26 10:15:18 +0000

    naming change in pytest

--- pypy/conftest.py
+++ pypy/conftest.py
@@ -43,7 +43,7 @@ class PyPyTestPlugin:
                default="host", callback=_set_platform,
                help="set up tests to use specified platform as compile/run target")
 
-    def pytest_pyfuncarg_space(self, pyfuncitem):
+    def pytest_funcarg_space(self, pyfuncitem):
         return gettestobjspace()
         
 ConftestPlugin = PyPyTestPlugin
