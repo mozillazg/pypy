c80da1bfc091d6814c94af259bd4d913d8f0acdb
Samuele Pedroni <pedronis@lucediurna.net>
2009-04-19 15:36:44 +0000

    it should have been bumped here

--- pypy/translator/geninterplevel.py
+++ pypy/translator/geninterplevel.py
@@ -71,7 +71,7 @@ from pypy.tool.ansi_print import ansi_log
 log = py.log.Producer("geninterp")
 py.log.setconsumer("geninterp", ansi_log)
 
-GI_VERSION = '1.1.23'  # bump this for substantial changes
+GI_VERSION = '1.1.26'  # bump this for substantial changes
 # ____________________________________________________________
 
 try:
