98879b2fc0604b5c7d958663482318494c754342
Armin Rigo <arigo@tunes.org>
2009-04-19 11:32:28 +0000

    Add digestsize and digest_size a bit everywhere.

--- pypy/lib/app_test/test_hashlib.py
+++ pypy/lib/app_test/test_hashlib.py
@@ -2,3 +2,20 @@ from pypy.lib import hashlib, _hashlib
 
 def test_unicode():
     assert isinstance(hashlib.new('sha1', u'xxx'), _hashlib.hash)
+
+def test_attributes():
+    for name, expected_size in {'md5': 16,
+                                'sha1': 20,
+                                'sha224': 28,
+                                'sha256': 32,
+                                'sha384': 48,
+                                'sha512': 64,
+                                }.items():
+        h = hashlib.new(name)
+        assert h.digest_size == expected_size
+        assert h.digestsize == expected_size
+
+        # also test the pure Python implementation
+        h = hashlib.__get_builtin_constructor(name)('')
+        assert h.digest_size == expected_size
+        assert h.digestsize == expected_size
