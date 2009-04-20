ae314804009dabf9b59ac81f9c08ee95f4ddc19e
Armin Rigo <arigo@tunes.org>
2009-04-20 14:46:52 +0000

    on PyPy, we don't support .PY and other case-mismatching extensions, even on Windows

--- lib-python/modified-2.5.2/test/test_import.py
+++ lib-python/modified-2.5.2/test/test_import.py
@@ -74,9 +74,11 @@ def test_with_extension(ext): # ext normally ".py"; perhaps ".pyw"
 sys.path.insert(0, os.curdir)
 try:
     test_with_extension(os.extsep + "py")
-    if sys.platform.startswith("win"):
-        for ext in ".PY", ".Py", ".pY", ".pyw", ".PYW", ".pYw":
-            test_with_extension(ext)
+    #--- on PyPy, we don't support .PY and other case-mismatching
+    #--- extensions, even on Windows
+    #if sys.platform.startswith("win"):
+    #    for ext in ".PY", ".Py", ".pY", ".pyw", ".PYW", ".pYw":
+    #        test_with_extension(ext)
 finally:
     del sys.path[0]
 
