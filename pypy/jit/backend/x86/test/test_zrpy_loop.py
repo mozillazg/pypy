6c617fc71d10ee3c2952e9784a89d3307d173a72
Maciej Fijalkowski <fijall@gmail.com>
2009-04-22 16:27:45 +0000

    Add a test that showcases a problem that is silently eaten by ll2ctypes (not really silently, but one need to look deeper).
    The problem is that exceptions like DoneWithThisFrame travel through
    assembler and they really shouldn't

--- pypy/jit/backend/x86/test/test_zrpy_loop.py
+++ pypy/jit/backend/x86/test/test_zrpy_loop.py
@@ -1,10 +1,8 @@
 
-import py
-from pypy.jit.metainterp.test import test_zrpy_exception
+from pypy.jit.metainterp.test.test_loop import LoopTest
 from pypy.jit.backend.x86.test.test_zrpy_slist import Jit386Mixin
 
-class TestException(Jit386Mixin, test_zrpy_exception.TestLLExceptions):
+class TestLoop(Jit386Mixin, LoopTest):
     # for the individual tests see
     # ====> ../../../metainterp/test/test_exception.py
     pass
-
