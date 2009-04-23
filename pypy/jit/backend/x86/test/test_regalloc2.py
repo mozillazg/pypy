e9565baf269418275587009409fcd48cc0fba681
Armin Rigo <arigo@tunes.org>
2009-04-23 20:17:39 +0000

    The next failing test.

--- pypy/jit/backend/x86/test/test_regalloc2.py
+++ pypy/jit/backend/x86/test/test_regalloc2.py
@@ -1,3 +1,4 @@
+import py
 from pypy.jit.metainterp.history import ResOperation, BoxInt, ConstInt,\
      BoxPtr, ConstPtr, TreeLoop
 from pypy.jit.metainterp.resoperation import rop
@@ -21,3 +22,25 @@ def test_bug_rshift():
     cpu.execute_operations(loop, [BoxInt(9)])
     assert v4.value == (9 >> 3)
     assert v3.value == (~18)
+
+def test_bug_int_is_true_1():
+    py.test.skip('fix me')
+    v1 = BoxInt()
+    v2 = BoxInt()
+    v3 = BoxInt()
+    v4 = BoxInt()
+    tmp5 = BoxInt()
+    loop = TreeLoop('test')
+    loop.inputargs = [v1]
+    loop.operations = [
+        ResOperation(rop.INT_MUL, [v1, v1], v2),
+        ResOperation(rop.INT_MUL, [v2, v1], v3),
+        ResOperation(rop.INT_IS_TRUE, [v2], tmp5),
+        ResOperation(rop.BOOL_NOT, [tmp5], v4),
+        ResOperation(rop.FAIL, [v4, v3], None),
+            ]
+    cpu = CPU(None, None)
+    cpu.compile_operations(loop)
+    cpu.execute_operations(loop, [BoxInt(-10)])
+    assert v4.value == 0
+    assert v3.value == -1000
