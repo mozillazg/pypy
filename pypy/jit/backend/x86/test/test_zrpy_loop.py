418f01c73ef0c97d827334d404641ce50ef3bc7e
Maciej Fijalkowski <fijall@gmail.com>
2009-04-24 00:33:26 +0000

    a test and a fix.

--- pypy/jit/backend/x86/test/test_zrpy_loop.py
+++ pypy/jit/backend/x86/test/test_zrpy_loop.py
@@ -4,5 +4,5 @@ from pypy.jit.backend.x86.test.test_zrpy_slist import Jit386Mixin
 
 class TestLoop(Jit386Mixin, LoopTest):
     # for the individual tests see
-    # ====> ../../../metainterp/test/test_exception.py
+    # ====> ../../../metainterp/test/test_loop.py
     pass
