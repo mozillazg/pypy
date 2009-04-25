77700f1a79e4e92c93223a999be6b524ac80dcd4
Igor Trindade Oliveira <igorto@codespeak.net>
2009-04-25 15:29:10 +0000

    remove files

--- dotviewer/conftest.py
+++
@@ -1,8 +0,0 @@
-import py
-
-class ConftestPlugin:
-    def pytest_addoption(self, parser):
-        group = parser.addgroup("dotviever")
-        group.addoption('--pygame', action="store_true", 
-            dest="pygame", default=False, 
-            help="allow interactive tests using Pygame")
