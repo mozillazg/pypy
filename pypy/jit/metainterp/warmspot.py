8366ddc05673834215c611cb6f231a36ae4fd6a5
Armin Rigo <arigo@tunes.org>
2009-04-26 18:26:08 +0000

    Use a custom interface to set the initial values to cpu.execute_operations(), which is set_future_value_xxx(); this is similar to the custom interface to fetch the values returned by a FAIL operation, which is get_latest_value_xxx().
    This allows us to remove the call to changevalue_xxx() in warmspot.py.

--- pypy/jit/metainterp/warmspot.py
+++ pypy/jit/metainterp/warmspot.py
@@ -204,11 +204,14 @@ class WarmRunnerDesc:
         args = op.args[2:]
         ALLARGS = []
         self.green_args_spec = []
+        self.red_args_types = []
         for i, v in enumerate(args):
             TYPE = v.concretetype
             ALLARGS.append(TYPE)
             if i < len(self.jitdriver.greens):
                 self.green_args_spec.append(TYPE)
+            else:
+                self.red_args_types.append(history.getkind(TYPE)[0])
         RESTYPE = graph.getreturnvar().concretetype
         (self.JIT_ENTER_FUNCTYPE,
          self.PTR_JIT_ENTER_FUNCTYPE) = self.ts.get_FuncType(ALLARGS, lltype.Void)
@@ -442,7 +445,7 @@ def make_state_class(warmrunnerdesc):
     warmrunnerdesc.num_green_args = num_green_args
     green_args_spec = unrolling_iterable(warmrunnerdesc.green_args_spec)
     green_args_names = unrolling_iterable(jitdriver.greens)
-    red_args_index = unrolling_iterable(range(len(jitdriver.reds)))
+    red_args_types = unrolling_iterable(warmrunnerdesc.red_args_types)
     if num_green_args:
         MAX_HASH_TABLE_BITS = 28
     else:
@@ -470,24 +473,22 @@ def make_state_class(warmrunnerdesc):
                     return False
                 i = i + 1
             return True
-        def fill_boxes(self, *redargs):
-            boxes = self.bridge.inputargs
-            for j in red_args_index:
+        def set_future_values(self, cpu, *redargs):
+            j = 0
+            for typecode in red_args_types:
                 value = redargs[j]
-                box = boxes[j]
-                TYPE = lltype.typeOf(value)
-                if isinstance(TYPE, lltype.Ptr):
-                    assert isinstance(box, history.BoxPtr)
-                    box.changevalue_ptr(lltype.cast_opaque_ptr(llmemory.GCREF, value))
-                elif isinstance(TYPE, ootype.OOType):
-                    assert isinstance(box, history.BoxObj)
-                    box.changevalue_obj(ootype.cast_to_object(value))
-                elif TYPE == lltype.Signed:
-                    assert isinstance(box, history.BoxInt)
-                    box.changevalue_int(value)
+                if typecode == 'p':
+                    ptrvalue = lltype.cast_opaque_ptr(llmemory.GCREF, value)
+                    cpu.set_future_value_ptr(j, ptrvalue)
+                elif typecode == 'o':
+                    objvalue = ootype.cast_to_object(value)
+                    cpu.set_future_value_obj(j, objvalue)
+                elif typecode == 'i':
+                    intvalue = lltype.cast_primitive(lltype.Signed, value)
+                    cpu.set_future_value_int(j, intvalue)
                 else:
-                    raise AssertionError("box is: %s" % (box,))
-            return boxes
+                    assert False
+                j = j + 1
 
     class WarmEnterState:
         def __init__(self):
@@ -534,27 +535,26 @@ def make_state_class(warmrunnerdesc):
                 #interp.debug_trace("jit_compile", *greenargs)
                 metainterp_sd = warmrunnerdesc.metainterp_sd
                 metainterp = MetaInterp(metainterp_sd)
-                loop, boxes = metainterp.compile_and_run_once(*args)
+                loop = metainterp.compile_and_run_once(*args)
             else:
                 # machine code was already compiled for these greenargs
                 # (or we have a hash collision)
                 assert isinstance(cell, MachineCodeEntryPoint)
                 if not cell.equalkey(*greenargs):
                     # hash collision
-                    loop, boxes = self.handle_hash_collision(cell, argshash,
-                                                             *args)
+                    loop = self.handle_hash_collision(cell, argshash, *args)
                     if loop is None:
                         return
                 else:
                     # get the assembler and fill in the boxes
+                    cpu = warmrunnerdesc.metainterp_sd.cpu
+                    cell.set_future_values(cpu, *args[num_green_args:])
                     loop = cell.bridge
-                    boxes = cell.fill_boxes(*args[num_green_args:])
             # ---------- execute assembler ----------
             while True:     # until interrupted by an exception
                 metainterp_sd = warmrunnerdesc.metainterp_sd
-                fail_op = metainterp_sd.cpu.execute_operations(loop, boxes)
-                loop, boxes = fail_op.descr.handle_fail_op(metainterp_sd,
-                                                           fail_op)
+                fail_op = metainterp_sd.cpu.execute_operations(loop)
+                loop = fail_op.descr.handle_fail_op(metainterp_sd, fail_op)
         maybe_compile_and_run._dont_inline_ = True
 
         def handle_hash_collision(self, cell, argshash, *args):
@@ -567,15 +567,16 @@ def make_state_class(warmrunnerdesc):
                     cell.next = next.next
                     next.next = self.cells[argshash]
                     self.cells[argshash] = next
-                    return (next.bridge,
-                            next.fill_boxes(*args[num_green_args:]))
+                    cpu = warmrunnerdesc.metainterp_sd.cpu
+                    next.set_future_values(cpu, *args[num_green_args:])
+                    return next.bridge
                 cell = next
                 next = cell.next
             # not found at all, do profiling
             n = next.counter + 1
             if n < self.threshold:
                 cell.next = Counter(n)
-                return (None, None)
+                return None
             metainterp_sd = warmrunnerdesc.metainterp_sd
             metainterp = MetaInterp(metainterp_sd)
             return metainterp.compile_and_run_once(*args)
