ee7cc8bd83bdee4d0dccd446cbe4ad93788ef3fd
David Schneider <david.schneider@picle.org>
2009-04-28 19:35:20 +0000

    support parameters in method calls

--- pypy/lang/io/test/test_method.py
+++ pypy/lang/io/test/test_method.py
@@ -14,3 +14,22 @@ def test_parse_method():
     res,space = interpret(inp)
     assert res.value == 1
     
+def test_call_method_with_args():
+    inp = "a := method(x, x+1)\na(2)"
+    res,space = interpret(inp)
+    assert res.value == 3
+    
+def test_call_method_without_all_args():
+    inp = "a := method(x, y, z, 42)\na(2)"
+    res,space = interpret(inp)
+    assert res.value == 42
+    
+def test_unspecified_args_are_nil():
+    inp = "a := method(x, y, z, z)\na(2)"
+    res,space = interpret(inp)
+    assert res == space.w_nil
+    
+def test_superfluous_args_are_ignored():
+    inp = "a := method(x, y, z, z)\na(1,2,3,4,5,6,6,7)"
+    res,space = interpret(inp)
+    assert res.value == 3
