4edcf6e50f1580d1bda438da6ee297a1b92c1d09
David Schneider <david.schneider@picle.org>
2009-05-07 12:47:29 +0000

    (cfbolz, david) unwrap spec for registering functions

--- pypy/lang/io/number.py
+++ pypy/lang/io/number.py
@@ -1,11 +1,10 @@
 from pypy.lang.io.register import register_method
 from pypy.lang.io.model import W_Number
-@register_method("Number", '+')
-def w_number_add(space, w_target, w_message, w_context):
-    w_arg = w_message.arguments[0].eval(space, w_context, w_context)
-    return W_Number(space, w_target.value + w_arg.value)
+
+@register_method("Number", '+', unwrap_spec=[float, float])
+def w_number_add(space, target, argument):
+    return W_Number(space, target + argument)
     
-@register_method("Number", '-')
-def w_number_minus(space, w_target, w_message, w_context):
-    w_arg = w_message.arguments[0].eval(space, w_context, w_context)
-    return W_Number(space, w_target.value - w_arg.value)
+@register_method("Number", '-', unwrap_spec=[float, float])
+def w_number_minus(space, target, argument):
+    return W_Number(space, target - argument)
