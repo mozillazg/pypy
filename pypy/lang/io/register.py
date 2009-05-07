434c3b8689b12af5ad899fe3258681d3ee8fa279
David Schneider <david.schneider@picle.org>
2009-05-07 12:47:29 +0000

    (cfbolz, david) unwrap spec for registering functions

--- pypy/lang/io/register.py
+++ pypy/lang/io/register.py
@@ -1,7 +1,30 @@
+from pypy.lang.io import model
 cfunction_definitions = {}
-def register_method(type_name, slot_name):
+def register_method(type_name, slot_name, unwrap_spec=None):
     def register(function):
+        if unwrap_spec is None:
+            wrapper = function
+        else:
+            def wrapper(space, w_target, w_message, w_context):
+                evaled_w = [w_target]
+                for i in range(len(unwrap_spec)-1):
+                    evaled_w.append(
+                        w_message.arguments[i].eval(
+                            space, w_context, w_context))
+                
+                args = ()
+                for x, typ in zip(evaled_w, unwrap_spec):
+                    if typ is float:
+                        assert isinstance(x, model.W_Number)
+                        args += (x.value, )
+                    elif typ is object:
+                        args += (x, )
+                    else:
+                        raise ValueError, 'Unknown unwrap spec'
+                return function(space, *args)
         subdict = cfunction_definitions.setdefault(type_name, {})
-        subdict[slot_name] = function
+        subdict[slot_name] = wrapper
+
         return function
+
     return register
