dce45b2fb4df2bc46abd3d2c653ad36b66515a2e
Antonio Cuni <anto.cuni@gmail.com>
2009-05-21 12:50:55 +0000

    implement emit_op_instanceof, and all the remaining virtualizable tests pass. Moreover, add stubs for all the few still missing operations, so that we can kill the warning and put an assert instead

--- pypy/jit/backend/cli/test/test_zrpy_virtualizable.py
+++ pypy/jit/backend/cli/test/test_zrpy_virtualizable.py
@@ -7,14 +7,4 @@ class TestVirtualizable(CliTranslatedJitMixin, test_virtualizable.TestOOtype):
     # for the individual tests see
     # ====> ../../../metainterp/test/test_virtualizable.py
 
-    def skip(self):
-        py.test.skip('in-progress')
-
-    test_virtual_on_virtualizable = skip
-    test_no_virtual_on_virtualizable = skip
-    test_unequal_list_lengths_cannot_be_virtual = skip
-    test_virtualizable_hierarchy = skip
-    test_non_virtual_on_always_virtual = skip
-    test_external_pass = skip
-    test_always_virtual_with_origfields = skip
-    test_pass_always_virtual_to_bridge = skip
+    pass
