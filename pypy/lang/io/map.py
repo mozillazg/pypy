38de1c45bc192f0dba7563af9ab503dbf5ba1969
David Schneider <david.schneider@picle.org>
2009-05-30 14:10:56 +0000

    Restricted keys in maps to strings

--- pypy/lang/io/map.py
+++ pypy/lang/io/map.py
@@ -1,10 +1,12 @@
 from pypy.lang.io.register import register_method
-
+from pypy.lang.io.model import W_ImmutableSequence
 @register_method('Map', 'atPut', unwrap_spec=[object, object, object])
 def map_at_put(space, w_target, w_key, w_value):
+    assert isinstance(w_key, W_ImmutableSequence)
     w_target.at_put(w_key, w_value)
     return w_target
     
 @register_method('Map', 'at', unwrap_spec=[object, object])
 def map_at(space, w_target, w_key):
+    assert isinstance(w_key, W_ImmutableSequence)
     return w_target.at(w_key)
