557cc4a45c9d0621b09deb061bfad42ea9b1f0c7
Benjamin Peterson <benjamin@python.org>
2009-06-05 23:35:52 +0000

    test the metaparser by running it on Python grammar files

---
+++ pypy/interpreter/pyparser/pygram.py
@@ -0,0 +1,63 @@
+import token # Temporary until we move to the new parser.
+from pypy.interpreter.pyparser import parser, pytoken
+
+class PythonGrammar(parser.Grammar):
+
+    KEYWORD_TOKEN = token.NAME
+    TOKENS = token.__dict__
+
+opmap_raw = """
+( LPAR
+) RPAR
+[ LSQB
+] RSQB
+: COLON
+, COMMA
+; SEMI
++ PLUS
+- MINUS
+* STAR
+/ SLASH
+| VBAR
+& AMPER
+< LESS
+> GREATER
+= EQUAL
+. DOT
+% PERCENT
+` BACKQUOTE
+{ LBRACE
+} RBRACE
+@ AT
+== EQEQUAL
+!= NOTEQUAL
+<> NOTEQUAL
+<= LESSEQUAL
+>= GREATEREQUAL
+~ TILDE
+^ CIRCUMFLEX
+<< LEFTSHIFT
+>> RIGHTSHIFT
+** DOUBLESTAR
++= PLUSEQUAL
+-= MINEQUAL
+*= STAREQUAL
+/= SLASHEQUAL
+%= PERCENTEQUAL
+&= AMPEREQUAL
+|= VBAREQUAL
+^= CIRCUMFLEXEQUAL
+<<= LEFTSHIFTEQUAL
+>>= RIGHTSHIFTEQUAL
+**= DOUBLESTAREQUAL
+// DOUBLESLASH
+//= DOUBLESLASHEQUAL
+"""
+
+opmap = {}
+for line in opmap_raw.splitlines():
+    if line:
+        op, name = line.split()
+        opmap[op] = getattr(token, name)
+PythonGrammar.OPERATOR_MAP = opmap
+del opmap, opmap_raw
