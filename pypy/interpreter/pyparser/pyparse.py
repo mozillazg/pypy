8ac19c5d7de6566325ef9b97194224cff3edd7cc
Benjamin Peterson <benjamin@python.org>
2009-06-07 20:29:59 +0000

    parse Python with the new parser

---
+++ pypy/interpreter/pyparser/pyparse.py
@@ -0,0 +1,110 @@
+import codeop
+from pypy.interpreter import gateway
+from pypy.interpreter.pyparser.error import SyntaxError, IndentationError
+from pypy.interpreter.pyparser import parser, pytokenizer, pygram, error
+
+
+_recode_to_utf8 = gateway.applevel(r'''
+    def _recode_to_utf8(text, encoding):
+        return unicode(text, encoding).encode("utf-8")
+''').interphook('_recode_to_utf8')
+
+def recode_to_utf8(space, text, encoding):
+    return space.str_w(_recode_to_utf8(space, space.wrap(text),
+                                          space.wrap(encoding)))
+def _normalize_encoding(encoding):
+    """returns normalized name for <encoding>
+
+    see dist/src/Parser/tokenizer.c 'get_normal_name()'
+    for implementation details / reference
+
+    NOTE: for now, parser.suite() raises a MemoryError when
+          a bad encoding is used. (SF bug #979739)
+    """
+    if encoding is None:
+        return None
+    # lower() + '_' / '-' conversion
+    encoding = encoding.replace('_', '-').lower()
+    if encoding.startswith('utf-8'):
+        return 'utf-8'
+    for variant in ['latin-1', 'iso-latin-1', 'iso-8859-1']:
+        if encoding.startswith(variant):
+            return 'iso-8859-1'
+    return encoding
+
+def _check_for_encoding(s):
+    eol = s.find('\n')
+    if eol < 0:
+        return _check_line_for_encoding(s)
+    enc = _check_line_for_encoding(s[:eol])
+    if enc:
+        return enc
+    eol2 = s.find('\n', eol + 1)
+    if eol2 < 0:
+        return _check_line_for_encoding(s[eol + 1:])
+    return _check_line_for_encoding(s[eol + 1:eol2])
+
+
+def _check_line_for_encoding(line):
+    """returns the declared encoding or None"""
+    i = 0
+    for i in range(len(line)):
+        if line[i] == '#':
+            break
+        if line[i] not in ' \t\014':
+            return None
+    return pytokenizer.match_encoding_declaration(line[i:])
+
+
+_targets = {
+'eval' : pygram.syms.eval_input,
+'single' : pygram.syms.single_input,
+'exec' : pygram.syms.file_input,
+}
+
+class PythonParser(parser.Parser):
+
+    def parse_source(self, textsrc, mode="exec", flags=0):
+        """Parse a python source according to goal"""
+        # Detect source encoding.
+        if textsrc[:3] == '\xEF\xBB\xBF':
+            textsrc = textsrc[3:]
+            enc = 'utf-8'
+            # check that there is no explicit encoding declared
+            decl_enc = _check_for_encoding(textsrc)
+            if decl_enc is not None:
+                raise SyntaxError("encoding declaration in Unicode string")
+        else:
+            enc = _normalize_encoding(_check_for_encoding(textsrc))
+            if enc is not None and enc not in ('utf-8', 'iso-8859-1'):
+                try:
+                    textsrc = recode_to_utf8(builder.space, textsrc, enc)
+                except OperationError, e:
+                    # if the codec is not found, LookupError is raised.  we
+                    # check using 'is_w' not to mask potential IndexError or
+                    # KeyError
+                    space = builder.space
+                    if space.is_w(e.w_type, space.w_LookupError):
+                        raise SyntaxError("Unknown encoding: %s" % enc)
+                    raise
+
+        self.prepare(_targets[mode])
+        try:
+            tokens = pytokenizer.generate_tokens(textsrc, flags)
+            for tp, value, lineno, column, line in tokens:
+                if self.add_token(tp, value, lineno, column, line):
+                    break
+        except parser.ParseError, e:
+            new_err = error.IndentationError
+            if tp == pygram.tokens.INDENT:
+                msg = "unexpected indent"
+            elif e.expected == pygram.tokens.INDENT:
+                msg = "expected indented block"
+            else:
+                new_err = error.SyntaxError
+                msg = "invalid syntax"
+            raise new_err(msg, e.lineno, e.column, e.line)
+        else:
+            return self.root
+        finally:
+            self.root = None
---
+++ pypy/interpreter/pyparser/pytokenizer.py
@@ -0,0 +1,254 @@
+import codeop
+from pypy.interpreter.pyparser import automata
+from pypy.interpreter.pyparser.pygram import tokens
+from pypy.interpreter.pyparser.pytoken import python_opmap
+from pypy.interpreter.pyparser.error import TokenError, TokenIndentationError
+from pypy.interpreter.pyparser.pytokenize import tabsize, whiteSpaceDFA, \
+    triple_quoted, endDFAs, single_quoted, pseudoDFA
+
+NAMECHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
+NUMCHARS = '0123456789'
+ALNUMCHARS = NAMECHARS + NUMCHARS
+EXTENDED_ALNUMCHARS = ALNUMCHARS + '-.'
+WHITESPACES = ' \t\n\r\v\f'
+
+def match_encoding_declaration(comment):
+    """returns the declared encoding or None
+
+    This function is a replacement for :
+    >>> py_encoding = re.compile(r"coding[:=]\s*([-\w.]+)")
+    >>> py_encoding.search(comment)
+    """
+    index = comment.find('coding')
+    if index < 0:
+        return None
+    next_char = comment[index + 6]
+    if next_char not in ':=':
+        return None
+    end_of_decl = comment[index + 7:]
+    index = 0
+    for char in end_of_decl:
+        if char not in WHITESPACES:
+            break
+        index += 1
+    else:
+        return None
+    encoding = ''
+    for char in end_of_decl[index:]:
+        if char in EXTENDED_ALNUMCHARS:
+            encoding += char
+        else:
+            break
+    if encoding != '':
+        return encoding
+    return None
+
+
+def generate_tokens(textsrc, flags):
+    """
+    This is a rewrite of pypy.module.parser.pytokenize.generate_tokens since
+    the original function is not RPYTHON (uses yield)
+    It was also slightly modified to generate Token instances instead
+    of the original 5-tuples -- it's now a 4-tuple of
+    
+    * the Token instance
+    * the whole line as a string
+    * the line number (the real one, counting continuation lines)
+    * the position on the line of the end of the token.
+
+    Original docstring ::
+    
+        The generate_tokens() generator requires one argment, readline, which
+        must be a callable object which provides the same interface as the
+        readline() method of built-in file objects. Each call to the function
+        should return one line of input as a string.
+
+        The generator produces 5-tuples with these members: the token type; the
+        token string; a 2-tuple (srow, scol) of ints specifying the row and
+        column where the token begins in the source; a 2-tuple (erow, ecol) of
+        ints specifying the row and column where the token ends in the source;
+        and the line on which the token was found. The line passed is the
+        logical line; continuation lines are included.
+    """
+    token_list = []
+    lnum = parenlev = continued = 0
+    namechars = NAMECHARS
+    numchars = NUMCHARS
+    contstr, needcont = '', 0
+    contline = None
+    indents = [0]
+    last_comment = ''
+
+    # make the annotator happy
+    endDFA = automata.DFA([], [])
+    # make the annotator happy
+    line = ''
+    for line in textsrc.splitlines(True):
+        lnum = lnum + 1
+        pos, max = 0, len(line)
+
+        if contstr:                            # continued string
+            if not line:
+                raise TokenError("EOF while scanning triple-quoted string",
+                                 line, lnum-1, 0, token_list)
+            endmatch = endDFA.recognize(line)
+            if endmatch >= 0:
+                pos = end = endmatch
+                tok = (tokens.STRING, constr + line[:end], lnum, pos, line)
+                token_list.append(tok)
+                last_comment = ''
+                # token_list.append((STRING, contstr + line[:end],
+                #                    strstart, (lnum, end), contline + line))
+                contstr, needcont = '', 0
+                contline = None
+            elif (needcont and not line.endswith('\\\n') and
+                               not line.endswith('\\\r\n')):
+                tok = (tokens.ERRORTOKEN, constr + line, lnum, pos, line)
+                token_list.append(tok)
+                last_comment = ''
+                # token_list.append((ERRORTOKEN, contstr + line,
+                #                    strstart, (lnum, len(line)), contline))
+                contstr = ''
+                contline = None
+                continue
+            else:
+                contstr = contstr + line
+                contline = contline + line
+                continue
+
+        elif parenlev == 0 and not continued:  # new statement
+            if not line: break
+            column = 0
+            while pos < max:                   # measure leading whitespace
+                if line[pos] == ' ': column = column + 1
+                elif line[pos] == '\t': column = (column/tabsize + 1)*tabsize
+                elif line[pos] == '\f': column = 0
+                else: break
+                pos = pos + 1
+            if pos == max: break
+
+            if line[pos] in '#\r\n':
+                # skip comments or blank lines
+                continue
+
+            if column > indents[-1]:           # count indents or dedents
+                indents.append(column)
+                token_list.append((tokens.INDENT, line[:pos], lnum, pos, line))
+                last_comment = ''
+            while column < indents[-1]:
+                indents = indents[:-1]
+                token_list.append((tokens.DEDENT, '', lnum, pos, line))
+                last_comment = ''
+            if column != indents[-1]:
+                raise TokenIndentationError("unindent does not match any outer indentation level",
+                                 line, lnum, 0, token_list)
+
+        else:                                  # continued statement
+            if not line:
+                raise TokenError("EOF in multi-line statement", line,
+                                 lnum, 0, token_list)
+            continued = 0
+
+        while pos < max:
+            pseudomatch = pseudoDFA.recognize(line, pos)
+            if pseudomatch >= 0:                            # scan for tokens
+                # JDR: Modified
+                start = whiteSpaceDFA.recognize(line, pos)
+                if start < 0:
+                    start = pos
+                end = pseudomatch
+
+                if start == end:
+                    # Nothing matched!!!
+                    raise TokenError("Unknown character", line,
+                                 lnum, start, token_list)
+
+                pos = end
+                token, initial = line[start:end], line[start]
+                if initial in numchars or \
+                   (initial == '.' and token != '.'):      # ordinary number
+                    token_list.append((tokens.NUMBER, token, lnum, pos, line))
+                    last_comment = ''
+                elif initial in '\r\n':
+                    if parenlev <= 0:
+                        tok = (tokens.NEWLINE, last_comment, lnum, pos, line)
+                        token_list.append(tok)
+                    last_comment = ''
+                elif initial == '#':
+                    # skip comment
+                    last_comment = token
+                elif token in triple_quoted:
+                    endDFA = endDFAs[token]
+                    endmatch = endDFA.recognize(line, pos)
+                    if endmatch >= 0:                     # all on one line
+                        pos = endmatch
+                        token = line[start:pos]
+                        tok = (tokens.STRING, token, lnum, pos, line)
+                        token_list.append(tok)
+                        last_comment = ''
+                    else:
+                        contstr = line[start:]
+                        contline = line
+                        break
+                elif initial in single_quoted or \
+                    token[:2] in single_quoted or \
+                    token[:3] in single_quoted:
+                    if token[-1] == '\n':                  # continued string
+                        endDFA = (endDFAs[initial] or endDFAs[token[1]] or
+                                   endDFAs[token[2]])
+                        contstr, needcont = line[start:], 1
+                        contline = line
+                        break
+                    else:                                  # ordinary string
+                        tok = (tokens.STRING, token, lnum, pos, line)
+                        token_list.append(tok)
+                        last_comment = ''
+                elif initial in namechars:                 # ordinary name
+                    token_list.append((tokens.NAME, token, lnum, pos, line))
+                    last_comment = ''
+                elif initial == '\\':                      # continued stmt
+                    continued = 1
+                    # lnum -= 1  disabled: count continuation lines separately
+                else:
+                    if initial in '([{':
+                        parenlev = parenlev + 1
+                    elif initial in ')]}':
+                        parenlev = parenlev - 1
+                        if parenlev < 0:
+                            raise TokenError("unmatched '%s'" % initial, line,
+                                             lnum-1, 0, token_list)
+                    if token in python_opmap:
+                        punct = python_opmap[token]
+                        token = ""
+                    else:
+                        punct = parser.tokens['OP']
+                    token_list.append((punct, token, lnum, pos, line))
+                    last_comment = ''
+            else:
+                start = whiteSpaceDFA.recognize(line, pos)
+                if start < 0:
+                    start = pos
+                if start<max and line[start] in single_quoted:
+                    raise TokenError("EOL while scanning single-quoted string",
+                             lnum, start, line, token_list)
+                tok = (tokens.ERRORTOKEN, line[pos], lnum, pos, lines)
+                token_list.append(tok)
+                last_comment = ''
+                pos = pos + 1
+
+    lnum -= 1
+    if not (flags & codeop.PyCF_DONT_IMPLY_DEDENT):
+        if token_list and token_list[-1][0] != tokens.NEWLINE:
+            tok = (tokens.NEWLINE, '', lnum, 0, '\n')
+            token_list.append(tok)
+        for indent in indents[1:]:                # pop remaining indent levels
+            token_list.append((tokens.DEDENT, '', lnum, pos, line))
+    #if token_list and token_list[-1][0].codename != pytoken.NEWLINE:
+    tok = (tokens.NEWLINE, '', lnum, 0, '\n')
+    token_list.append(tok)
+
+    token_list.append((tokens.ENDMARKER, '', lnum, pos, line))
+    #for t in token_list:
+    #    print '%20s  %-25s %d' % (pytoken.tok_name.get(t[0].codename, '?'), t[0], t[-2])
+    #print '----------------------------------------- pyparser/pythonlexer.py'
+    return token_list
