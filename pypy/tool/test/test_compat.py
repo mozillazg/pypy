d8d61c6efe503aa3e4742aac40e935af3147cf8a
Armin Rigo <arigo@tunes.org>
2009-07-13 13:49:05 +0000

    Fix the test, mostly randomly (what is the point of this test?).

--- pypy/tool/test/test_compat.py
+++ pypy/tool/test/test_compat.py
@@ -1,5 +1,5 @@
 
 def test_base_exc():
     from pypy.tool.compat import BaseException
-    assert KeyboardInterrupt.__mro__[-2] is BaseException
-    assert ValueError.__mro__[-2] is BaseException
+    assert issubclass(KeyboardInterrupt, BaseException)
+    assert issubclass(ValueError, BaseException)
