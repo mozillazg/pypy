5a1cade8661bbec157f32b031deb2225bb7f4a44
Armin Rigo <arigo@tunes.org>
2009-07-17 16:15:10 +0000

    More tests about new_with_vtable. Fix some code about intersect(). Skipped test about an infinite loop.

--- pypy/jit/metainterp/specnode.py
+++ pypy/jit/metainterp/specnode.py
@@ -10,6 +10,9 @@ class SpecNode(object):
 class NotSpecNode(SpecNode):
     __slots__ = ()
 
+    def _equals(self, other):   # for tests only
+        return type(other) is NotSpecNode
+
 
 prebuiltNotSpecNode = NotSpecNode()
 
@@ -18,8 +21,21 @@ class FixedClassSpecNode(SpecNode):
     def __init__(self, known_class):
         self.known_class = known_class
 
+    def _equals(self, other):   # for tests only
+        return (type(other) is FixedClassSpecNode and
+                self.known_class.equals(other.known_class))
+
 
 class VirtualInstanceSpecNode(FixedClassSpecNode):
     def __init__(self, known_class, fields):
         FixedClassSpecNode.__init__(self, known_class)
         self.fields = fields
+
+    def _equals(self, other):   # for tests only
+        ok = (type(other) is VirtualInstanceSpecNode and
+              self.known_class.equals(other.known_class) and
+              len(self.fields) == len(other.fields))
+        if ok:
+            for (o1, s1), (o2, s2) in zip(self.fields, other.fields):
+                ok = ok and o1 == o2 and s1._equals(s2)
+        return ok
