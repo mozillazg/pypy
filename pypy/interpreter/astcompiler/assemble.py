a7cf2ed3b2279648c56cf8a55aff8391fc77743f
Benjamin Peterson <benjamin@python.org>
2009-07-20 02:16:51 +0000

    allow call to be constant folded

--- pypy/interpreter/astcompiler/assemble.py
+++ pypy/interpreter/astcompiler/assemble.py
@@ -503,7 +503,7 @@ def _opcode_stack_effect(op, arg):
     if we_are_translated():
         for possible_op in ops.unrolling_opcode_descs:
             if op == possible_op.index:
-                return _stack_effect_computers[op](arg)
+                return _stack_effect_computers[possible_op](arg)
         else:
             raise AssertionError("unkown opcode: %s" % (op,))
     else:
