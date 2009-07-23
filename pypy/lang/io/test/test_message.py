db3c8fdca68ef349b86f0e2e1850f86a7f62a73b
David Schneider <david.schneider@picle.org>
2009-07-23 16:31:30 +0000

    name on Message

--- pypy/lang/io/test/test_message.py
+++ pypy/lang/io/test/test_message.py
@@ -1,5 +1,5 @@
 from pypy.lang.io.parserhack import interpret
-from pypy.lang.io.model import W_Message, W_Block, W_List
+from pypy.lang.io.model import W_Message, W_Block, W_List, W_ImmutableSequence
 import py
 
 
@@ -23,7 +23,14 @@ def test_message_arguments():
   assert res[0].name == 'C'
   assert res[0].next.name == 'D' 
   assert res[1].name == 'E'
-  
+
+def test_message_name():
+    inp = """msg := message(B(C D, E));
+    msg name"""
+    res, space = interpret(inp)
+    assert isinstance(res, W_ImmutableSequence)
+    assert res.value == 'B'
+    
 # def test_setIsActivatable():
 #     inp = "a := block(1);a setIsActivateable(true); a"
 #     res,space = interpret(inp)
