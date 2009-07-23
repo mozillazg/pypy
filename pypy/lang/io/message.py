234612b9777d9f3be69e1046b3a59a529fd879d5
David Schneider <david.schneider@picle.org>
2009-07-23 16:31:30 +0000

    name on Message

--- pypy/lang/io/message.py
+++ pypy/lang/io/message.py
@@ -1,5 +1,5 @@
 from pypy.lang.io.register import register_method
-from pypy.lang.io.model import W_Message
+from pypy.lang.io.model import W_Message, W_ImmutableSequence
 
 @register_method('Message', 'argAt', unwrap_spec=[object, int])
 def message_arg_at(space, w_message, arg_num):
@@ -10,7 +10,11 @@ def message_arg_at(space, w_message, arg_num):
 @register_method('Message', 'arguments')
 def message_arguments(space, w_receiver, w_message, w_context):
   return space.w_list.clone_and_init(space, w_receiver.arguments)
-  
+ 
+@register_method('Message', 'name')
+def message_name(space, w_receiver, w_message, w_context):
+    # XXX TODO clone from space
+    return W_ImmutableSequence(space, w_receiver.name)
 
 # @register_method('Message', 'setIsActivatable', unwrap_spec=[object, bool])
 # def message_setIsActivatable(space, w_target, setting):
