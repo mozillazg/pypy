d46d6c070f6db797e73fffdeab76f1c89a53e52a
Benjamin Peterson <benjamin@python.org>
2009-07-24 01:31:54 +0000

    move a few global helpers to methods of the ast

---
+++ pypy/interpreter/astcompiler/asthelpers.py
@@ -0,0 +1,46 @@
+from pypy.interpreter.astcompiler import ast2 as ast
+from pypy.interpreter.error import OperationError
+
+
+class __extend__(ast.expr):
+
+    constant = False
+
+    def as_node_list(self, space):
+        return None
+
+
+class __extend__(ast.List):
+
+    def as_node_list(self, space):
+        return self.elts
+
+
+class __extend__(ast.Tuple):
+
+    def as_list_w(self, space):
+        return self.elts
+
+
+class __extend__(ast.Const):
+
+    constant = True
+
+    def as_node_list(self, space):
+        try:
+            values_w = space.unpackiterable(self.value)
+        except OperationError:
+            return None
+        line = self.lineno
+        column = self.col_offset
+        return [ast.Const(w_obj, line, column) for w_obj in values_w]
+
+
+class __extend__(ast.Str):
+
+    constant = True
+
+
+class __extend__(ast.Num):
+
+    constant = True
