a63d6c78cb328d39500712ce5c9a1eef48b4abb1
David Schneider <david.schneider@picle.org>
2009-08-13 20:23:46 +0000

    run, setResult and setRunMessage on Coroutines

--- pypy/lang/io/coroutine.py
+++ pypy/lang/io/coroutine.py
@@ -2,7 +2,6 @@ from pypy.lang.io.register import register_method
 from pypy.lang.io.model import W_Object
 from pypy.lang.io.coroutinemodel import W_Coroutine
 
-
 @register_method('Coroutine', 'currentCoroutine')
 def coroutine_get_current(space, w_target, w_message, w_context):
     return W_Coroutine.w_getcurrent(space)
@@ -10,3 +9,20 @@ def coroutine_get_current(space, w_target, w_message, w_context):
 @register_method('Coroutine', 'isCurrent')
 def coroutine_is_current(space, w_target, w_message, w_context):
     return space.newbool(w_target is W_Coroutine.w_getcurrent(space))
+    
+@register_method('Coroutine', 'setRunMessage', unwrap_spec=[object, object])
+def coroutine_setRunMessage(space, w_coroutine, w_message):
+    w_coroutine.slots['runMessage'] = w_message
+    return w_coroutine
+    
+@register_method('Coroutine', 'run')
+def coroutine_run(space, w_target, w_message, w_context):
+    w_target.run(space, w_target, w_context)
+    return w_target
+    
+# @register_method('Coroutine', 'setResult', unwrap_spec=[object, object])
+# def coroutine_set_result(space, w_coro, w_result):
+#     print w_result
+#     w_coro.slots['result'] = w_result
+#     return w_coro
+#     
