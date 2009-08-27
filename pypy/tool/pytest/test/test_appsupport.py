f054f2d12d266d3577f37cc66cae636e18757eb7
Benjamin Peterson <benjamin@python.org>
2009-08-27 21:41:14 +0000

    fix app tests using raises with a string instead of a callable argument
    and I thought just using sys._getframe() was bad enough... :(

--- pypy/tool/pytest/test/test_appsupport.py
+++ pypy/tool/pytest/test/test_appsupport.py
@@ -4,3 +4,6 @@ def app_test_raises():
     info = raises(TypeError, id)
     assert info[0] is TypeError
     assert isinstance(info[1], TypeError)
+
+    x = 43
+    raises(ZeroDivisionError, "x/0")
