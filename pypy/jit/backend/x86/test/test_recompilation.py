e5db2376a275480501bde90f9a0f99089aacab6d
Maciej Fijalkowski <fijall@gmail.com>
2009-09-01 19:15:05 +0000

    Add more tests

--- pypy/jit/backend/x86/test/test_recompilation.py
+++ pypy/jit/backend/x86/test/test_recompilation.py
@@ -53,5 +53,86 @@ class TestRecompilation(BaseTestRegalloc):
         assert self.getint(2) == 23
         assert self.getint(3) == 24
 
-#    def test_bridge_jump_to_other_loop(self):
-#        xxx
+    def test_bridge_jump_to_other_loop(self):
+        loop = self.interpret('''
+        [i0, i10, i11, i12, i13, i14, i15, i16]
+        i1 = int_add(i0, 1)
+        i2 = int_lt(i1, 20)
+        guard_true(i2)
+           fail(i1)
+        jump(i1, i10, i11, i12, i13, i14, i15, i16)
+        ''', [0])
+        other_loop = self.interpret('''
+        [i3]
+        guard_false(i3)
+           fail(i3)
+        jump(i3)
+        ''', [1])
+        ops = '''
+        [i3]
+        jump(i3, 1, 2, 3, 4, 5, 6, 7)
+        '''
+        bridge = self.attach_bridge(ops, other_loop, other_loop.operations[0],
+                                    jump_targets=[loop])
+        self.cpu.set_future_value_int(0, 1)
+        op = self.cpu.execute_operations(other_loop)
+        assert op is loop.operations[2].suboperations[0]
+
+    def test_bridge_jumps_to_self_deeper(self):
+        loop = self.interpret('''
+        [i0, i1, i2]
+        i3 = int_add(i0, 1)
+        i4 = int_and(i3, 1)
+        guard_false(i4)
+            fail(0, i3)
+        i5 = int_lt(i3, 20)
+        guard_true(i5)
+            fail(1, i3)
+        jump(i3, i1, i2)
+        ''', [0])
+        assert self.getint(0) == 0
+        assert self.getint(1) == 1
+        ops = '''
+        [i3]
+        i10 = int_mul(i3, 2)
+        i8 = int_add(i3, 1)
+        i6 = int_add(i8, i10)
+        i7 = int_add(i3, i6)
+        jump(i3, i6, i7)
+        '''
+        bridge = self.attach_bridge(ops, loop, loop.operations[2],
+                                    jump_targets=[loop])
+        self.cpu.set_future_value_int(0, 0)
+        self.cpu.set_future_value_int(1, 0)
+        self.cpu.set_future_value_int(2, 0)
+        self.cpu.execute_operations(loop)
+        assert self.getint(0) == 1
+        assert self.getint(1) == 20
+
+    def test_bridge_jumps_to_self_shallower(self):
+        loop = self.interpret('''
+        [i0, i1, i2]
+        i3 = int_add(i0, 1)
+        i4 = int_and(i3, 1)
+        guard_false(i4)
+            fail(0, i3)
+        i5 = int_lt(i3, 20)
+        guard_true(i5)
+            fail(1, i3)
+        jump(i3, i1, i2)
+        ''', [0])
+        assert self.getint(0) == 0
+        assert self.getint(1) == 1
+        ops = '''
+        [i3]
+        jump(i3, 0, 1)
+        '''
+        bridge = self.attach_bridge(ops, loop, loop.operations[2],
+                                    jump_targets=[loop])
+        self.cpu.set_future_value_int(0, 0)
+        self.cpu.set_future_value_int(1, 0)
+        self.cpu.set_future_value_int(2, 0)
+        self.cpu.execute_operations(loop)
+        assert self.getint(0) == 1
+        assert self.getint(1) == 20
+        
