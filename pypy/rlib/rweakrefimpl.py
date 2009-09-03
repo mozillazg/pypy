13abd9298f3919f07f6958b83078d5109f997437
Armin Rigo <arigo@tunes.org>
2009-09-03 15:24:47 +0000

    Finish and test rweakrefimpl.py.

--- pypy/rlib/rweakrefimpl.py
+++ pypy/rlib/rweakrefimpl.py
@@ -1,7 +1,7 @@
 from pypy.rpython.lltypesystem import lltype, rstr, rclass, rdict
 from pypy.rpython.controllerentry import Controller
-from pypy.rpython.annlowlevel import llstr, cast_base_ptr_to_instance
-from pypy.rpython.annlowlevel import cast_instance_to_base_ptr
+from pypy.rpython.annlowlevel import cast_base_ptr_to_instance
+from pypy.rpython.annlowlevel import cast_instance_to_base_ptr, llstr
 from pypy.rlib.objectmodel import specialize
 from pypy.rlib.rweakref import RWeakValueDictionary
 
@@ -11,11 +11,10 @@ class WeakDictController(Controller):
 
     @specialize.arg(1)
     def new(self, valueclass):
-        d = lltype.malloc(get_WEAKDICT(valueclass))
-        entries = lltype.malloc(WEAKDICTENTRYARRAY, DICT_INITSIZE)
-        for i in range(DICT_INITSIZE):
-            entries[i].key = pristine_marker
-        d.entries = entries
+        WEAKDICT = get_WEAKDICT(valueclass)
+        d = lltype.malloc(WEAKDICT)
+        d.entries = WEAKDICT.entries.TO.allocate(DICT_INITSIZE)
+        d.num_pristine_entries = DICT_INITSIZE
         return d
 
     def get_get(self, d):
@@ -40,9 +39,23 @@ def ll_everused(entries, i):
     key = entries[i].key
     return key != pristine_marker
 
+str_fasthashfn = rstr.string_repr.get_ll_fasthash_function()
+
+def ll_hash(entries, i):
+    return str_fasthashfn(entries[i].key)
+
+def ll_malloc_entries_and_initialize(ENTRIES, n):
+    entries = lltype.malloc(ENTRIES, n, zero=True)
+    for i in range(n):
+        entries[i].key = pristine_marker
+    return entries
+
 entrymeths = {
+    'allocate': lltype.typeMethod(ll_malloc_entries_and_initialize),
+    'delete': rdict._ll_free_entries,
     'valid': ll_valid,
     'everused': ll_everused,
+    'hash': ll_hash,
     }
 WEAKDICTENTRYARRAY = lltype.GcArray(WEAKDICTENTRY,
                                     adtmeths=entrymeths,
@@ -50,21 +63,49 @@ WEAKDICTENTRYARRAY = lltype.GcArray(WEAKDICTENTRY,
 
 def ll_get(d, key):
     llkey = llstr(key)
-    i = rdict.ll_dict_lookup(d, llkey, llkey.gethash())
+    hash = llkey.gethash()
+    i = rdict.ll_dict_lookup(d, llkey, hash)
     llvalue = d.entries[i].value
+    #print 'get', i, key, hash, llvalue
     return cast_base_ptr_to_instance(d.valueclass, llvalue)
 
 def ll_set(d, key, value):
     llkey = llstr(key)
     llvalue = cast_instance_to_base_ptr(value)
-    i = rdict.ll_dict_lookup(d, llkey, llkey.gethash())
+    hash = llkey.gethash()
+    i = rdict.ll_dict_lookup(d, llkey, hash)
+    everused = d.entries.everused(i)
+    #print 'set', i, key, hash, llvalue
     d.entries[i].key = llkey
     d.entries[i].value = llvalue
+    if not everused:
+        d.num_pristine_entries -= 1
+        #print 'num_pristine_entries:', d.num_pristine_entries
+        if d.num_pristine_entries <= len(d.entries) / 3:
+            ll_weakdict_resize(d)
+    else:
+        pass   #print 'entry reused'
+
+def ll_weakdict_resize(d):
+    # first set num_items to its correct, up-to-date value
+    #print 'in ll_weakdict_resize'
+    entries = d.entries
+    num_items = 0
+    for i in range(len(entries)):
+        if entries.valid(i):
+            num_items += 1
+    d.num_items = num_items
+    #print 'num_items:', num_items
+    rdict.ll_dict_resize(d)
+    #print 'resized.'
+
+str_keyeq = lltype.staticAdtMethod(rstr.string_repr.get_ll_eq_function())
 
 dictmeths = {
     'll_get': ll_get,
     'll_set': ll_set,
-    'keyeq': None,
+    'keyeq': str_keyeq,
+    'paranoia': False,
     }
 
 @specialize.memo()
@@ -72,6 +113,8 @@ def get_WEAKDICT(valueclass):
     adtmeths = dictmeths.copy()
     adtmeths['valueclass'] = valueclass
     WEAKDICT = lltype.GcStruct("weakdict",
+                               ("num_items", lltype.Signed),
+                               ("num_pristine_entries", lltype.Signed),
                                ("entries", lltype.Ptr(WEAKDICTENTRYARRAY)),
                                adtmeths=adtmeths)
     return WEAKDICT
--- pypy/rlib/test/test_rweakref.py
+++ pypy/rlib/test/test_rweakref.py
@@ -6,37 +6,45 @@ class X(object):
     pass
 
 
-def test_RWeakValueDictionary():
-    d = RWeakValueDictionary(X)
-    assert d.get("hello") is None
-    x1 = X(); x2 = X(); x3 = X()
-    d.set("abc", x1)
-    d.set("def", x2)
-    d.set("ghi", x3)
-    assert d.get("abc") is x1
-    assert d.get("def") is x2
-    assert d.get("ghi") is x3
-    assert d.get("hello") is None
-    x2 = None
-    rgc.collect(); rgc.collect()
-    assert d.get("abc") is x1
-    assert d.get("def") is None
-    assert d.get("ghi") is x3
-    assert d.get("hello") is None
-    d.set("abc", None)
-    assert d.get("abc") is None
-    assert d.get("def") is None
-    assert d.get("ghi") is x3
-    assert d.get("hello") is None
-    # resizing should also work
-    for i in range(100):
-        d.set(str(i), x1)
-    for i in range(100):
-        assert d.get(str(i)) is x1
-    assert d.get("abc") is None
-    assert d.get("def") is None
-    assert d.get("ghi") is x3
-    assert d.get("hello") is None
+def make_test(go_away=True, loop=100):
+    def f():
+        d = RWeakValueDictionary(X)
+        assert d.get("hello") is None
+        x1 = X(); x2 = X(); x3 = X()
+        d.set("abc", x1)
+        d.set("def", x2)
+        d.set("ghi", x3)
+        assert d.get("abc") is x1
+        assert d.get("def") is x2
+        assert d.get("ghi") is x3
+        assert d.get("hello") is None
+        if go_away:
+            x2 = None
+            rgc.collect(); rgc.collect()
+        assert d.get("abc") is x1
+        assert d.get("def") is x2
+        assert d.get("ghi") is x3
+        assert d.get("hello") is None
+        d.set("abc", None)
+        assert d.get("abc") is None
+        assert d.get("def") is x2
+        assert d.get("ghi") is x3
+        assert d.get("hello") is None
+        # resizing should also work
+        for i in range(loop):
+            d.set(str(i), x1)
+        for i in range(loop):
+            assert d.get(str(i)) is x1
+        assert d.get("abc") is None
+        assert d.get("def") is x2
+        assert d.get("ghi") is x3
+        assert d.get("hello") is None
+    return f
 
-#def test_rpython_RWeakValueDictionary():
-#    interpret(test_RWeakValueDictionary, [])
+def test_RWeakValueDictionary():
+    make_test()()
+
+def test_rpython_RWeakValueDictionary():
+    # we don't implement the correct weakref behavior in lltype when
+    # directly interpreted, but we can still test that the rest works.
+    interpret(make_test(go_away=False, loop=12), [])
