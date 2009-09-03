380755820b34120e5600d9674ab92a9ccef65710
Armin Rigo <arigo@tunes.org>
2009-09-03 17:10:40 +0000

    Pff.  Rewrite it "the long and boring way".  Indeed, controllerentry is yet again not the right thing -- there is a level mismatch when trying to pass test_rpython_prebuilt.

--- pypy/rlib/rweakref.py
+++ pypy/rlib/rweakref.py
@@ -29,17 +29,48 @@ class RWeakValueDictionary(object):
 
 # ____________________________________________________________
 
-from pypy.rpython import controllerentry
+from pypy.rpython import extregistry
+from pypy.annotation import model as annmodel
+from pypy.annotation.bookkeeper import getbookkeeper
 
-@staticmethod
-def _get_controller():
-    from pypy.rlib.rweakrefimpl import WeakDictController
-    return WeakDictController()
+class SomeWeakValueDict(annmodel.SomeObject):
+    knowntype = RWeakValueDictionary
 
-class Entry(controllerentry.ControllerEntry):
+    def __init__(self, valueclassdef):
+        self.valueclassdef = valueclassdef
+
+    def rtyper_makerepr(self, rtyper):
+        from pypy.rlib import rweakrefimpl
+        return rweakrefimpl.WeakValueDictRepr(rtyper)
+
+    def rtyper_makekey_ex(self, rtyper):
+        return self.__class__,
+
+    def method_get(self, s_key):
+        assert isinstance(s_key, annmodel.SomeString)
+        return annmodel.SomeInstance(self.valueclassdef, can_be_None=True)
+
+    def method_set(self, s_key, s_value):
+        s_oldvalue = self.method_get(s_key)
+        assert s_oldvalue.contains(s_value)
+
+class Entry(extregistry.ExtRegistryEntry):
     _about_ = RWeakValueDictionary
-    _controller_ = _get_controller
 
-class Entry(controllerentry.ControllerEntryForPrebuilt):
+    def compute_result_annotation(self, s_valueclass):
+        assert isinstance(s_valueclass, annmodel.SomePBC)
+        assert s_valueclass.is_constant()
+        [desc] = s_valueclass.descriptions
+        return SomeWeakValueDict(desc.getuniqueclassdef())
+
+    def specialize_call(self, hop):
+        from pypy.rlib import rweakrefimpl
+        return rweakrefimpl.specialize_make_weakdict(hop)
+
+class Entry(extregistry.ExtRegistryEntry):
     _type_ = RWeakValueDictionary
-    _controller_ = _get_controller
+
+    def compute_annotation(self):
+        bk = self.bookkeeper
+        x = self.instance
+        return SomeWeakValueDict(bk.getuniqueclassdef(x._valueclass))
