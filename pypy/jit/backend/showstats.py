b34b407d614cbdb11d5b35c516c00f3556d2cdc5
Maciej Fijalkowski <fijall@gmail.com>
2009-09-10 18:07:03 +0000

    improve a bit

--- pypy/jit/backend/showstats.py
+++ pypy/jit/backend/showstats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys, py
 from pypy.jit.metainterp.test.oparser import parse
+from pypy.jit.metainterp.resoperation import rop
 from pypy.rpython.lltypesystem import lltype, llmemory
 
 class AllDict(dict):
@@ -13,7 +14,18 @@ def main(argv):
     lst = py.path.local(argv[0]).read().split("[")
     lst = ['[' + i for i in lst if i]
     for oplist in lst:
-        print len(parse(oplist, namespace=alldict).operations)
+        loop = parse(oplist, namespace=alldict)
+        num_ops = 0
+        num_dmp = 0
+        num_guards = 0
+        for op in loop.operations:
+            if op.opnum == rop.DEBUG_MERGE_POINT:
+                num_dmp += 1
+            else:
+                num_ops += 1
+            if op.is_guard():
+                num_guards += 1
+        print "Loop, length: %d, opcodes: %d, guards: %d" % (num_ops, num_dmp, num_guards)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
