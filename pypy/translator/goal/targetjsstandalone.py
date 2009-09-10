34902c082ade7f4b15d90893dda32884414a0a56
Jan de Mooij <jandem@codespeak.net>
2009-09-10 13:10:12 +0000

    update targetjsstandalone.py

--- pypy/translator/goal/targetjsstandalone.py
+++ pypy/translator/goal/targetjsstandalone.py
@@ -1,26 +1,45 @@
 """
 A simple standalone target for the javascript interpreter.
+
+Usage: js-c [-f] jsourcefile [-f] other_source ...
 """
 
 import sys
+
 from pypy.lang.js.interpreter import *
-from pypy.lang.js.jsobj import ExecutionReturned
+from pypy.lang.js.console import JSConsole
 
 # __________  Entry point  __________
 
-interp = Interpreter()
+def run_file(interp, name):
+    t = load_file(name)
+    interp.run(t)    
 
 def entry_point(argv):
-    if len(argv) == 2:
-        t = load_file(argv[1])
-        interp.run(t)
+    i = 1
+    
+    if len(argv) == 1:
+        console = JSConsole()
+        console.interact()
         return 0
-    elif argv[0] == 'foo':
-        raise ExecutionReturned(None)
-    else:
-        print "Usage: %s jsourcefile" % argv[0]
-        return 1
-
+    
+    interp = Interpreter()
+    while i < len(argv):
+        arg = argv[i]
+        if arg == '-f':
+            if i == len(argv) - 1:
+                print __doc__
+                return 1
+            i += 1
+            run_file(interp, argv[i])
+        elif arg.startswith('-'):
+            print "Unsupported option %s" % arg
+            print __doc__
+            return 1
+        else:
+            run_file(interp, argv[i])
+        i += 1
+    return 0
 # _____ Define and setup target ___
 
 def target(driver, args):
