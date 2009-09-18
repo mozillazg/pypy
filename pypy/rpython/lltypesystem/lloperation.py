1bc41e8a8e95eba2c5a310c589b28729ba56e5a9
Armin Rigo <arigo@tunes.org>
2009-09-18 11:58:19 +0000

    Uh?  Nonsense.

--- pypy/rpython/lltypesystem/lloperation.py
+++ pypy/rpython/lltypesystem/lloperation.py
@@ -418,7 +418,7 @@ LL_OPERATIONS = {
     # for asmgcroot support to get the address of various static structures
     # see translator/c/src/mem.h for the valid indices
     'gc_asmgcroot_static':  LLOp(sideeffects=False),
-    'gc_stack_bottom':      LLOp(sideeffects=False, canrun=True),
+    'gc_stack_bottom':      LLOp(canrun=True),
 
     # NOTE NOTE NOTE! don't forget *** canunwindgc=True *** for anything that
     # can go through a stack unwind, in particular anything that mallocs!
