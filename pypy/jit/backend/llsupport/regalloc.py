cdb6fc24a37b8881d75f4cde08dc0b402f92b256
Maciej Fijalkowski <fijall@gmail.com>
2009-09-28 11:31:10 +0000

    This is more or less it, except that we might not have enough asserts in tests (maybe)

--- pypy/jit/backend/llsupport/regalloc.py
+++ pypy/jit/backend/llsupport/regalloc.py
@@ -123,7 +123,7 @@ class RegisterManager(object):
         del self.reg_bindings[v_to_spill]
         if self.stack_manager.get(v_to_spill) is None:
             newloc = self.stack_manager.loc(v_to_spill)
-            self.assembler.regalloc_store(loc, newloc)
+            self.assembler.regalloc_mov(loc, newloc)
         return loc
 
     def pick_variable_to_spill(self, v, forbidden_vars, selected_reg=None,
@@ -168,3 +168,82 @@ class RegisterManager(object):
             return self.reg_bindings[box]
         except KeyError:
             return self.stack_manager.loc(box)
+
+    def return_constant(self, v, forbidden_vars=[], selected_reg=None,
+                        imm_fine=True):
+        assert isinstance(v, Const)
+        if selected_reg or not imm_fine:
+            # this means we cannot have it in IMM, eh
+            if selected_reg in self.free_regs:
+                self.assembler.regalloc_mov(self.convert_to_imm(v), selected_reg)
+                return selected_reg
+            if selected_reg is None and self.free_regs:
+                loc = self.free_regs.pop()
+                self.assembler.regalloc_mov(self.convert_to_imm(v), loc)
+                return loc
+            loc = self._spill_var(v, forbidden_vars, selected_reg)
+            self.free_regs.append(loc)
+            self.Load(v, convert_to_imm(v), loc)
+            return loc
+        return convert_to_imm(v)
+
+    def make_sure_var_in_reg(self, v, forbidden_vars=[], selected_reg=None,
+                             imm_fine=True, need_lower_byte=False):
+        if isinstance(v, Const):
+            return self.return_constant(v, forbidden_vars, selected_reg,
+                                        imm_fine)
+        
+        prev_loc = self.loc(v)
+        loc = self.force_allocate_reg(v, forbidden_vars, selected_reg,
+                                      need_lower_byte=need_lower_byte)
+        if prev_loc is not loc:
+            self.assembler.regalloc_mov(prev_loc, loc)
+        return loc
+
+    def reallocate_from_to(self, from_v, to_v):
+        reg = self.reg_bindings[from_v]
+        del self.reg_bindings[from_v]
+        self.reg_bindings[to_v] = reg
+
+    def move_variable_away(self, v, prev_loc):
+        reg = None
+        if self.free_regs:
+            loc = self.free_regs.pop()
+            self.reg_bindings[v] = loc
+            self.assembler.regalloc_mov(prev_loc, loc)
+        else:
+            loc = self.stack_manager.loc(v)
+            self.assembler.regalloc_mov(prev_loc, loc)
+
+    def force_result_in_reg(self, result_v, v, forbidden_vars=[]):
+        """ Make sure that result is in the same register as v
+        and v is copied away if it's further used
+        """
+        if isinstance(v, Const):
+            loc = self.make_sure_var_in_reg(v, forbidden_vars,
+                                            imm_fine=False)
+            self.reg_bindings[result_v] = loc
+            self.free_regs = [reg for reg in self.free_regs if reg is not loc]
+            return loc
+        if v not in self.reg_bindings:
+            prev_loc = self.stack_manager.loc(v)
+            loc = self.force_allocate_reg(v, forbidden_vars)
+            self.assembler.regalloc_mov(prev_loc, loc)
+        assert v in self.reg_bindings
+        if self.longevity[v][1] > self.position:
+            # we need to find a new place for variable v and
+            # store result in the same place
+            loc = self.reg_bindings[v]
+            del self.reg_bindings[v]
+            if self.stack_manager.get(v) is None:
+                self.move_variable_away(v, loc)
+            self.reg_bindings[result_v] = loc
+        else:
+            self.reallocate_from_to(v, result_v)
+            loc = self.reg_bindings[result_v]
+        return loc
+
+    # abstract methods, override
+
+    def convert_to_imm(self, c):
+        raise NotImplementedError("Abstract")
