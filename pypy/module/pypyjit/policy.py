24e6ccb0873fc2554ddac8a6ac9e701c422f0e8e
Maciej Fijalkowski <fijall@gmail.com>
2009-09-30 23:31:45 +0000

    We do support floats, but force_cast is a bit too much for us so far, ignore, keep in mind

--- pypy/module/pypyjit/policy.py
+++ pypy/module/pypyjit/policy.py
@@ -35,9 +35,11 @@ class PyPyJitPolicy(JitPolicy):
             # gc_id operation
             if func.__name__ == 'id__ANY':
                 return False
-        # floats
         if mod == 'pypy.rlib.rbigint':
             #if func.__name__ == '_bigint_true_divide':
+            return False
+        if mod == 'pypy.rpython.lltypesystem.ll_math':
+            # XXX temporary, contains force_cast
             return False
         if '_geninterp_' in func.func_globals: # skip all geninterped stuff
             return False
