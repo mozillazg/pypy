6786e9f47dbecbf47630e5e917c320805e27718d
Maciej Fijalkowski <fijall@gmail.com>
2009-10-01 09:46:01 +0000

    get/set arrayitem tests for floats

--- pypy/jit/backend/test/runner_test.py
+++ pypy/jit/backend/test/runner_test.py
@@ -961,6 +961,19 @@ class LLtypeBackendTest(BaseBackendTest):
             descr_B)
         assert isinstance(x, BoxPtr)
         assert x.getref(lltype.Ptr(A)) == a
+        C = lltype.GcArray(lltype.Float)
+        c = lltype.malloc(C, 6)
+        c[3] = 3.5
+        descr_C = cpu.arraydescrof(C)
+        x = cpu.do_getarrayitem_gc(
+            BoxPtr(lltype.cast_opaque_ptr(llmemory.GCREF, c)), BoxInt(3),
+            descr_C)
+        assert isinstance(x, BoxFloat)
+        assert x.getfloat() == 3.5
+        cpu.do_setarrayitem_gc(
+            BoxPtr(lltype.cast_opaque_ptr(llmemory.GCREF, c)), BoxInt(4),
+            BoxFloat(4.5), descr_C)
+        assert c[4] == 4.5
         #
         s = rstr.mallocstr(6)
         x = cpu.do_strlen(
