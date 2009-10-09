08aec030293b679047f0d5179bda540b6bf8a55c
Armin Rigo <arigo@tunes.org>
2009-10-09 11:05:56 +0000

    Fix translation.  A lot of efforts for what is finally just one line...

--- pypy/translator/c/node.py
+++ pypy/translator/c/node.py
@@ -914,6 +914,9 @@ class GroupNode(ContainerNode):
         return self.obj.name
 
     def enum_dependencies(self):
+        # note: for the group used by the GC, it can grow during this phase,
+        # which means that we might not return all members yet.  This is
+        # fixed by finish_tables() in rpython/memory/gctransform/framework.py
         for member in self.obj.members:
             yield member._as_ptr()
 
