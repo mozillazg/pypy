46c784ad7b2de719f11da171d4f71811652f57a5
Samuele Pedroni <pedronis@lucediurna.net>
2009-10-12 13:41:02 +0000

    (pedronis, cfbolz): kill an unpack

--- pypy/module/_weakref/interp__weakref.py
+++ pypy/module/_weakref/interp__weakref.py
@@ -143,12 +143,7 @@ class W_Weakref(W_WeakrefBase):
         return w_obj
         
 
-def descr__new__weakref(space, w_subtype, w_obj, __args__):
-    args_w, kw_w = __args__.unpack()
-    if args_w:
-        w_callable = args_w[0]
-    else:
-        w_callable = space.w_None
+def descr__new__weakref(space, w_subtype, w_obj, w_callable=None):
     lifeline = w_obj.getweakref()
     if lifeline is None:
         lifeline = WeakrefLifeline(space)
@@ -189,7 +184,7 @@ W_Weakref.typedef = TypeDef("weakref",
 which is called with the weak reference as an argument when 'obj'
 is about to be finalized.""",
     __new__ = interp2app(descr__new__weakref,
-                         unwrap_spec=[ObjSpace, W_Root, W_Root, Arguments]),
+                         unwrap_spec=[ObjSpace, W_Root, W_Root, W_Root]),
     __eq__ = interp2app(descr__eq__,
                         unwrap_spec=[ObjSpace, W_Weakref, W_Root]),
     __ne__ = interp2app(descr__ne__,
