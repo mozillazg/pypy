a6c4c050288e80259a5e06151fd7405e01e83b94
Samuele Pedroni <pedronis@lucediurna.net>
2009-10-14 16:29:40 +0000

    (pedronis, cfbolz): sprinkle unroll_safe in strategic places. clean up the policy of the Python interpreter

--- pypy/jit/metainterp/policy.py
+++ pypy/jit/metainterp/policy.py
@@ -43,11 +43,10 @@ class JitPolicy(object):
             contains_loop = contains_loop and not getattr(
                     func, '_jit_unroll_safe_', False)
 
-        res = (see_function and not contains_loop and
-               not unsupported)
-        if not res and see_function and not unsupported:
+        res = see_function and not unsupported
+        if res and contains_loop:
             self.unsafe_loopy_graphs.add(graph)
-        return res
+        return res and not contains_loop
 
     def graphs_from(self, op, rtyper, supports_floats):
         if op.opname == 'direct_call':
