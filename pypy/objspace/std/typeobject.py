7d2bb9e0faf16aaa4b6dc6c8faf6c256f5811245
Carl Friedrich Bolz <cfbolz@gmx.de>
2009-10-26 13:46:32 +0000

    Refactor the multidict to use one level of indirection less. Instead of having an indirection, all W_DictMultiObjects have an attribute r_dict_content that is usually None. When the dict devolves, it is set to an r_dict and used instead of the normal attributes.

--- pypy/objspace/std/typeobject.py
+++ pypy/objspace/std/typeobject.py
@@ -281,7 +281,7 @@ class W_TypeObject(W_Object):
         if w_self.lazyloaders:
             w_self._freeze_()    # force un-lazification
         space = w_self.space
-        newdic = space.DictObjectCls(space)
+        newdic = space.newdict()
         newdic.initialize_from_strdict_shared(w_self.dict_w)
         return W_DictProxyObject(newdic)
 
