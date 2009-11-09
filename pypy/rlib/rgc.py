1ed4eab606f5662aaeadaf425b0be902b6acdacb
Maciej Fijalkowski <fijall@gmail.com>
2009-11-09 15:36:35 +0000

    Rename dump_heap to heap_stats here as well

--- pypy/rlib/rgc.py
+++ pypy/rlib/rgc.py
@@ -184,11 +184,11 @@ class CanMoveEntry(ExtRegistryEntry):
         hop.exception_cannot_occur()
         return hop.genop('gc_can_move', hop.args_v, resulttype=hop.r_result)
 
-def _dump_heap():
+def _heap_stats():
     raise NotImplementedError # can't be run directly
 
 class DumpHeapEntry(ExtRegistryEntry):
-    _about_ = _dump_heap
+    _about_ = _heap_stats
 
     def compute_result_annotation(self):
         from pypy.annotation import model as annmodel
@@ -201,7 +201,7 @@ class DumpHeapEntry(ExtRegistryEntry):
         from pypy.rpython.memory.gc.base import ARRAY_TYPEID_MAP
         from pypy.rpython.lltypesystem import lltype
         hop.exception_is_here()
-        return hop.genop('gc_dump_heap', [], resulttype=hop.r_result)
+        return hop.genop('gc_heap_stats', [], resulttype=hop.r_result)
 
 def malloc_nonmovable(TP, n=None, zero=False):
     """ Allocate a non-moving buffer or return nullptr.
