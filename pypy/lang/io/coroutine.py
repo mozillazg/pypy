1a9a5fa8c7512573b928ef5f0f8816cde192e51e
David Schneider <david.schneider@picle.org>
2009-11-09 20:35:25 +0000

    Fix some lookup related coroutine issues

--- pypy/lang/io/coroutine.py
+++ pypy/lang/io/coroutine.py
@@ -9,24 +9,13 @@ def coroutine_get_current(space, w_target, w_message, w_context):
 @register_method('Coroutine', 'isCurrent')
 def coroutine_is_current(space, w_target, w_message, w_context):
     return space.newbool(w_target is W_Coroutine.w_getcurrent(space))
-    
-@register_method('Coroutine', 'setRunMessage', unwrap_spec=[object, object])
-def coroutine_setRunMessage(space, w_coroutine, w_message):
-    w_coroutine.slots['runMessage'] = w_message
-    return w_coroutine
-    
+        
 @register_method('Coroutine', 'run')
 def coroutine_run(space, w_target, w_message, w_context):
-    w_target.run(space, w_target, w_context)
+    # XXX check this, because w_target.run(space, w_context, w_context) also works, maybe missing some scenarios
+    w_target.run(space, w_target, w_target)
     return w_target
     
-# @register_method('Coroutine', 'setResult', unwrap_spec=[object, object])
-# def coroutine_set_result(space, w_coro, w_result):
-#     print w_result
-#     w_coro.slots['result'] = w_result
-#     return w_coro
-#     
-
 @register_method('Coroutine', 'resume')
 def coroutine_switch(space, w_target, w_message, w_context):
     w_target.switch()
