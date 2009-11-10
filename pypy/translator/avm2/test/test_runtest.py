c96e8f1bef0a582a5685241ff87f5f18ede1caac
Jean-Philippe St. Pierre <magcius@codespeak.net>
2009-11-10 23:55:36 +0000

    Some tests run, for a very limited definition of 'some'.

--- pypy/translator/avm2/test/test_runtest.py
+++ pypy/translator/avm2/test/test_runtest.py
@@ -4,13 +4,7 @@ from pypy.translator.oosupport.test_template.runtest import BaseTestRunTest
 from pypy.translator.avm2.test.runtest import AVM2Test
 
 class TestRunTest(BaseTestRunTest, AVM2Test):
-
-    def test_auto_raise_exc(self):
-        def fn():
-            raise ValueError
-        f = self._compile(fn, [], auto_raise_exc=True)
-        py.test.raises(ValueError, f)
-
+    
     def test_big_arglist(self):
         def fn(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9):
             return a0
