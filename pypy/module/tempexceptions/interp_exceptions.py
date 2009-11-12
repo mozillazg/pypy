83cfc9752779a062430d375bdda7cfaf06300514
Maciej Fijalkowski <fijall@gmail.com>
2009-11-12 10:45:42 +0000

    (arigo, fijal) Slight progress

--- pypy/module/tempexceptions/interp_exceptions.py
+++ pypy/module/tempexceptions/interp_exceptions.py
@@ -72,7 +72,7 @@ BaseException
 
 from pypy.interpreter.baseobjspace import ObjSpace, Wrappable, W_Root
 from pypy.interpreter.typedef import TypeDef, interp_attrproperty_w,\
-     GetSetProperty
+     GetSetProperty, interp_attrproperty, descr_get_dict, descr_set_dict
 from pypy.interpreter.gateway import interp2app
 
 class W_BaseException(Wrappable):
@@ -81,9 +81,11 @@ class W_BaseException(Wrappable):
     The __getitem__ method is provided for backwards-compatibility
     and will be deprecated at some point. 
     """
+    w_dict = None
 
     def __init__(self, space, args_w):
         self.args_w = args_w
+        self.space = space
         if len(args_w) == 1:
             self.w_message = args_w[0]
         else:
@@ -111,6 +113,17 @@ class W_BaseException(Wrappable):
     def descr_getargs(space, self):
         return space.newtuple(self.args_w)
 
+    def getdict(self):
+        if self.w_dict is None:
+            self.w_dict = self.space.newdict()
+        return self.w_dict
+
+    def setdict(self, space, w_dict):
+        if not space.is_true(space.isinstance( w_dict, space.w_dict )):
+            raise OperationError( space.w_TypeError, space.wrap("setting exceptions's dictionary to a non-dict") )
+        self.w_dict = w_dict
+
+
 def _new(cls):
     def descr_new_base_exception(space, w_subtype, args_w):
         exc = space.allocate_instance(cls, w_subtype)
@@ -126,16 +139,111 @@ W_BaseException.typedef = TypeDef(
     __new__ = _new(W_BaseException),
     __str__ = interp2app(W_BaseException.descr_str),
     __repr__ = interp2app(W_BaseException.descr_repr),
+    __dict__ = GetSetProperty(descr_get_dict, descr_set_dict,
+                              cls=W_BaseException),
     message = interp_attrproperty_w('w_message', W_BaseException),
     args = GetSetProperty(W_BaseException.descr_getargs),
 )
 
-class W_Exception(W_BaseException):
-    """Common base class for all non-exit exceptions."""
+def _new_exception(name, base, docstring, **kwargs):
+    class W_Exception(base):
+        __doc__ = docstring
 
-W_Exception.typedef = TypeDef(
-    'Exception',
-    W_BaseException.typedef,
-    __doc__ = W_Exception.__doc__,
-    __new__ = _new(W_Exception),
+    W_Exception.__name__ = 'W_' + name
+
+    for k, v in kwargs.items():
+        kwargs[k] = interp2app(v.__get__(None, W_Exception))
+    W_Exception.typedef = TypeDef(
+        name,
+        base.typedef,
+        __doc__ = W_Exception.__doc__,
+        __new__ = _new(W_Exception),
+        **kwargs
+    )
+    return W_Exception
+
+W_Exception = _new_exception('Exception', W_BaseException,
+                         """Common base class for all non-exit exceptions.""")
+
+W_GeneratorExit = _new_exception('GeneratorExit', W_Exception,
+                          """Request that a generator exit.""")
+
+W_StandardError = _new_exception('StandardError', W_Exception,
+                         """Base class for all standard Python exceptions.""")
+
+W_ValueError = _new_exception('ValueError', W_StandardError,
+                         """Inappropriate argument value (of correct type).""")
+
+W_ImportError = _new_exception('ImportError', W_StandardError,
+                  """Import can't find module, or can't find name in module.""")
+
+W_RuntimeError = _new_exception('RuntimeError', W_StandardError,
+                     """Unspecified run-time error.""")
+
+W_UnicodeError = _new_exception('UnicodeError', W_ValueError,
+                          """Unicode related error.""")
+
+
+class W_UnicodeTranslateError(W_UnicodeError):
+    """Unicode translation error."""
+    def __init__(self, space, w_obj, w_start, w_end, w_reason):
+        self.object = space.unicode_w(w_obj)
+        self.start = space.int_w(w_start)
+        self.end = space.int_w(w_end)
+        self.reason = space.str_w(w_reason)
+        W_BaseException.__init__(self, space, [w_obj, w_start, w_end, w_reason])
+
+    def descr_str(self, space):
+        return space.appexec([space.wrap(self)], """(self):
+            if self.end == self.start + 1:
+                badchar = ord(self.object[self.start])
+                if badchar <= 0xff:
+                    return "can't translate character u'\\\\x%02x' in position %d: %s" % (badchar, self.start, self.reason)
+                if badchar <= 0xffff:
+                    return "can't translate character u'\\\\u%04x' in position %d: %s"%(badchar, self.start, self.reason)
+                return "can't translate character u'\\\\U%08x' in position %d: %s"%(badchar, self.start, self.reason)
+            return "can't translate characters in position %d-%d: %s" % (self.start, self.end - 1, self.reason)
+        """)
+    descr_str.unwrap_spec = ['self', ObjSpace]
+
+def descr_new_unicode_translate_error(space, w_subtype, w_obj, w_start, w_end,
+                                      w_reason):
+    exc = space.allocate_instance(W_UnicodeTranslateError, w_subtype)
+    W_UnicodeTranslateError.__init__(exc, space, w_obj, w_start,
+                                     w_end, w_reason)
+    return space.wrap(exc)
+
+def readwrite_attrproperty(name, cls, unwrapname):
+    def fget(space, obj):
+        return space.wrap(getattr(obj, name))
+    def fset(space, obj, w_val):
+        setattr(obj, name, getattr(space, unwrapname)(w_val))
+    return GetSetProperty(fget, fset, cls=cls)
+
+W_UnicodeTranslateError.typedef = TypeDef(
+    'UnicodeTranslateError',
+    W_UnicodeError.typedef,
+    __doc__ = W_UnicodeTranslateError.__doc__,
+    __new__ = interp2app(descr_new_unicode_translate_error),
+    __str__ = interp2app(W_UnicodeTranslateError.descr_str),
+    object = readwrite_attrproperty('object', W_UnicodeTranslateError, 'unicode_w'),
+    start  = readwrite_attrproperty('start', W_UnicodeTranslateError, 'int_w'),
+    end    = readwrite_attrproperty('end', W_UnicodeTranslateError, 'int_w'),
+    reason = readwrite_attrproperty('reason', W_UnicodeTranslateError, 'str_w'),
 )
+
+W_LookupError = _new_exception('LookupError', W_StandardError,
+                               """Base class for lookup errors.""")
+
+def key_error_str(self, space):
+    if len(self.args_w) == 0:
+        return space.wrap('')
+    elif len(self.args_w) == 1:
+        return space.repr(self.args_w[0])
+    else:
+        return space.str(space.newtuple(self.args_w))
+key_error_str.unwrap_spec = ['self', ObjSpace]
+    
+W_KeyError = _new_exception('KeyError', W_LookupError,
+                            """Mapping key not found.""",
+                            __str__ = key_error_str)
--- pypy/module/tempexceptions/test/test_exc.py
+++ pypy/module/tempexceptions/test/test_exc.py
@@ -20,6 +20,9 @@ class AppTestExc(object):
         assert repr(BaseException(3, "x")) == "BaseException(3, 'x')"
         assert str(BaseException(3, "x")) == "(3, 'x')"
         assert BaseException(3, "x").message == ''
+        x = BaseException()
+        x.xyz = 3
+        assert x.xyz == 3
 
     def test_exc(self):
         from tempexceptions import Exception, BaseException
@@ -28,3 +31,34 @@ class AppTestExc(object):
         assert isinstance(Exception(), Exception)
         assert isinstance(Exception(), BaseException)
         assert repr(Exception(3, "x")) == "Exception(3, 'x')"
+
+    def test_custom_class(self):
+        from tempexceptions import Exception
+
+        class MyException(Exception):
+            def __init__(self, x):
+                self.x = x
+
+            def __str__(self):
+                return self.x
+
+        assert issubclass(MyException, Exception)
+        assert str(MyException("x")) == "x"
+
+    def test_unicode_translate_error(self):
+        from tempexceptions import UnicodeTranslateError
+        ut = UnicodeTranslateError(u"x", 1, 5, "bah")
+        assert ut.object == u'x'
+        assert ut.start == 1
+        assert ut.end == 5
+        assert ut.reason == 'bah'
+        assert ut.args == (u'x', 1, 5, 'bah')
+        assert ut.message == ''
+        ut.object = u'y'
+        assert ut.object == u'y'
+        assert str(ut) == "can't translate characters in position 1-4: bah"
+
+    def test_key_error(self):
+        from tempexceptions import KeyError
+
+        assert str(KeyError('s')) == "'s'"
