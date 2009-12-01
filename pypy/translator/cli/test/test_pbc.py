453c550734d3eebb7b41d1acc5ddd84ffdc6bf75
Antonio Cuni <anto.cuni@gmail.com>
2009-12-01 16:55:00 +0000

    aesthetic changes

--- pypy/translator/cli/test/test_pbc.py
+++ pypy/translator/cli/test/test_pbc.py
@@ -2,5 +2,7 @@ import py
 from pypy.translator.cli.test.runtest import CliTest
 from pypy.rpython.test.test_rpbc import BaseTestRPBC
 
+# ====> ../../../rpython/test/test_rpbc.py
+
 class TestCliPBC(CliTest, BaseTestRPBC):
     pass
