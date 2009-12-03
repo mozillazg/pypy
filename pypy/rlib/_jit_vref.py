eada7074fffbc5ad21330a4f9d752e64e68514e2
Armin Rigo <arigo@tunes.org>
2009-12-03 17:56:29 +0000

    Rtyping.  Still no real work.

--- pypy/rlib/_jit_vref.py
+++ pypy/rlib/_jit_vref.py
@@ -1,4 +1,6 @@
 from pypy.annotation import model as annmodel
+from pypy.rpython.rclass import getinstancerepr
+from pypy.rpython.rmodel import Repr
 
 
 class SomeVRef(annmodel.SomeObject):
@@ -8,3 +10,28 @@ class SomeVRef(annmodel.SomeObject):
 
     def simple_call(self):
         return self.s_instance
+
+    def rtyper_makerepr(self, rtyper):
+        return get_vref(rtyper)
+
+    def rtyper_makekey(self):
+        return self.__class__,
+
+
+def specialize_call(hop):
+    [v] = hop.inputargs(getinstancerepr(hop.rtyper, None))
+    return v
+
+def get_vref(rtyper):
+    if not hasattr(rtyper, '_vrefrepr'):
+        rtyper._vrefrepr = VRefRepr(rtyper)
+    return rtyper._vrefrepr
+
+
+class VRefRepr(Repr):
+    def __init__(self, rtyper):
+        self.lowleveltype = getinstancerepr(rtyper, None).lowleveltype
+
+    def rtype_simple_call(self, hop):
+        [v] = hop.inputargs(self)
+        return hop.genop('cast_pointer', [v], resulttype = hop.r_result)
