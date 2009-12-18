2cafba8bf82cf5e55b7071a7ae93af09bfaeaa24
Armin Rigo <arigo@tunes.org>
2009-12-18 18:14:24 +0000

    Make test_jump.py pass again.

--- pypy/jit/backend/x86/test/test_jump.py
+++ pypy/jit/backend/x86/test/test_jump.py
@@ -1,5 +1,7 @@
-from pypy.jit.backend.x86.ri386 import *
+from pypy.jit.metainterp.history import ConstInt
 from pypy.jit.backend.x86.regalloc import X86FrameManager
+from pypy.jit.backend.x86.regalloc import StackLoc, RegLoc
+from pypy.jit.backend.x86.regalloc import eax, ebx, ecx, edx, esi, edi
 from pypy.jit.backend.x86.jump import remap_frame_layout
 
 frame_pos = X86FrameManager.frame_pos
@@ -8,8 +10,15 @@ class MockAssembler:
     def __init__(self):
         self.ops = []
 
-    def regalloc_mov(self, from_loc, to_loc):
-        self.ops.append(('mov', from_loc, to_loc))
+    def regalloc_load(self, from_loc, to_loc):
+        assert isinstance(from_loc, (RegLoc, StackLoc, ConstInt))
+        assert isinstance(to_loc, RegLoc)
+        self.ops.append(('load', from_loc, to_loc))
+
+    def regalloc_store(self, from_loc, to_loc):
+        assert isinstance(from_loc, (RegLoc, ConstInt))
+        assert isinstance(to_loc, StackLoc)
+        self.ops.append(('store', from_loc, to_loc))
 
     def regalloc_push(self, loc):
         self.ops.append(('push', loc))
@@ -25,11 +34,7 @@ class MockAssembler:
                 continue
             assert len(op1) == len(op2)
             for x, y in zip(op1, op2):
-                if isinstance(x, MODRM) and isinstance(y, MODRM):
-                    assert x.byte == y.byte
-                    assert x.extradata == y.extradata
-                else:
-                    assert x == y
+                assert x == y
         assert len(self.ops) == len(expected)
         return True
 
@@ -52,9 +57,9 @@ def test_trivial():
 def test_simple_registers():
     assembler = MockAssembler()
     remap_frame_layout(assembler, [eax, ebx, ecx], [edx, esi, edi], '?')
-    assert assembler.ops == [('mov', eax, edx),
-                             ('mov', ebx, esi),
-                             ('mov', ecx, edi)]
+    assert assembler.ops == [('load', eax, edx),
+                             ('load', ebx, esi),
+                             ('load', ecx, edi)]
 
 def test_simple_framelocs():
     assembler = MockAssembler()
@@ -63,10 +68,10 @@ def test_simple_framelocs():
     s20 = frame_pos(20, 1)
     s24 = frame_pos(221, 1)
     remap_frame_layout(assembler, [s8, eax, s12], [s20, s24, edi], edx)
-    assert assembler.ops == [('mov', s8, edx),
-                             ('mov', edx, s20),
-                             ('mov', eax, s24),
-                             ('mov', s12, edi)]
+    assert assembler.ops == [('load', s8, edx),
+                             ('store', edx, s20),
+                             ('store', eax, s24),
+                             ('load', s12, edi)]
 
 def test_reordering():
     assembler = MockAssembler()
@@ -76,10 +81,10 @@ def test_reordering():
     s24 = frame_pos(1, 1)
     remap_frame_layout(assembler, [eax, s8, s20, ebx],
                                   [s8, ebx, eax, edi], '?')
-    assert assembler.got([('mov', ebx, edi),
-                          ('mov', s8, ebx),
-                          ('mov', eax, s8),
-                          ('mov', s20, eax)])
+    assert assembler.got([('load', ebx, edi),
+                          ('load', s8, ebx),
+                          ('store', eax, s8),
+                          ('load', s20, eax)])
 
 def test_cycle():
     assembler = MockAssembler()
@@ -90,9 +95,9 @@ def test_cycle():
     remap_frame_layout(assembler, [eax, s8, s20, ebx],
                                   [s8, ebx, eax, s20], '?')
     assert assembler.got([('push', s8),
-                          ('mov', eax, s8),
-                          ('mov', s20, eax),
-                          ('mov', ebx, s20),
+                          ('store', eax, s8),
+                          ('load', s20, eax),
+                          ('store', ebx, s20),
                           ('pop', ebx)])
 
 def test_cycle_2():
@@ -107,37 +112,37 @@ def test_cycle_2():
                        [eax, s8, edi, s20, eax, s20, s24, esi, s2, s3],
                        [s8, s20, edi, eax, edx, s24, ebx, s12, s3, s2],
                        ecx)
-    assert assembler.got([('mov', eax, edx),
-                          ('mov', s24, ebx),
-                          ('mov', esi, s12),
-                          ('mov', s20, ecx),
-                          ('mov', ecx, s24),
+    assert assembler.got([('load', eax, edx),
+                          ('load', s24, ebx),
+                          ('store', esi, s12),
+                          ('load', s20, ecx),
+                          ('store', ecx, s24),
                           ('push', s8),
-                          ('mov', eax, s8),
-                          ('mov', s20, eax),
+                          ('store', eax, s8),
+                          ('load', s20, eax),
                           ('pop', s20),
                           ('push', s3),
-                          ('mov', s2, ecx),
-                          ('mov', ecx, s3),
+                          ('load', s2, ecx),
+                          ('store', ecx, s3),
                           ('pop', s2)])
 
 def test_constants():
     assembler = MockAssembler()
-    c3 = imm(3)
+    c3 = ConstInt(3)
     remap_frame_layout(assembler, [c3], [eax], '?')
-    assert assembler.ops == [('mov', c3, eax)]
+    assert assembler.ops == [('load', c3, eax)]
     assembler = MockAssembler()
     s12 = frame_pos(12, 1)
     remap_frame_layout(assembler, [c3], [s12], '?')
-    assert assembler.ops == [('mov', c3, s12)]
+    assert assembler.ops == [('store', c3, s12)]
 
 def test_constants_and_cycle():
     assembler = MockAssembler()
-    c3 = imm(3)
+    c3 = ConstInt(3)
     s12 = frame_pos(13, 1)
     remap_frame_layout(assembler, [ebx, c3,  s12],
                                   [s12, eax, ebx], edi)
-    assert assembler.ops == [('mov', c3, eax),
+    assert assembler.ops == [('load', c3, eax),
                              ('push', s12),
-                             ('mov', ebx, s12),
+                             ('store', ebx, s12),
                              ('pop', ebx)]
