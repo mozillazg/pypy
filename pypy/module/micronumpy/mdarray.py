6bceba8301bcdb4a03aeb6d6d77bdd1f1e20dc5d
Daniel Roberts <Ademan555@gmail.com>
2010-01-02 02:02:05 +0000

    Reverted to working revision using merge.

--- pypy/module/micronumpy/mdarray.py
+++ pypy/module/micronumpy/mdarray.py
@@ -25,123 +25,45 @@ def compute_pos(space, indexes, dim):
         current *= d
     return pos
 
-def compute_slices(space, slices, dim):
-    strides = []
-    i = 1
-    for d in dim:
-        strides.append(i)
-        i *= d
-    strides.reverse()
-    length = i
-    shape = []
-    sliceout = []
-    for v in space.unpackiterable(slices):
-        sliceoutnew=[] #FIXME: Not RPYthon - there are no slices.
-        if space.is_true(space.isinstance(v, space.w_slice)):
-            sl=space.unwrap(v)
-            if sl.step<0:
-                reverse=True
-                sl=slice(sl.stop, sl.start, -sl.step)
-            else:
-                reverse=False
-            stride=strides.pop(0)
-            if sl.step == 1:
-                newsl = [slice(stride*sl.start, stride*sl.stop)]
-            else:
-                newsl = []
-                for i in range(sl.start, sl.stop, sl.step):
-                    newsl.append(slice(stride*i, stride*(i+1)))
+def create_mdarray(data_type, unwrap, coerce):
+    class MultiDimArray(BaseNumArray):
+        def __init__(self, space, shape):
+            self.shape = shape
+            self.space = space
+            size = 1
+            for dimension in shape:
+                size *= dimension
+            self.storage = [data_type(0.0)] * size
+            make_sure_not_resized(self.storage)
 
-            if reverse:
-                newsl.reverse()
+        def _unpack_indexes(self, space, w_index):
+            indexes = [space.int_w(w_i) for w_i in space.fixedview(w_index)]
+            if len(indexes) != len(self.shape):
+                raise OperationError(space.w_IndexError, space.wrap(
+                    'Wrong index'))
+            return indexes
 
-            shape.append((sl.stop-sl.start)//sl.step)
+        def getitem(self, w_index):
+            space = self.space
+            indexes = self._unpack_indexes(space, w_index)
+            pos = compute_pos(space, indexes, self.shape)
+            return space.wrap(self.storage[pos])
 
-            #here multiple old slices x new slices.
-            for sl in sliceout:
-                for sl2 in newsl:
-                    pass #I have no time
+        def setitem(self, w_index, w_value):
+            space = self.space
+            indexes = self._unpack_indexes(space, w_index)
+            pos = compute_pos(space, indexes, self.shape)
+            self.storage[pos] = coerce(space, w_value)
+            return space.w_None #XXX: necessary?
 
-        else:
-            #extract item from slices, without appending to shape
+        def len(self):
+            space = self.space
+            return space.wrap(self.shape[0])
+    return MultiDimArray
 
-        sliceout = sliceoutnew
-
-    return shape, sliceout
-
-#Was undetectable
-class MultiDimArrayAbstract(BaseNumArray):
-
-    def dtype(self, data):
-        return self.__class__.data_type(data)
-
-    def unwrap(self, w_data): #XXX: NOT USED
-        return self.__class__.data_w(w_data)
-
-    def coerce(self, data):
-        return self.__class__.data_coerce(data)
-
-    def __init__(self, space, shape):
-        self.shape = shape
-        self.space = space
-        size = 1
-        for dimension in shape:
-            size *= dimension
-        self.storage = [self.dtype(0.0)] * size
-        make_sure_not_resized(self.storage)
-
-    def _unpack_indexes(self, space, w_index):
-        indexes = [space.int_w(w_i) for w_i in space.fixedview(w_index)]
-        if len(indexes) != len(self.shape):
-            raise OperationError(space.w_IndexError, space.wrap(
-                'Wrong index'))
-        return indexes
-
-    def getitem(self, w_index):
-        space = self.space
-        indexes = self._unpack_indexes(space, w_index)
-        pos = compute_pos(space, indexes, self.shape)
-        return space.wrap(self.storage[pos])
-
-    def setitem(self, w_index, w_value):
-        space = self.space
-        indexes = self._unpack_indexes(space, w_index)
-        pos = compute_pos(space, indexes, self.shape)
-        self.storage[pos] = self.coerce(space, w_value)
-        return space.w_None #XXX: necessary?
-
-    def len(self):
-        space = self.space
-        return space.wrap(self.shape[0])
-
-    def load_iterable(self, space, w_values):
-        self._load_iter(space, w_values, 0)
-
-    def _load_iter(self, space, w_values, start): #TODO: shape check
-        vals=space.unpackiterable(w_values)
-        if space.is_true(space.isinstance(vals[0], space.w_tuple) or space.is_true(space.isinstance(vals[0], space.w_list):
-            idx=start
-            for v in vals:
-                add=self._load_iter(space, v, idx)
-                idx+=add
-            return idx
-        else:
-            idx=start
-            for v in vals:
-                self.storage[idx]=self.unwrap(val)
-                idx+=1
-            return idx
-                
-
-mdarraytype=MultiDimArrayAbstract
-
-class MultiDimIntArray(MultiDimArrayAbstact):
-    data_type, data_w, data_coerce = int, unwrap_int, coerce_int
-
+MultiDimIntArray = create_mdarray(int, unwrap_int, coerce_int)
 MultiDimArray = MultiDimIntArray #XXX: compatibility
-
-class MultiDimFloatArray(MultiDimArrayAbstact):
-    data_type, data_w, data_coerce = float, unwrap_float, coerce_float
+MultiDimFloatArray = create_mdarray(float, unwrap_float, coerce_float)
 
 class ResultFactory(object):
     def __init__(self, space):
--- pypy/module/micronumpy/ufunc.py
+++ pypy/module/micronumpy/ufunc.py
@@ -1,16 +1,11 @@
 from pypy.module.micronumpy.ndarray import array, zeros, ndarray
-from pypy.module.micronumpy.sdarray import sdarrytype
-from pypy.module.micronumpy.mdarray import mdarrytype
 from pypy.interpreter.baseobjspace import ObjSpace, W_Root
 from pypy.interpreter.error import OperationError
 
-def _assert_both(space, w_a, w_b):
+def minimum(space, w_a, w_b):
     if not isinstance(w_a, ndarray) or not isinstance(w_b, ndarray):
         raise OperationError(space.w_TypeError,
                              space.wrap("expecting ndarray object"))
-
-def minimum(space, w_a, w_b):
-    _assert_both(w_a, w_b)
     if w_a.array.length != w_b.array.length:
         raise OperationError(space.w_ValueError,
                              space.wrap("minimum of arrays of different length"))
@@ -24,15 +19,3 @@ def minimum(space, w_a, w_b):
             res.array.storage[i] = two
     return space.wrap(res)
 minimum.unwrap_spec = [ObjSpace, W_Root, W_Root]
-
-def dot(space, w_a, w_b):
-    _assert_both(w_a, w_b)
-    if len(w_b.array.shape)==1:
-        w_b_new=zeros(space, space.newtuple([space.wrap(1), space.wrap(w_b.array.shape[0])]))
-        for idx, value in enumerate(w_b.array.storage):
-            w_b_new.array.storage[idx]=value
-        w_b=w_b_new
-    
-    #waiting for slice.
-
-
