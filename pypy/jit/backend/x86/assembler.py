8ce6db314750c1635e0f411829f633fd110e3c71
Maciej Fijalkowski <fijall@gmail.com>
2010-01-13 11:19:08 +0000

    Improve debugging

--- pypy/jit/backend/x86/assembler.py
+++ pypy/jit/backend/x86/assembler.py
@@ -171,6 +171,7 @@ class Assembler386(object):
         self._patch_stackadjust(adr_stackadjust, frame_depth+param_depth)
         looptoken._x86_frame_depth = frame_depth
         looptoken._x86_param_depth = param_depth
+        debug_print("Loop #", looptoken.number, "has address", looptoken._x86_loop_code)
 
     def assemble_bridge(self, faildescr, inputargs, operations):
         self.make_sure_mc_exists()
@@ -193,6 +194,9 @@ class Assembler386(object):
             faildescr._x86_bridge_param_depth = param_depth
         # patch the jump from original guard
         self.patch_jump(faildescr, adr_bridge)
+        debug_print("Bridge out of guard",
+                    self.cpu.get_fail_descr_number(faildescr),
+                    "has address", adr_bridge)
 
     def patch_jump(self, faildescr, adr_new_target):
         adr_jump_offset = faildescr._x86_adr_jump_offset
