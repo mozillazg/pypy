dd720462f1b4b4294509c47b9f0e9a5d566c3855
Armin Rigo <arigo@tunes.org>
2010-01-30 14:59:23 +0000

    First test.

---
+++ pypy/jit/metainterp/test/test_two.py
@@ -0,0 +1,34 @@
+import py
+from pypy.rlib.jit import JitDriver
+from pypy.jit.metainterp.test.test_basic import LLJitMixin
+
+
+class TwoJitsTests:
+
+    def test_simple(self):
+        myjitdriver1 = JitDriver(greens = [], reds = ['n'])
+        def f1(n):
+            while n > 0:
+                myjitdriver1.can_enter_jit(n=n)
+                myjitdriver1.jit_merge_point(n=n)
+                n -= 10
+            return n
+        #
+        myjitdriver2 = JitDriver(greens = [], reds = ['m', 'x'])
+        def f2(m, x):
+            while m > 0:
+                myjitdriver2.can_enter_jit(m=m, x=x)
+                myjitdriver2.jit_merge_point(m=m, x=x)
+                m -= 1
+                x += 7
+            return x
+        #
+        def main(n, m, x):
+            return f1(n) * f2(m, x)
+        #
+        res = self.meta_interp(main, [78, 3, 0])
+        assert res == -2 * 21
+
+
+class TestLLtype(TwoJitsTests, LLJitMixin):
+    pass
