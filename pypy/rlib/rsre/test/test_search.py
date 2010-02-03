a2cac249fc0c90f21ff5270e21cab7f0a23d52af
Armin Rigo <arigo@tunes.org>
2010-02-03 11:54:49 +0000

    Test and fix.

--- pypy/rlib/rsre/test/test_search.py
+++ pypy/rlib/rsre/test/test_search.py
@@ -1,17 +1,27 @@
+import re, _sre
 from pypy.rlib.rsre import rsre
-
-
-# <item>\s*<title>(.*?)</title>
-r_code1 = [17, 18, 1, 21, 131091, 6, 6, 60, 105, 116, 101, 109, 62, 0,
-0, 0, 0, 0, 0, 19, 60, 19, 105, 19, 116, 19, 101, 19, 109, 19, 62, 29,
-9, 0, 65535, 15, 4, 9, 2, 0, 1, 19, 60, 19, 116, 19, 105, 19, 116, 19,
-108, 19, 101, 19, 62, 21, 0, 31, 5, 0, 65535, 2, 1, 21, 1, 19, 60, 19,
-47, 19, 116, 19, 105, 19, 116, 19, 108, 19, 101, 19, 62, 1]
 
 
 class TestSearch:
 
-    def test_simple(self):
+    def get_code(self, regexp):
+        class GotIt(Exception):
+            pass
+        def my_compile(pattern, flags, code, *args):
+            raise GotIt(code)
+        saved = _sre.compile
+        try:
+            _sre.compile = my_compile
+            try:
+                re.compile(regexp)
+            except GotIt, e:
+                return e.args[0]
+        finally:
+            _sre.compile = saved
+        assert 0, "did not reach my_compile()?"
+
+    def test_code1(self):
+        r_code1 = self.get_code(r'<item>\s*<title>(.*?)</title>')
         state = rsre.SimpleStringState("foo<item>  <title>abc</title>def")
         res = state.search(r_code1)
         assert res is True
@@ -19,10 +29,21 @@ class TestSearch:
         assert groups[0] == (3, 29)
         assert groups[1] == (18, 21)
 
+    def test_code2_fail(self):
+        r_code2 = self.get_code(r'x((a)|(b)|(c)|(d)|(e)|(f)|(g)|(h)|(i))|(j)')
+        state = rsre.SimpleStringState("i")
+        res = state.match(r_code2)
+        assert res is False
 
-if __name__ == '__main__':
-    import re, _sre
-    def my_compile(pattern, flags, code, *args):
-        print code
-    _sre.compile = my_compile
-    re.compile(r'<item>\s*<title>(.*?)</title>')
+    def test_code2_success(self):
+        r_code2 = self.get_code(r'x((a)|(b)|(c)|(d)|(e)|(f)|(g)|(h)|(i))|(j)')
+        state = rsre.SimpleStringState("j")
+        res = state.match(r_code2)
+        assert res is True
+        groups = state.create_regs(11)
+        assert groups[0] == (0, 1)
+        for i in range(1, 12):
+            if i == 11:
+                assert groups[i] == (0, 1)
+            else:
+                assert groups[i] == (-1, -1)
