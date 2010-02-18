c1e67edf6e0c197b7047d8199af59e60c5b18299
Maciej Fijalkowski <fijall@gmail.com>
2010-02-18 21:12:43 +0000

    I think it's sane to say we should never promote a string

--- pypy/jit/metainterp/codewriter.py
+++ pypy/jit/metainterp/codewriter.py
@@ -836,9 +836,7 @@ class BytecodeMaker(object):
         if hints.get('promote') and op.args[0].concretetype is not lltype.Void:
             self.minimize_variables()
             from pypy.rpython.lltypesystem.rstr import STR
-            if op.args[0].concretetype == STR:
-                import pdb
-                pdb.set_trace()
+            assert op.args[0].concretetype != STR
             self.emit('guard_value', self.var_position(op.args[0]))
             self.register_var(op.result)
         else:
