19f439ad3ce77e1cdd832bb9d9a94e370112c8d6
Armin Rigo <arigo@tunes.org>
2010-02-21 12:06:26 +0000

    Uses 121 instead of 120, as 0x79 is less likely to show up in assembler for unrelated reasons than 0x78.

--- pypy/jit/metainterp/optimizeopt.py
+++ pypy/jit/metainterp/optimizeopt.py
@@ -812,7 +812,7 @@ class Optimizer(object):
                       descr = vrefinfo.descr_virtual_token)
         self.optimize_SETFIELD_GC(op1)
         # - set debug stuff
-        args = [op.args[0], ConstInt(120)]
+        args = [op.args[0], ConstInt(121)]
         op1 = ResOperation(rop.SETFIELD_GC, args, None,
                       descr = vrefinfo.descr_debug_from)
         self.optimize_SETFIELD_GC(op1)
