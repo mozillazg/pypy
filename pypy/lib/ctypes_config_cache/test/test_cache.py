bca65f0409c70dc271e891fb887979c33cec741d
Armin Rigo <arigo@tunes.org>
2010-03-06 11:49:51 +0000

    Add hashlib.ctc.py, and write tests for *.ctc.py.

---
+++ pypy/lib/ctypes_config_cache/test/test_cache.py
@@ -0,0 +1,32 @@
+import py
+import sys, os
+from pypy.tool.udir import udir
+
+dirpath = py.path.local(__file__).dirpath().dirpath()
+
+
+def run(filename, outputname):
+    filepath = dirpath.join(filename)
+    tmpdir = udir.ensure('testcache-' + filename, dir=True)
+    outputpath = tmpdir.join(outputname)
+    d = {'__file__': str(outputpath)}
+    path = sys.path[:]
+    try:
+        sys.path.insert(0, str(dirpath))
+        execfile(str(filepath), d)
+    finally:
+        sys.path[:] = path
+    #
+    assert outputpath.check(exists=1)
+    d = {}
+    execfile(str(outputpath), d)
+    return d
+
+
+def test_syslog():
+    d = run('syslog.ctc.py', '_syslog_cache.py')
+    assert 'LOG_NOTICE' in d
+
+def test_hashlib():
+    d = run('hashlib.ctc.py', '_hashlib_cache.py')
+    assert hasattr(d['EVP_MD_CTX'], 'digest')
