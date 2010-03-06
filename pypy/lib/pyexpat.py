1379e5bcab55b73149e2a932360530af0c58e128
Maciej Fijalkowski <fijall@gmail.com>
2010-03-06 04:34:14 +0000

    Also use caching in pyexpat, although it's compiled as module these days

--- pypy/lib/pyexpat.py
+++ pypy/lib/pyexpat.py
@@ -1,44 +1,53 @@
 
 import ctypes
 import ctypes.util
-from ctypes_configure import configure
 from ctypes import c_char_p, c_int, c_void_p, POINTER, c_char, c_wchar_p
+from ctypes_support import cache_dir
 import sys
+import os.path
 
 lib = ctypes.CDLL(ctypes.util.find_library('expat'))
 
-class CConfigure:
-    _compilation_info_ = configure.ExternalCompilationInfo(
-        includes = ['expat.h'],
-        libraries = ['expat'],
-        pre_include_lines = [
-        '#define XML_COMBINED_VERSION (10000*XML_MAJOR_VERSION+100*XML_MINOR_VERSION+XML_MICRO_VERSION)'],
-        )
+cache_file = os.path.join(cache_dir, 'pyexpat')
+try:
+    info = {}
+    execfile(cache_file, info)
+except (IOError, OSError):
+    from ctypes_configure import configure
+    class CConfigure:
+        _compilation_info_ = configure.ExternalCompilationInfo(
+            includes = ['expat.h'],
+            libraries = ['expat'],
+            pre_include_lines = [
+            '#define XML_COMBINED_VERSION (10000*XML_MAJOR_VERSION+100*XML_MINOR_VERSION+XML_MICRO_VERSION)'],
+            )
 
-    XML_Char = configure.SimpleType('XML_Char', ctypes.c_char)
-    XML_COMBINED_VERSION = configure.ConstantInteger('XML_COMBINED_VERSION')
-    for name in ['XML_PARAM_ENTITY_PARSING_NEVER',
-                 'XML_PARAM_ENTITY_PARSING_UNLESS_STANDALONE',
-                 'XML_PARAM_ENTITY_PARSING_ALWAYS']:
-        locals()[name] = configure.ConstantInteger(name)
+        XML_Char = configure.SimpleType('XML_Char', ctypes.c_char)
+        XML_COMBINED_VERSION = configure.ConstantInteger('XML_COMBINED_VERSION')
+        for name in ['XML_PARAM_ENTITY_PARSING_NEVER',
+                     'XML_PARAM_ENTITY_PARSING_UNLESS_STANDALONE',
+                     'XML_PARAM_ENTITY_PARSING_ALWAYS']:
+            locals()[name] = configure.ConstantInteger(name)
 
-    XML_Encoding = configure.Struct('XML_Encoding',[
-                                    ('data', c_void_p),
-                                    ('convert', c_void_p),
-                                    ('release', c_void_p),
-                                    ('map', c_int * 256)])
-    XML_Content = configure.Struct('XML_Content',[
-        ('numchildren', c_int),
-        ('children', c_void_p),
-        ('name', c_char_p),
-        ('type', c_int),
-        ('quant', c_int),
-    ])
-    # this is insanely stupid
-    XML_FALSE = configure.ConstantInteger('XML_FALSE')
-    XML_TRUE = configure.ConstantInteger('XML_TRUE')
+        XML_Encoding = configure.Struct('XML_Encoding',[
+                                        ('data', c_void_p),
+                                        ('convert', c_void_p),
+                                        ('release', c_void_p),
+                                        ('map', c_int * 256)])
+        XML_Content = configure.Struct('XML_Content',[
+            ('numchildren', c_int),
+            ('children', c_void_p),
+            ('name', c_char_p),
+            ('type', c_int),
+            ('quant', c_int),
+        ])
+        # this is insanely stupid
+        XML_FALSE = configure.ConstantInteger('XML_FALSE')
+        XML_TRUE = configure.ConstantInteger('XML_TRUE')
 
-info = configure.configure(CConfigure)
+    info = configure.configure(CConfigure, savecache=cache_file)
+
+
 for k, v in info.items():
     globals()[k] = v
 
