8d6580748e8552bda31054ac7ba56697c3a4d7dd
Alexander Schremmer <alex-bitbucket@alexanderweb.de>
2010-03-24 22:42:12 +0000

    Fix test.

--- pypy/module/cpyext/test/test_tupleobject.py
+++ pypy/module/cpyext/test/test_tupleobject.py
@@ -5,7 +5,5 @@ from pypy.module.cpyext.test.test_cpyext import BaseApiTest
 class TestTupleObject(BaseApiTest):
     def test_tupleobject(self, space, api):
         assert not api.PyTuple_Check(space.w_None)
-        py.test.raises(TypeError, api.PyTuple_SetItem, space.w_None,
-                0, space.w_None)
-        api.PyTuple_SetItem(space.w_None, 0, space.w_None)
-        #api.PyErr_Clear()
+        assert api.PyTuple_SetItem(space.w_None, 0, space.w_None) == -1
+        api.PyErr_Clear()
