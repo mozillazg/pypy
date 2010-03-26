cca3a4047cbd32f2fdd6e22c8a51ff583743e171
Maciej Fijalkowski <fijall@gmail.com>
2010-03-26 02:48:06 +0000

    Return a correct empty box. I think this is the cause of crashes (not sure)

--- pypy/jit/backend/llsupport/test/test_descr.py
+++ pypy/jit/backend/llsupport/test/test_descr.py
@@ -3,7 +3,7 @@ from pypy.jit.backend.llsupport.descr import *
 from pypy.jit.backend.llsupport import symbolic
 from pypy.rlib.objectmodel import Symbolic
 from pypy.rpython.annlowlevel import llhelper
-from pypy.jit.metainterp.history import BoxInt
+from pypy.jit.metainterp.history import BoxInt, BoxFloat, BoxPtr
 
 def test_get_size_descr():
     c0 = GcCache(False)
@@ -148,6 +148,7 @@ def test_get_call_descr_not_translated():
     assert not descr1.returns_a_pointer()
     assert not descr1.returns_a_float()
     assert descr1.arg_classes == "ii"
+    assert isinstance(descr1.empty_box, BoxInt)
     #
     T = lltype.GcStruct('T')
     descr2 = get_call_descr(c0, [lltype.Ptr(T)], lltype.Ptr(T))
@@ -155,15 +156,22 @@ def test_get_call_descr_not_translated():
     assert descr2.returns_a_pointer()
     assert not descr2.returns_a_float()
     assert descr2.arg_classes == "r"
+    assert isinstance(descr2.empty_box, BoxPtr)
     #
     U = lltype.GcStruct('U', ('x', lltype.Signed))
     assert descr2 == get_call_descr(c0, [lltype.Ptr(U)], lltype.Ptr(U))
+    #
+    V = lltype.Struct('V', ('x', lltype.Signed))
+    assert isinstance(get_call_descr(c0, [], lltype.Ptr(V)).empty_box, BoxInt)
+    #
+    assert get_call_descr(c0, [], lltype.Void).empty_box is None
     #
     descr4 = get_call_descr(c0, [lltype.Float, lltype.Float], lltype.Float)
     assert descr4.get_result_size(False) == rffi.sizeof(lltype.Float)
     assert not descr4.returns_a_pointer()
     assert descr4.returns_a_float()
     assert descr4.arg_classes == "ff"
+    assert isinstance(descr4.empty_box, BoxFloat)
 
 def test_get_call_descr_translated():
     c1 = GcCache(True)
