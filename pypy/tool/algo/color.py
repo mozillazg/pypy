6117b5bfe0df03c2bb12f63d9734135894ac7254
Maciej Fijalkowski <fijall@gmail.com>
2010-03-26 23:59:54 +0000

    Mention a paper which describes SSA-based efficient graph coloring

--- pypy/tool/algo/color.py
+++ pypy/tool/algo/color.py
@@ -4,4 +4,7 @@ class DependencyGraph(object):
 
     Each variable is an node in a graph and we have an edge
     if two variables are alive at the same point of time
+
+    the whole algorithm is based on the following paper:
+    http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.86.1578&rep=rep1&type=pdf
     """
