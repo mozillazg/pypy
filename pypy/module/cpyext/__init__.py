dfebfd2f04b8091046fcafac5578f698bb165b5d
Alexander Schremmer <alex-bitbucket@alexanderweb.de>
2010-03-27 22:28:03 +0000

    Reorder code in __init__.py.

--- pypy/module/cpyext/__init__.py
+++ pypy/module/cpyext/__init__.py
@@ -16,7 +16,7 @@ class Module(MixedModule):
     def setup_after_space_initialization(self):
         """NOT_RPYTHON"""
         state = self.space.fromcache(State)
-        if not we_are_translated():
+        if not self.space.config.translating:
             state.api_lib = str(pypy.module.cpyext.api.build_bridge(self.space))
             state.slotdefs = init_slotdefs(self.space)
         else:
@@ -24,9 +24,10 @@ class Module(MixedModule):
 
     def startup(self, space):
         state = space.fromcache(State)
-        space.setattr(space.wrap(self),
-                      space.wrap('api_lib'),
-                      space.wrap(state.api_lib))
+        if not we_are_translated():
+            space.setattr(space.wrap(self),
+                          space.wrap('api_lib'),
+                          space.wrap(state.api_lib))
 
 # import these modules to register api functions by side-effect
 import pypy.module.cpyext.boolobject
