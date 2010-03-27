6d6bfe2ae75ee33f3d38d3d3626b5b3eb32fcbb5
Maciej Fijalkowski <fijall@gmail.com>
2010-03-27 19:57:21 +0000

    (zooko, fijal) PyInt_FromLong and PyInt_AsLong

--- pypy/module/cpyext/intobject.py
+++ pypy/module/cpyext/intobject.py
@@ -10,3 +10,18 @@ def PyInt_Check(space, w_obj):
     
     Allowed subtypes to be accepted."""
     return general_check(space, w_obj, space.w_int)
+
+@cpython_api([lltype.Signed], PyObject)
+def PyInt_FromLong(space, ival):
+    """Create a new integer object with a value of ival.
+    
+    """
+    return space.wrap(ival)
+
+@cpython_api([PyObject], lltype.Signed, error=-1)
+def PyInt_AsLong(space, w_obj):
+    """Will first attempt to cast the object to a PyIntObject, if it is not
+    already one, and then return its value. If there is an error, -1 is
+    returned, and the caller should check PyErr_Occurred() to find out whether
+    there was an error, or whether the value just happened to be -1."""
+    return space.int_w(w_obj)
