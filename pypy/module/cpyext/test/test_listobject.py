1f261c3a337659bfa5f35a26f0c5a36e7ad049c2
Maciej Fijalkowski <fijall@gmail.com>
2010-03-27 21:19:56 +0000

    Implement a couple of list operations

---
+++ pypy/module/cpyext/test/test_listobject.py
@@ -0,0 +1,44 @@
+
+from pypy.module.cpyext.test.test_cpyext import AppTestCpythonExtensionBase
+
+class AppTestListObject(AppTestCpythonExtensionBase):
+    def test_listobject(self):
+        import sys
+        module = self.import_extension('foo', [
+            ("newlist", "METH_NOARGS",
+             """
+             PyObject *lst = PyList_New(3);
+             PyList_SetItem(lst, 0, PyInt_FromLong(3));
+             PyList_SetItem(lst, 2, PyInt_FromLong(1000));
+             PyList_SetItem(lst, 1, PyInt_FromLong(-5));
+             return lst;
+             """
+             ),
+            ("setlistitem", "METH_VARARGS",
+             """
+             PyObject *l = PyTuple_GetItem(args, 0);
+             int index = PyInt_AsLong(PyTuple_GetItem(args, 1));
+             Py_INCREF(Py_None);
+             int res = PyList_SetItem(l, index, Py_None);
+             if (res == -1) {
+                return NULL;
+             }
+             Py_INCREF(Py_None);
+             return Py_None;
+             """
+             )
+            ])
+        l = module.newlist()
+        assert l == [3, -5, 1000]
+        module.setlistitem(l, 0)
+        assert l[0] is None
+
+        class L(list):
+            def __setitem__(self):
+                self.append("XYZ")
+
+        l = L([1])
+        module.setlistitem(l, 0)
+        assert len(l) == 1
+        
+        raises(TypeError, module.setlistitem, (1, 2, 3), 0)
