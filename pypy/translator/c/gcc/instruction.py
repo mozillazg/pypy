392937f94324aeb0b7bdafa914df7c93f89d3262
Armin Rigo <arigo@tunes.org>
2010-03-27 20:06:14 +0000

    Whack whack whack.

--- pypy/translator/c/gcc/instruction.py
+++ pypy/translator/c/gcc/instruction.py
@@ -5,13 +5,17 @@ LOC_EBP_MINUS = 3
 LOC_MASK      = 0x03
 LOC_NOWHERE   = LOC_REG | 0
 
-def frameloc_esp(offset):
-    assert offset >= 0
-    assert offset % 4 == 0
-    return LOC_ESP_PLUS | offset
+def frameloc_esp(offset, WORD=4):
+    assert offset % WORD == 0
+    if offset >= 0:
+        return LOC_ESP_PLUS | offset
+    else:
+        # On 64-bit, the "red zone" might need access to e.g. -8(%esp).
+        # But such positions should never be used across a CALL...
+        raise AssertionError(offset)
 
-def frameloc_ebp(offset):
-    assert offset % 4 == 0
+def frameloc_ebp(offset, WORD=4):
+    assert offset % WORD == 0
     if offset >= 0:
         return LOC_EBP_PLUS | offset
     else:
@@ -22,7 +26,7 @@ class SomeNewValue(object):
     pass
 somenewvalue = SomeNewValue()
 
-class LocalVar(object):
+class LocalVarAbstract(object):
     # A local variable location at position 'ofs_from_frame_end',
     # which is counted from the end of the stack frame (so it is always
     # negative, unless it refers to arguments of the current function).
@@ -37,7 +41,7 @@ class LocalVar(object):
         return hash(self.ofs_from_frame_end)
 
     def __cmp__(self, other):
-        if isinstance(other, LocalVar):
+        if isinstance(other, LocalVarAbstract):
             return cmp(self.ofs_from_frame_end, other.ofs_from_frame_end)
         else:
             return 1
@@ -48,12 +52,18 @@ class LocalVar(object):
             # try to use esp-relative addressing
             ofs_from_esp = framesize + self.ofs_from_frame_end
             if ofs_from_esp % 2 == 0:
-                return frameloc_esp(ofs_from_esp)
+                return frameloc_esp(ofs_from_esp, self.WORD)
             # we can get an odd value if the framesize is marked as bogus
             # by visit_andl()
         assert uses_frame_pointer
-        ofs_from_ebp = self.ofs_from_frame_end + 4
-        return frameloc_ebp(ofs_from_ebp)
+        ofs_from_ebp = self.ofs_from_frame_end + self.WORD
+        return frameloc_ebp(ofs_from_ebp, self.WORD)
+
+class LocalVar32(LocalVarAbstract):
+    WORD = 4
+
+class LocalVar64(LocalVarAbstract):
+    WORD = 8
 
 
 class Insn(object):
@@ -98,7 +108,7 @@ class InsnFunctionStart(Insn):
                 # the 3 registers above are then used to pass arguments
                 pass
             else:
-                assert (isinstance(localvar, LocalVar) and
+                assert (isinstance(localvar, LocalVarAbstract) and
                         localvar.ofs_from_frame_end > 0), (
                     "must come from an argument to the function, got %r" %
                     (localvar,))
@@ -220,13 +230,11 @@ class InsnGCROOT(Insn):
 class InsnPrologue(Insn):
     def __setattr__(self, attr, value):
         if attr == 'framesize':
-            assert value == 4, ("unrecognized function prologue - "
-                                "only supports push %ebp; movl %esp, %ebp")
+            assert value in (4,8), ("unrecognized function prologue - "
+                                    "only supports push %ebp; movl %esp, %ebp")
         Insn.__setattr__(self, attr, value)
 
 class InsnEpilogue(Insn):
     def __init__(self, framesize=None):
         if framesize is not None:
             self.framesize = framesize
-
-
