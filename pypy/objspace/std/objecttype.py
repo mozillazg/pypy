66cb89d33a9dd38426d238e7598f4eb200605ab6
Benjamin Peterson <benjamin@python.org>
2010-03-29 21:47:23 +0000

    cleanup type definition files
    * remove stdtypedef.newmethod
    * import things from their actual location
    * some whitespace cleanup

--- pypy/objspace/std/objecttype.py
+++ pypy/objspace/std/objecttype.py
@@ -1,9 +1,8 @@
 from pypy.interpreter.error import OperationError, operationerrfmt
-from pypy.interpreter.typedef import GetSetProperty
-from pypy.objspace.descroperation import Object
+from pypy.interpreter.typedef import GetSetProperty, default_identity_hash
 from pypy.interpreter import gateway
-from pypy.interpreter.typedef import default_identity_hash
-from pypy.objspace.std.stdtypedef import StdTypeDef, newmethod
+from pypy.objspace.descroperation import Object
+from pypy.objspace.std.stdtypedef import StdTypeDef, no_hash_descr
 from pypy.objspace.std.register_all import register_all
 
 
@@ -171,8 +170,8 @@ object_typedef = StdTypeDef("object",
     __repr__ = gateway.interp2app(descr__repr__),
     __class__ = GetSetProperty(descr__class__, descr_set___class__),
     __doc__ = '''The most base type''',
-    __new__ = newmethod(descr__new__,
-                        unwrap_spec = [gateway.ObjSpace,gateway.W_Root,gateway.Arguments]),
+    __new__ = gateway.interp2app(descr__new__,
+    unwrap_spec = [gateway.ObjSpace,gateway.W_Root,gateway.Arguments]),
     __hash__ = gateway.interp2app(default_identity_hash),
     __reduce_ex__ = gateway.interp2app(descr__reduce_ex__,
                                   unwrap_spec=[gateway.ObjSpace,gateway.W_Root,int]),
