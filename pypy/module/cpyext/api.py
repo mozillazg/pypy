7c82ae1c59a4bd09d2db4263e86e1bfd69690625
Alexander Schremmer <alex-bitbucket@alexanderweb.de>
2010-03-29 18:12:01 +0000

    Use _isllptr here.

--- pypy/module/cpyext/api.py
+++ pypy/module/cpyext/api.py
@@ -152,7 +152,7 @@ def cpython_api(argtypes, restype, borrowed=False, error=_NOT_SPECIFIED, externa
                             arg = input_arg
                     elif ARG is PyObject and is_wrapped:
                         # convert to a wrapped object
-                        if not isinstance(input_arg, W_Root):
+                        if rffi._isllptr(input_arg):
                             arg = from_ref(space, input_arg)
                         else:
                             arg = input_arg
