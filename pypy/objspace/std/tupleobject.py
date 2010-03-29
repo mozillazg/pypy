63e2a9f3005ea24a95cae8641b94f9c8cf903291
Benjamin Peterson <benjamin@python.org>
2010-03-29 22:07:55 +0000

    add FailedToImplement import

--- pypy/objspace/std/tupleobject.py
+++ pypy/objspace/std/tupleobject.py
@@ -2,6 +2,7 @@ from pypy.interpreter.error import OperationError
 from pypy.objspace.std.model import registerimplementation, W_Object
 from pypy.objspace.std.register_all import register_all
 from pypy.objspace.std.inttype import wrapint
+from pypy.objspace.std.multimethod import FailedToImplement
 from pypy.rlib.rarithmetic import intmask
 from pypy.objspace.std.sliceobject import W_SliceObject, normalize_simple_slice
 from pypy.interpreter import gateway
