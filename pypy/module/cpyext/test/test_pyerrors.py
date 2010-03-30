430d8ea6efbdbd3f9c8c2cd53dca96c7e47aeccd
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-03-30 15:11:22 +0000

    Correctly borrow exc_type with PyErr_Occurred()

--- pypy/module/cpyext/test/test_pyerrors.py
+++ pypy/module/cpyext/test/test_pyerrors.py
@@ -1,4 +1,5 @@
 from pypy.module.cpyext.test.test_api import BaseApiTest
+from pypy.module.cpyext.test.test_cpyext import AppTestCpythonExtensionBase
 from pypy.rpython.lltypesystem import rffi
 
 class TestExceptions(BaseApiTest):
@@ -10,3 +11,17 @@ class TestExceptions(BaseApiTest):
         assert api.PyErr_Occurred() is space.w_ValueError
 
         api.PyErr_Clear()
+
+class AppTestFetch(AppTestCpythonExtensionBase):
+    def test_occurred(self):
+        module = self.import_extension('foo', [
+            ("check_error", "METH_NOARGS",
+             '''
+             PyErr_SetString(PyExc_TypeError, "message");
+             PyErr_Occurred();
+             PyErr_Clear();
+             Py_RETURN_TRUE;
+             '''
+             ),
+            ])
+        module.check_error()
