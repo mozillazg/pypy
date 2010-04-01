a11db423035b5ac874b65ba49eca2afff5f2089b
Andreas Stührk <andy-python@hammerhartes.de>
2010-04-01 14:40:47 +0000

    Add forgotten structmember.h, sorry.

---
+++ pypy/module/cpyext/include/structmember.h
@@ -0,0 +1,29 @@
+#ifndef Py_STRUCTMEMBER_H
+#define Py_STRUCTMEMBER_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+typedef struct PyMemberDef {
+	/* Current version, use this */
+	char *name;
+	int type;
+	Py_ssize_t offset;
+	int flags;
+	char *doc;
+} PyMemberDef;
+
+
+/* Types */
+#define T_INT		1
+
+/* Flags */
+#define READONLY      1
+#define RO            READONLY                /* Shorthand */
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* !Py_STRUCTMEMBER_H */
