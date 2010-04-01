fcd056484868233e68f7b7a2b5303d83daa7c155
Maciej Fijalkowski <fijall@gmail.com>
2010-04-01 21:22:39 +0000

    A failing test

---
+++ pypy/module/cpyext/test/test_getargs.py
@@ -0,0 +1,17 @@
+
+from pypy.module.cpyext.test.test_api import BaseApiTest
+from pypy.module.cpyext.test.test_cpyext import AppTestCpythonExtensionBase
+
+class AppTestGetargs(AppTestCpythonExtensionBase):
+    def test_pyarg_parse(self):
+        mod = self.import_extension('foo', [
+            ('oneargint', 'METH_VARARGS',
+             '''
+             int l;
+             #if (!PyArg_Parse(args, "i", &l)) {
+             #    return NULL;
+             #}
+             return PyInt_FromLong(l);
+             '''
+             )])
+        assert mod.oneargint(1) == 1
