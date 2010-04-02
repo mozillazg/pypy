3e3b3c1184baf9894205a94573da1a1a996a1933
Maciej Fijalkowski <fijall@gmail.com>
2010-04-02 00:49:36 +0000

    hack around COpaque object. vgetargs is still unfinished though

--- pypy/module/cpyext/getargs.py
+++ pypy/module/cpyext/getargs.py
@@ -1,6 +1,6 @@
 
-from pypy.module.cpyext.api import cpython_api, PyObject, CANNOT_FAIL, va_list,\
-     cpython_api_c
+from pypy.module.cpyext.api import cpython_api, PyObject, CANNOT_FAIL, \
+     VA_LIST_P, cpython_api_c
 from pypy.module.cpyext import api
 from pypy.rpython.lltypesystem import lltype, rffi
 
@@ -8,16 +8,16 @@ from pypy.rpython.lltypesystem import lltype, rffi
 def PyArg_Parse():
     pass
 
-@cpython_api([PyObject, rffi.CCHARP, lltype.Ptr(va_list), rffi.INT_real],
+@cpython_api([PyObject, rffi.CCHARP, VA_LIST_P, rffi.INT_real],
              rffi.INT_real, error=-1)
 def pypy_vgetargs1(space, w_obj, fmt, va_list_p, lgt):
     i = 0
-    raise Exception("This is broken so far")
     while True:
         c = fmt[i]
         if c == "\x00":
             return 0
         if c == "i":
-            pyobj = api.va_get_PyObject(va_list_p);
+            pyobj = api.va_get_int_star(va_list_p)
+            # XXX processs....
         i += 1
     return 0
