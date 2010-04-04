86f9907dea55d2569c40aa11f3612a9a5bb29f22
Andreas Stührk <andy-python@hammerhartes.de>
2010-04-04 23:08:51 +0000

    Add various Py_UNICODE_IS* functions and Py_UNICODE_TOLOWER.

---
+++ pypy/module/cpyext/test/test_unicodeobject.py
@@ -0,0 +1,31 @@
+# encoding: iso-8859-15
+from pypy.module.cpyext.test.test_api import BaseApiTest
+
+class TestUnicode(BaseApiTest):
+    def test_IS(self, space, api):
+        for char in [0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x1c, 0x1d, 0x1e, 0x1f,
+                     0x20, 0x85, 0xa0, 0x1680, 0x2000, 0x2001, 0x2002,
+                     0x2003, 0x2004, 0x2005, 0x2006, 0x2007, 0x2008,
+                     0x2009, 0x200a,
+                     #0x200b is in Other_Default_Ignorable_Code_Point in 4.1.0
+                     0x2028, 0x2029, 0x202f, 0x205f, 0x3000]:
+            assert api.Py_UNICODE_ISSPACE(char)
+        assert not api.Py_UNICODE_ISSPACE(ord(u'a'))
+
+        assert api.Py_UNICODE_ISDECIMAL(ord(u'\u0660'))
+        assert not api.Py_UNICODE_ISDECIMAL(ord(u'a'))
+
+        for char in [0x0a, 0x0d, 0x1c, 0x1d, 0x1e, 0x85, 0x2028, 0x2029]:
+            assert api.Py_UNICODE_ISLINEBREAK(char)
+
+        assert api.Py_UNICODE_ISLOWER(ord(u'ä'))
+        assert not api.Py_UNICODE_ISUPPER(ord(u'ä'))
+        assert api.Py_UNICODE_ISLOWER(ord(u'a'))
+        assert not api.Py_UNICODE_ISUPPER(ord(u'a'))
+        assert not api.Py_UNICODE_ISLOWER(ord(u'Ä'))
+        assert api.Py_UNICODE_ISUPPER(ord(u'Ä'))
+
+    def test_TOLOWER(self, space, api):
+        assert api.Py_UNICODE_TOLOWER(ord(u'ä')) == ord(u'ä')
+        assert api.Py_UNICODE_TOLOWER(ord(u'Ä')) == ord(u'ä')
+
