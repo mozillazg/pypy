e2b582471bd83f8ccc55b6cbb7f685f5646d0acd
Alexander Schremmer <alex-bitbucket@alexanderweb.de>
2010-04-04 18:35:01 +0000

    Add comments and a statichere define.

--- pypy/module/cpyext/include/pymem.h
+++ pypy/module/cpyext/include/pymem.h
@@ -5,8 +5,8 @@
 #define PyMem_REALLOC(p, n)	realloc((p), (n) ? (n) : 1)
 #define PyMem_FREE		free
 
-/* XXX use obmalloc like cpython does */
+/* XXX use obmalloc like cpython and pypy do, otherwise we might get segfaults */
 #define PyObject_MALLOC		PyMem_MALLOC
-#define PyObject_REALLOC	PyMem_REALLOC
+//#define PyObject_REALLOC	PyMem_REALLOC
 #define PyObject_FREE		PyMem_FREE
 
