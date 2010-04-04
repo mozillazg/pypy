1e738ad1b3b089866fb3ae5370e20a2ad6d0d716
Alexander Schremmer <alex-bitbucket@alexanderweb.de>
2010-04-04 19:11:12 +0000

    Enable HAVE_UNICODE and add typedefs and defines.

--- pypy/module/cpyext/include/unicodeobject.h
+++ pypy/module/cpyext/include/unicodeobject.h
@@ -1,3 +1,9 @@
 typedef struct {
     PyObject_HEAD
 } PyUnicodeObject;
+//XXX
+typedef unsigned int Py_UCS4; 
+// pypy only supports only UCS4
+#define PY_UNICODE_TYPE Py_UCS4
+typedef PY_UNICODE_TYPE Py_UNICODE;
+
