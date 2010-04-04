24db0d942e624ca744fa2115b8ef3981f91786e3
Alexander Schremmer <alex-bitbucket@alexanderweb.de>
2010-04-04 17:16:30 +0000

    Add PyLong_AsUnsignedLong.

--- pypy/module/cpyext/longobject.py
+++ pypy/module/cpyext/longobject.py
@@ -1,6 +1,7 @@
-
-from pypy.rpython.lltypesystem import lltype
+from pypy.rpython.lltypesystem import lltype, rffi
 from pypy.module.cpyext.api import cpython_api, PyObject
+from pypy.objspace.std.longobject import W_LongObject
+from pypy.interpreter.error import OperationError
 
 
 @cpython_api([lltype.Signed], PyObject)
@@ -8,4 +9,11 @@ def PyLong_FromLong(space, val):
     """Return a new PyLongObject object from v, or NULL on failure."""
     return space.wrap(val)
 
+@cpython_api([PyObject], rffi.ULONG, error=0)
+def PyLong_AsUnsignedLong(space, w_long):
+    """
+    Return a C unsigned long representation of the contents of pylong.
+    If pylong is greater than ULONG_MAX, an OverflowError is
+    raised."""
+    return rffi.cast(rffi.ULONG, space.uint_w(w_long))
 
