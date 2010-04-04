e2fca73b43ed28d1b8e4eb27aad01d6efe750a2c
Alexander Schremmer <alex-bitbucket@alexanderweb.de>
2010-04-04 17:38:14 +0000

    I want to see all errors, not only some! Pt. 2

--- pypy/translator/platform/__init__.py
+++ pypy/translator/platform/__init__.py
@@ -105,10 +105,8 @@ class Platform(object):
             errorfile = outname.new(ext='errors')
             errorfile.write(stderr, 'wb')
             stderrlines = stderr.splitlines()
-            for line in stderrlines[:50]:
+            for line in stderrlines:
                 log.ERROR(line)
-            #if len(stderrlines) > 50:
-            #    log.ERROR('...')
             raise CompilationError(stdout, stderr)
         else:
             for line in stderr.splitlines():
