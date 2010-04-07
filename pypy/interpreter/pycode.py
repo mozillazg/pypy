cb90a7206fbe9113b2dce4bd160c74e9f3663490
Maciej Fijalkowski <fijall@gmail.com>
2010-04-07 17:17:38 +0000

    break translation - we should not see cpython bytecode during translation

--- pypy/interpreter/pycode.py
+++ pypy/interpreter/pycode.py
@@ -117,6 +117,11 @@ class PyCode(eval.Code):
 
         self._compute_flatcall()
 
+    def _freeze_(self):
+        if self.magic == cpython_magic:
+            raise Exception("CPython host codes should not be rendered")
+        # XXX since PyCode is an immutable object, why not return True?
+        return False
 
     def _init_flags(self):
         co_code = self.co_code
