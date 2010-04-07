ecf17fda2768796bfe3a98870dbe3e81f0fe2faa
Alexander Schremmer <alex-bitbucket@alexanderweb.de>
2010-04-07 02:32:18 +0000

    Add a test for re.search and fix str_getreadbuffer.

--- pypy/module/cpyext/test/test_typeobject.py
+++ pypy/module/cpyext/test/test_typeobject.py
@@ -90,15 +90,12 @@ class AppTestTypeObject(AppTestCpythonExtensionBase):
         assert sre_compile.MAGIC == module.MAGIC
         import re
         import time
-        try:
-            s = u"Foo " * 1000 + u"Bar"
-            prog = re.compile(ur"Foo.*Bar")
-            assert prog.match(s)
-            m = re.search(u"xyz", u"xyzxyz")
-            assert m
-            skip("Crashing in fijals code")
-            m = re.search("xyz", "xyzxyz")
-            assert m
-        finally:
-            re._cache.clear()
-            re._cache_repl.clear()
+        s = u"Foo " * 1000 + u"Bar"
+        prog = re.compile(ur"Foo.*Bar")
+        assert prog.match(s)
+        m = re.search(u"xyz", u"xyzxyz")
+        assert m
+        m = re.search("xyz", "xyzxyz")
+        assert m
+        re._cache.clear()
+        re._cache_repl.clear()
