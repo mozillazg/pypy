fc4ccad577873286a28a4738113058edb3bb4ca6
Jan de Mooij <jandem@codespeak.net>
2010-04-08 15:39:22 +0000

    Add PyNumber_AsSsize_t, not finished but enough to test bufferobject

--- pypy/module/cpyext/number.py
+++ pypy/module/cpyext/number.py
@@ -1,5 +1,5 @@
 from pypy.interpreter.error import OperationError
-from pypy.module.cpyext.api import cpython_api, CANNOT_FAIL
+from pypy.module.cpyext.api import cpython_api, CANNOT_FAIL, Py_ssize_t
 from pypy.module.cpyext.pyobject import PyObject
 from pypy.rpython.lltypesystem import rffi, lltype
 
@@ -13,3 +13,15 @@ def PyIndex_Check(space, w_obj):
         return 1
     except OperationError:
         return 0
+
+@cpython_api([PyObject, PyObject], Py_ssize_t, error=-1)
+def PyNumber_AsSsize_t(space, w_obj, w_exc):
+    """Returns o converted to a Py_ssize_t value if o can be interpreted as an
+    integer. If o can be converted to a Python int or long but the attempt to
+    convert to a Py_ssize_t value would raise an OverflowError, then the
+    exc argument is the type of exception that will be raised (usually
+    IndexError or OverflowError).  If exc is NULL, then the
+    exception is cleared and the value is clipped to PY_SSIZE_T_MIN for a negative
+    integer or PY_SSIZE_T_MAX for a positive integer.
+    """
+    return space.int_w(w_obj) #XXX: this is wrong
