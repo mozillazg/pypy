2248ed787ee9ffb0e39d1765b2ce4983710fc29a
Alexander Schremmer <alex-bitbucket@alexanderweb.de>
2010-04-11 20:14:04 +0000

    Add PyCObject_FromVoidPtrAndDesc (untested).

--- pypy/module/cpyext/test/test_pycobject.py
+++ pypy/module/cpyext/test/test_pycobject.py
@@ -2,9 +2,9 @@ import py
 
 from pypy.rpython.lltypesystem import rffi, lltype
 from pypy.module.cpyext.test.test_api import BaseApiTest
-from pypy.module.cpyext.pycobject import destructor
+from pypy.module.cpyext.pycobject import destructor_short
 
 class TestPyCObject(BaseApiTest):
     def test_pycobject(self, space, api):
-        obj = api.PyCObject_FromVoidPtr(rffi.cast(rffi.VOIDP_real, 0), lltype.nullptr(destructor.TO))
+        obj = api.PyCObject_FromVoidPtr(rffi.cast(rffi.VOIDP_real, 0), lltype.nullptr(destructor_short.TO))
         api.Py_DecRef(obj)
