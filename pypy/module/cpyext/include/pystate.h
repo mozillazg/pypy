64028e14bed3911a4f866f7a17a73ade31027b09
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-04-13 10:54:12 +0000

    Add Py_BEGIN_ALLOW_THREADS &co with a minimal test.

--- pypy/module/cpyext/include/pystate.h
+++ pypy/module/cpyext/include/pystate.h
@@ -1,3 +1,16 @@
+#ifndef Py_PYSTATE_H
+#define Py_PYSTATE_H
+
 typedef struct _ts {
     int initialized; // not used
 } PyThreadState;
+
+#define Py_BEGIN_ALLOW_THREADS { \
+			PyThreadState *_save; \
+			_save = PyEval_SaveThread();
+#define Py_BLOCK_THREADS	PyEval_RestoreThread(_save);
+#define Py_UNBLOCK_THREADS	_save = PyEval_SaveThread();
+#define Py_END_ALLOW_THREADS	PyEval_RestoreThread(_save); \
+		 }
+
+#endif /* !Py_PYSTATE_H */
