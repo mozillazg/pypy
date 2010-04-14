c24f97795da51cc283ef004c44f2ef6675570495
Armin Rigo <arigo@tunes.org>
2010-04-14 11:27:50 +0000

    Perfect elimination order.

--- pypy/tool/algo/color.py
+++ pypy/tool/algo/color.py
@@ -1,10 +1,59 @@
+"""
+the whole algorithm is based on the following paper:
+http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.86.1578&rep=rep1&type=pdf
+
+and the source code of libFIRM (file ir/be/becopyheur.c):
+http://pp.info.uni-karlsruhe.de/firm/Main_Page
+"""
+
 
 class DependencyGraph(object):
-    """ A dependency graph for a given control flow graph (CFG).
 
-    Each variable is an node in a graph and we have an edge
-    if two variables are alive at the same point of time
+    def __init__(self):
+        self.neighbours = {}
 
-    the whole algorithm is based on the following paper:
-    http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.86.1578&rep=rep1&type=pdf
-    """
+    def add_node(self, v):
+        assert v not in self.neighbours, "duplicate vertex %r" % (v,)
+        self.neighbours[v] = set()
+
+    def add_edge(self, v1, v2):
+        self.neighbours[v1].add(v2)
+        self.neighbours[v2].add(v1)
+
+    def perfect_elimination_order(self):
+        sigma = [set(self.neighbours)]
+        result = []
+        while sigma:
+            v = sigma[0].pop()
+            result.append(v)
+            newsigma = []
+            neighb = self.neighbours[v]
+            for s in sigma:
+                s1 = set()
+                s2 = set()
+                for x in s:
+                    if x in neighb:
+                        s1.add(x)
+                    else:
+                        s2.add(x)
+                if s1:
+                    newsigma.append(s1)
+                if s2:
+                    newsigma.append(s2)
+            sigma = newsigma
+        result.reverse()
+        return result
+
+
+_emptyset = frozenset()
+
+
+##class Unit(object):
+##    """An optimization unit.  Represents a phi instruction."""
+##    def __init__(self, result, args):
+##        self.result = result
+##        self.args = args
+
+##    def optimize(self, depgraph):
+##        self.queue = []
+##        self.insert_qnode(QNode(...
