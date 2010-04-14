fa2e9ec8819ac24f5a2cb987f8e64855c2d2fd2a
Armin Rigo <arigo@tunes.org>
2010-04-14 11:27:50 +0000

    Perfect elimination order.

--- pypy/tool/algo/test/test_color.py
+++ pypy/tool/algo/test/test_color.py
@@ -1,16 +1,25 @@
-
-from pypy.translator.translator import graphof
-from pypy.annotation.annrpython import RPythonAnnotator
 from pypy.tool.algo.color import DependencyGraph
 
-def test_one():
-    def f(a, b, c):
-        d = a + b
-        e = b + c
-        f = d + e
-        return d + e + f
 
-    a = RPythonAnnotator()
-    a.build_types(f, [int, int, int])
-    graph = graphof(a.translator, f)
-    dep_graph = DependencyGraph(graph)
+def test_perfect_elimination_order():
+    dg = DependencyGraph()
+    dg.add_node('a')
+    dg.add_node('b')
+    dg.add_node('c')
+    dg.add_node('d')
+    dg.add_node('e')
+    dg.add_edge('a', 'b')
+    dg.add_edge('a', 'd')
+    dg.add_edge('d', 'b')
+    dg.add_edge('d', 'e')
+    dg.add_edge('b', 'c')
+    dg.add_edge('b', 'e')
+    dg.add_edge('e', 'c')
+    order = list(dg.perfect_elimination_order())
+    assert len(order) == 5
+    assert ''.join(order) in [
+        'adbce', 'adbec', 'adcbe', 'adceb', 'adebc', 'adecb',
+        'acbde', 'acbed', 'acdbe', 'acdeb', 'acebd', 'acedb',
+        'cebad', 'cebda', 'ceabd', 'ceadb', 'cedba', 'cedab',
+        'cabde', 'cabed', 'cadbe', 'cadeb', 'caebd', 'caedb',
+        ]
