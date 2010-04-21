118c445ea97048b30514408da3c3c4e081ad0489
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-04-21 14:28:20 +0000

    implement METH_NOARGS and METH_OLDARGS calling conventions

--- pypy/module/cpyext/test/test_methodobject.py
+++ pypy/module/cpyext/test/test_methodobject.py
@@ -2,15 +2,47 @@ from pypy.module.cpyext.test.test_api import BaseApiTest
 from pypy.module.cpyext.test.test_cpyext import AppTestCpythonExtensionBase
 
 class AppTestMethodObject(AppTestCpythonExtensionBase):
-    def test_METH_O(self):
+    def test_call_METH(self):
         mod = self.import_extension('foo', [
-            ('getarg', 'METH_O',
+            ('getarg_O', 'METH_O',
              '''
              Py_INCREF(args);
              return args;
              '''
              ),
+            ('getarg_NO', 'METH_NOARGS',
+             '''
+             if(args) {
+                 Py_INCREF(args);
+                 return args;
+             }
+             else {
+                 Py_INCREF(Py_None);
+                 return Py_None;
+             }
+             '''
+             ),
+            ('getarg_OLD', 'METH_OLDARGS',
+             '''
+             if(args) {
+                 Py_INCREF(args);
+                 return args;
+             }
+             else {
+                 Py_INCREF(Py_None);
+                 return Py_None;
+             }
+             '''
+             ),
             ])
-        assert mod.getarg(1) == 1
-        raises(TypeError, mod.getarg)
-        raises(TypeError, mod.getarg, 1, 1)
+        assert mod.getarg_O(1) == 1
+        raises(TypeError, mod.getarg_O)
+        raises(TypeError, mod.getarg_O, 1, 1)
+
+        assert mod.getarg_NO() is None
+        raises(TypeError, mod.getarg_NO, 1)
+        raises(TypeError, mod.getarg_NO, 1, 1)
+
+        assert mod.getarg_OLD(1) == 1
+        assert mod.getarg_OLD() is None
+        assert mod.getarg_OLD(1, 2) == (1, 2)
