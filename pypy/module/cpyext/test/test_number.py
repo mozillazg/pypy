0a11f170a7560016b3671658118a082063799696
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-04-21 17:42:36 +0000

    Add PyNumber_* abstract operations

--- pypy/module/cpyext/test/test_number.py
+++ pypy/module/cpyext/test/test_number.py
@@ -7,3 +7,29 @@ class TestIterator(BaseApiTest):
     def test_index(self, space, api):
         assert api.PyIndex_Check(space.wrap(12))
         assert not api.PyIndex_Check(space.wrap('12'))
+
+    def test_numbermethods(self, space, api):
+        assert "ab" == space.unwrap(
+            api.PyNumber_Add(space.wrap("a"), space.wrap("b")))
+        assert "aaa" == space.unwrap(
+            api.PyNumber_Multiply(space.wrap("a"), space.wrap(3)))
+
+        w_l = space.newlist([1, 2, 3])
+        w_l2 = api.PyNumber_Multiply(w_l, space.wrap(3))
+        assert api.PyObject_Size(w_l2) == 9
+        assert api.PyObject_Size(w_l) == 3
+
+        w_l3 = api.PyNumber_InplaceMultiply(w_l, space.wrap(3))
+        assert api.PyObject_Size(w_l) == 9
+        assert w_l3 is w_l
+
+        # unary function
+        assert 9 == space.unwrap(api.PyNumber_Absolute(space.wrap(-9)))
+
+        # power
+        assert 9 == space.unwrap(
+            api.PyNumber_Power(space.wrap(3), space.wrap(2), space.w_None))
+        assert 4 == space.unwrap(
+            api.PyNumber_Power(space.wrap(3), space.wrap(2), space.wrap(5)))
+        assert 9 == space.unwrap(
+            api.PyNumber_InplacePower(space.wrap(3), space.wrap(2), space.w_None))
