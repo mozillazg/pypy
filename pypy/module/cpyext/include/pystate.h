b3fb6fcb598730e09011b0dda9258f0fc3ab55f2
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-04-22 11:01:57 +0000

    Should have been committed with r73960.

--- pypy/module/cpyext/include/pystate.h
+++ pypy/module/cpyext/include/pystate.h
@@ -5,6 +5,10 @@ typedef struct _ts {
     int initialized; // not used
 } PyThreadState;
 
+typedef struct _is {
+    int _foo;
+} PyInterpreterState;
+
 #define Py_BEGIN_ALLOW_THREADS { \
 			PyThreadState *_save; \
 			_save = PyEval_SaveThread();
@@ -12,5 +16,9 @@ typedef struct _ts {
 #define Py_UNBLOCK_THREADS	_save = PyEval_SaveThread();
 #define Py_END_ALLOW_THREADS	PyEval_RestoreThread(_save); \
 		 }
+
+typedef
+    enum {PyGILState_LOCKED, PyGILState_UNLOCKED}
+        PyGILState_STATE;
 
 #endif /* !Py_PYSTATE_H */
