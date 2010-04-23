7553f7f34e44b52376a5e6208407ca01623e5f92
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-04-23 12:55:38 +0000

    PySys_SetObject

--- pypy/module/cpyext/test/test_sysmodule.py
+++ pypy/module/cpyext/test/test_sysmodule.py
@@ -1,4 +1,5 @@
 from pypy.module.cpyext.test.test_cpyext import AppTestCpythonExtensionBase
+from pypy.module.cpyext.test.test_api import BaseApiTest
 from pypy.rpython.lltypesystem import rffi
 
 class AppTestSysModule(AppTestCpythonExtensionBase):
@@ -13,3 +14,9 @@ class AppTestSysModule(AppTestCpythonExtensionBase):
         assert module.get("excepthook")
         assert not module.get("spam_spam_spam")
 
+class TestSysModule(BaseApiTest):
+    def test_sysmodule(self, space, api):
+        buf = rffi.str2charp("last_tb")
+        api.PySys_SetObject(buf, space.wrap(1))
+        rffi.free_charp(buf)
+        assert space.unwrap(space.sys.get("last_tb")) == 1
