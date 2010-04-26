a1c544c8239dff61e21bfea1be945a27a7c70c3e
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-04-26 11:23:23 +0000

    Expose the PyCObject structure to C code.

--- pypy/module/cpyext/include/pycobject.h
+++ pypy/module/cpyext/include/pycobject.h
@@ -1,5 +1,19 @@
+#ifndef Py_COBJECT_H
+#define Py_COBJECT_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if (PY_VERSION_HEX >= 0x02060000 || defined(Py_BUILD_CORE))
 typedef struct {
     PyObject_HEAD
+    void *cobject;
+    void *desc;
     void (*destructor)(void *);
 } PyCObject;
-
+#endif
+ 
+#ifdef __cplusplus
+}
+#endif
+#endif /* !Py_COBJECT_H */
