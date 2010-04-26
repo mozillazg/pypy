22bdf6e48d1b43f760b5d217a90fa2d6a4bb69de
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-04-26 14:59:28 +0000

    PyNumber_Int

--- pypy/module/cpyext/number.py
+++ pypy/module/cpyext/number.py
@@ -38,6 +38,13 @@ def PyNumber_AsSsize_t(space, w_obj, w_exc):
     return space.int_w(w_obj) #XXX: this is wrong on win64
 
 @cpython_api([PyObject], PyObject)
+def PyNumber_Int(space, w_obj):
+    """Returns the o converted to an integer object on success, or NULL on failure.
+    If the argument is outside the integer range a long object will be returned
+    instead. This is the equivalent of the Python expression int(o)."""
+    return space.int(w_obj)
+
+@cpython_api([PyObject], PyObject)
 def PyNumber_Long(space, w_obj):
     """    Returns the o converted to a long integer object on success, or NULL on
     failure.  This is the equivalent of the Python expression long(o)."""
