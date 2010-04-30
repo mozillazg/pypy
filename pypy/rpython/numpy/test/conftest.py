89ff192423b1386c838dd85025c985629ae997e6
Holger Krekel <holger.krekel@gmail.com>
2010-04-30 18:20:45 +0000

    simplify directory/conditional skipping and get get rid of deprecations

--- pypy/rpython/numpy/test/conftest.py
+++ pypy/rpython/numpy/test/conftest.py
@@ -1,10 +1,4 @@
 import py
 
-class Directory(py.test.collect.Directory):
-
-    def run(self):
-        try:
-            import numpy
-        except ImportError:
-            py.test.skip("these tests need numpy installed")
-        return super(Directory, self).run()
+def pytest_collect_directory():
+    py.test.importorskip("numpy")
