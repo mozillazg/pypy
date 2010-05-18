12a5f9b86cc259f2d54b702dba5d2896a5080bc4
Jason Creighton <jcreigh@codespeak.net>
2010-05-18 14:44:38 +0000

    import rx86.py from remove-ri386-multimethod-2 branch

--- pypy/jit/backend/x86/test/conftest.py
+++ pypy/jit/backend/x86/test/conftest.py
@@ -3,6 +3,9 @@ from pypy.jit.backend import detect_cpu
 
 cpu = detect_cpu.autodetect()
 def pytest_runtest_setup(item):
-    if cpu != 'x86':
-        py.test.skip("x86 directory skipped: cpu is %r" % (cpu,))
-    
+    # XXX
+    if "test_rx86" in str(item.fspath):
+        # Don't skip
+        pass
+    elif cpu != 'x86':
+        py.test.skip("x86 tests skipped: cpu is %r" % (cpu,))
