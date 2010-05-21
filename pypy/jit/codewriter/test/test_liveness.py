09c7c99baf69c93a790a348e95664da9904790db
Armin Rigo <arigo@tunes.org>
2010-05-21 09:42:28 +0000

    Change again the precise meaning of -live-.  Now it's hopefully a simple and clear meaning.

--- pypy/jit/codewriter/test/test_liveness.py
+++ pypy/jit/codewriter/test/test_liveness.py
@@ -13,9 +13,11 @@ class TestFlatten:
         self.liveness_test("""
             -live-
             int_add %i0, $10 -> %i1
-        """, """
             -live-
+        """, """
+            -live- %i0
             int_add %i0, $10 -> %i1
+            -live-
         """)
 
     def test_simple(self):
@@ -30,138 +32,137 @@ class TestFlatten:
             int_add %i0, $6 -> %i4
             -live-
             int_mul %i3, %i4 -> %i5
+            -live-
             int_return %i5
         """, """
             -live- %i0
             int_add %i0, $10 -> %i1
             -live- %i0, %i1
             int_add %i0, $3 -> %i2
-            -live- %i0
+            -live- %i0, %i1, %i2
             int_mul %i1, %i2 -> %i3
-            -live- %i3
+            -live- %i0, %i3
             int_add %i0, $6 -> %i4
-            -live-
+            -live- %i3, %i4
             int_mul %i3, %i4 -> %i5
+            -live- %i5
             int_return %i5
         """)
 
     def test_one_path(self):
         self.liveness_test("""
-            -live-
             int_add %i0, $5 -> %i2
+            -live-
             int_is_true %i2 -> %i3
             goto_if_not %i3, L1
             int_copy %i0 -> %i4
-            -live-
             int_add %i4, $1 -> %i5
+            -live-
             int_return %i5
             ---
             L1:
             int_copy %i1 -> %i6
-            -live-
             int_add %i6, $2 -> %i7
+            -live-
             int_return %i7
         """, """
-            -live- %i0, %i1
             int_add %i0, $5 -> %i2
+            -live- %i0, %i1, %i2
             int_is_true %i2 -> %i3
             goto_if_not %i3, L1
             int_copy %i0 -> %i4
-            -live-
             int_add %i4, $1 -> %i5
+            -live- %i5
             int_return %i5
             ---
             L1:
             int_copy %i1 -> %i6
-            -live-
             int_add %i6, $2 -> %i7
+            -live- %i7
             int_return %i7
         """)
 
     def test_other_path(self):
         self.liveness_test("""
-            -live-
             int_add %i0, $5 -> %i2
+            -live- %i2
             int_is_true %i2 -> %i3
             goto_if_not %i3, L1
             int_copy %i0 -> %i4
             int_copy %i1 -> %i5
-            -live-
             int_add %i4, %i5 -> %i6
+            -live- %i6
             int_return %i6
             ---
             L1:
             int_copy %i0 -> %i7
-            -live-
             int_add %i7, $2 -> %i8
+            -live- %i8
             int_return %i8
         """, """
-            -live- %i0, %i1
             int_add %i0, $5 -> %i2
+            -live- %i0, %i1, %i2
             int_is_true %i2 -> %i3
             goto_if_not %i3, L1
             int_copy %i0 -> %i4
             int_copy %i1 -> %i5
-            -live-
             int_add %i4, %i5 -> %i6
+            -live- %i6
             int_return %i6
             ---
             L1:
             int_copy %i0 -> %i7
-            -live-
             int_add %i7, $2 -> %i8
+            -live- %i8
             int_return %i8
         """)
 
     def test_no_path(self):
         self.liveness_test("""
-            -live-
             int_add %i0, %i1 -> %i2
+            -live- %i2
             int_is_true %i2 -> %i3
             goto_if_not %i3, L1
             int_copy %i0 -> %i4
-            -live-
             int_add %i4, $5 -> %i5
+            -live- %i5
             int_return %i5
             ---
             L1:
             int_copy %i0 -> %i6
-            -live-
             int_add %i6, $2 -> %i7
+            -live- %i7
             int_return %i7
         """, """
-            -live- %i0
             int_add %i0, %i1 -> %i2
+            -live- %i0, %i2
             int_is_true %i2 -> %i3
             goto_if_not %i3, L1
             int_copy %i0 -> %i4
-            -live-
             int_add %i4, $5 -> %i5
+            -live- %i5
             int_return %i5
             ---
             L1:
             int_copy %i0 -> %i6
-            -live-
             int_add %i6, $2 -> %i7
+            -live- %i7
             int_return %i7
         """)
 
     def test_list_of_kind(self):
         self.liveness_test("""
             -live-
-            stub
-            foobar F[%f0]
+            foobar I[$25, %i0]
         """, """
-            -live- %f0
-            stub
-            foobar F[%f0]
+            -live- %i0
+            foobar I[$25, %i0]
         """)
 
     def test_switch(self):
         self.liveness_test("""
             goto_maybe L1
             -live-
-            stub
             fooswitch <SwitchDictDescr 4:L2, 5:L3>
             ---
             L3:
@@ -175,7 +176,6 @@ class TestFlatten:
         """, """
             goto_maybe L1
             -live- %i3, %i7
-            stub
             fooswitch <SwitchDictDescr 4:L2, 5:L3>
             ---
             L3:
@@ -190,11 +190,30 @@ class TestFlatten:
 
     def test_already_some(self):
         self.liveness_test("""
-            -live- %i0, $52, %i2, %i0
             foo %i0, %i1, %i2
+            -live- %i0, $52, %i2, %i0
             bar %i3, %i4, %i5
         """, """
-            -live- %i0, %i2, %i3, %i4, %i5
             foo %i0, %i1, %i2
+            -live- %i0, %i2, %i3, %i4, %i5
             bar %i3, %i4, %i5
+        """)
+
+    def test_keepalive(self):
+        self.liveness_test("""
+            -live-
+            build $1 -> %i6
+            -live-
+            foo %i0, %i1 -> %i2
+            -live-
+            bar %i3, %i2 -> %i5
+            -live- %i6
+        """, """
+            -live- %i0, %i1, %i3
+            build $1 -> %i6
+            -live- %i0, %i1, %i3, %i6
+            foo %i0, %i1 -> %i2
+            -live- %i2, %i3, %i6
+            bar %i3, %i2 -> %i5
+            -live- %i6
         """)
