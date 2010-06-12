bfd79e69059bfc92b3d6c71ceefaaa9785d68c1e
Armin Rigo <arigo@tunes.org>
2010-06-12 08:39:59 +0000

    Add a first simple test about multiple jitdrivers.

---
+++ pypy/jit/metainterp/test/test_jitdriver.py
@@ -0,0 +1,34 @@
+"""Tests for multiple JitDrivers."""
+from pypy.rlib.jit import JitDriver
+from pypy.jit.metainterp.test.test_basic import LLJitMixin, OOJitMixin
+
+
+class MultipleJitDriversTests:
+
+    def test_simple(self):
+        myjitdriver1 = JitDriver(greens=[], reds=['n', 'm'])
+        myjitdriver2 = JitDriver(greens=['g'], reds=['r'])
+        #
+        def loop1(n, m):
+            while n > 0:
+                myjitdriver1.can_enter_jit(n=n, m=m)
+                myjitdriver1.jit_merge_point(n=n, m=m)
+                n -= m
+            return n
+        #
+        def loop2(g, r):
+            while r > 0:
+                myjitdriver2.can_enter_jit(g=g, r=r)
+                myjitdriver2.jit_merge_point(g=g, r=r)
+                r += loop1(r, g) - 1
+            return r
+        #
+        res = self.meta_interp(loop2, [4, 40])
+        assert res == loop2(4, 40)
+
+
+class TestLLtype(MultipleJitDriversTests, LLJitMixin):
+    pass
+
+class TestOOtype(MultipleJitDriversTests, OOJitMixin):
+    pass
