f2447e69793b3138cc12671d15be9517a1d5dace
Armin Rigo <arigo@tunes.org>
2010-06-13 12:08:51 +0000

    If the JitDrivers take incompatible greens, we get an annotation crash.

--- pypy/jit/metainterp/test/test_ztranslation.py
+++ pypy/jit/metainterp/test/test_ztranslation.py
@@ -63,17 +63,18 @@ class TranslationTest:
                 frame.i -= 1
             return total * 10
         #
-        myjitdriver2 = JitDriver(greens = [], reds = ['m', 'x'])
-        def f2(m, x):
+        myjitdriver2 = JitDriver(greens = ['g'], reds = ['m', 'x'],
+                                 can_inline = lambda *args: False)
+        def f2(g, m, x):
             while m > 0:
-                myjitdriver2.can_enter_jit(m=m, x=x)
-                myjitdriver2.jit_merge_point(m=m, x=x)
+                myjitdriver2.can_enter_jit(g=g, m=m, x=x)
+                myjitdriver2.jit_merge_point(g=g, m=m, x=x)
                 m -= 1
                 x += 3
             return x
         #
         def main(i, j):
-            return f(i) - f2(i, j)
+            return f(i) - f2(i+j, i, j)
         res = ll_meta_interp(main, [40, 5], CPUClass=self.CPUClass,
                              type_system=self.type_system)
         assert res == main(40, 5)
