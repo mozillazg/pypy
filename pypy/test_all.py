e283f0a31d8896d6f96d3d9bbf678e395e50af52
Antonio Cuni <anto.cuni@gmail.com>
2010-06-22 15:23:05 +0000

    tweak the hack for the new directory structure

--- pypy/test_all.py
+++ pypy/test_all.py
@@ -4,30 +4,17 @@ def fix_lib_python_path():
     """
     This is a (hopefully temporary) hack.
 
-    Currently buildbot assumes that lib-python is under trunk/ and invokes
-    lib-python tests like this:
+    Currently buildbot assumes runs lib-python tests like this:
     
         python pypy/test_all.py --pypy=pypy/translator/goal/pypy-c \
                                 --resultlog=cpython.log lib-python
 
-    However, now lib-python is under lib/pypy1.2/lib-python.  We cannot just
-    change buildbot, as it would break all the other current branches, so
-    instead we replace lib-python with the correct path here.
-
-    Note: instead of passing lib/pypy1.2/lib-python, but pass lib/pyyp1.2/ as
-    an argument: this way, the tests under lib_pypy are also run.
+    However, with the currenct buildbot configuration the tests under lib_pypy
+    are never run, so we monkey-patch the command line arguments to add it.    
     """
     import sys
-    from pypy.tool.lib_pypy import LIB_ROOT
-    if sys.argv and sys.argv[-1].endswith('lib-python'):
-        libpython = py.path.local(sys.argv[-1])
-        if libpython.check(dir=True):
-            # the argument passed to the command line actually exists, so no
-            # need to patch it
-            return
-        else:
-            # patch it with the correct path
-            sys.argv[-1] = str(LIB_ROOT)
+    if sys.argv and sys.argv[-1] == 'lib-python':
+        sys.argv.append('lib_pypy')
 
     
 if __name__ == '__main__':
