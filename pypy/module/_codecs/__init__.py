c383575af3ce6793f9f3caa1c3666453c538406f
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-06-29 13:05:56 +0000

    Finally remove app_codecs.py

--- pypy/module/_codecs/__init__.py
+++ pypy/module/_codecs/__init__.py
@@ -3,9 +3,43 @@ from pypy.rlib import runicode
 from pypy.module._codecs import interp_codecs
 
 class Module(MixedModule):
-    appleveldefs = {
-         '__doc__' :  'app_codecs.__doc__',
-    }
+    """
+   _codecs -- Provides access to the codec registry and the builtin
+              codecs.
+
+   This module should never be imported directly. The standard library
+   module "codecs" wraps this builtin module for use within Python.
+
+   The codec registry is accessible via:
+
+     register(search_function) -> None
+
+     lookup(encoding) -> (encoder, decoder, stream_reader, stream_writer)
+
+   The builtin Unicode codecs use the following interface:
+
+     <encoding>_encode(Unicode_object[,errors='strict']) -> 
+         (string object, bytes consumed)
+
+     <encoding>_decode(char_buffer_obj[,errors='strict']) -> 
+        (Unicode object, bytes consumed)
+
+   <encoding>_encode() interfaces also accept non-Unicode object as
+   input. The objects are then converted to Unicode using
+   PyUnicode_FromObject() prior to applying the conversion.
+
+   These <encoding>s are available: utf_8, unicode_escape,
+   raw_unicode_escape, unicode_internal, latin_1, ascii (7-bit),
+   mbcs (on win32).
+
+
+Written by Marc-Andre Lemburg (mal@lemburg.com).
+
+Copyright (c) Corporation for National Research Initiatives.
+"""
+
+    appleveldefs = {}
+
     interpleveldefs = {
          'encode':         'interp_codecs.encode',
          'decode':         'interp_codecs.decode',
