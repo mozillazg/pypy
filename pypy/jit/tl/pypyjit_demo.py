5eac621de22b4fe925e6cfa4d42c6dd1fe202fa7
Hakan Ardo <hakan@debian.org>
2010-07-02 15:29:47 +0000

    testcode

--- pypy/jit/tl/pypyjit_demo.py
+++ pypy/jit/tl/pypyjit_demo.py
@@ -1,38 +1,48 @@
-base = object
+## base = object
 
-class Number(base):
-    __slots__ = ('val', )
-    def __init__(self, val=0):
-        self.val = val
+## class Number(base):
+##     __slots__ = ('val', )
+##     def __init__(self, val=0):
+##         self.val = val
 
-    def __add__(self, other):
-        if not isinstance(other, int):
-            other = other.val
-        return Number(val=self.val + other)
+##     def __add__(self, other):
+##         if not isinstance(other, int):
+##             other = other.val
+##         return Number(val=self.val + other)
             
-    def __cmp__(self, other):
-        val = self.val
-        if not isinstance(other, int):
-            other = other.val
-        return cmp(val, other)
+##     def __cmp__(self, other):
+##         val = self.val
+##         if not isinstance(other, int):
+##             other = other.val
+##         return cmp(val, other)
 
-    def __nonzero__(self):
-        return bool(self.val)
+##     def __nonzero__(self):
+##         return bool(self.val)
 
-def g(x, inc=2):
-    return x + inc
+## def g(x, inc=2):
+##     return x + inc
 
-def f(n, x, inc):
-    while x < n:
-        x = g(x, inc=1)
-    return x
+## def f(n, x, inc):
+##     while x < n:
+##         x = g(x, inc=1)
+##     return x
 
-import time
-#t1 = time.time()
-#f(10000000, Number(), 1)
-#t2 = time.time()
-#print t2 - t1
-t1 = time.time()
-f(10000000, 0, 1)
-t2 = time.time()
-print t2 - t1
+## import time
+## #t1 = time.time()
+## #f(10000000, Number(), 1)
+## #t2 = time.time()
+## #print t2 - t1
+## t1 = time.time()
+## f(10000000, 0, 1)
+## t2 = time.time()
+## print t2 - t1
+
+from array import array
+img=array(4)
+def f():
+    sa=0
+    for i in xrange(4):
+        sa+=img[i]
+    return sa
+
+print f()
