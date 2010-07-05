4768d705abfabcb0464b36112e9d8eb12ebd1612
Bartosz Skowron <getxsick@codespeak.net>
2010-07-05 18:56:08 +0000

    put in order classes in the source file

--- pypy/module/jitffi/interp_jitffi.py
+++ pypy/module/jitffi/interp_jitffi.py
@@ -4,6 +4,42 @@ from pypy.interpreter.error import OperationError, wrap_oserror
 from pypy.interpreter.gateway import interp2app
 from pypy.interpreter.typedef import TypeDef
 
+class W_CDLL(Wrappable):
+    def __init__(self, space, name):
+        self.space = space
+        self.rcdll = rjitffi.CDLL(name, load=False)
+        try:
+            self.lib_w = W_LibHandler(self.space, name)
+        except OSError, e:
+            raise OperationError(space.w_OSError, space.wrap(str(e)))
+
+    def get_w(self, space, func, w_args_type, res_type='v'):
+        args_type_w = [ space.str_w(w_x)
+                        for w_x in space.listview(w_args_type) ]
+        try:
+            ret = W_Get(space, self.rcdll.cpu, space.wrap(self.lib_w),
+                        func, args_type_w, res_type)
+        except ValueError, e:
+            raise OperationError(space.w_ValueError, space.wrap(str(e)))
+        return space.wrap(ret)
+
+def W_CDLL___new__(space, w_type, name):
+    try:
+        return space.wrap(W_CDLL(space, name))
+    except OSError, e:
+        raise wrap_oserror(space, e)
+
+W_CDLL.typedef = TypeDef(
+        'CDLL',
+        __new__ = interp2app(W_CDLL___new__,unwrap_spec=[ObjSpace,W_Root,str]),
+        get     = interp2app(W_CDLL.get_w, unwrap_spec=['self',
+                                                        ObjSpace, str, W_Root, str]),
+        __doc__ = """ C Dynamically loaded library
+use CDLL(libname) to create a handle to a C library (the argument is processed
+the same way as dlopen processes it)."""
+)
+
+
 class W_LibHandler(Wrappable):
     def __init__(self, space, name):
         self.space = space
@@ -69,40 +105,4 @@ W_Get.typedef = TypeDef(
         'Get',
         #__new__ = interp2app(W_Get___new__, unwrap_spec=[ObjSpace, W_Root, W_Root, W_Root, str, W_Root, str]),
         call = interp2app(W_Get.call_w, unwrap_spec=['self', ObjSpace, W_Root])
-)
-
-
-class W_CDLL(Wrappable):
-    def __init__(self, space, name):
-        self.space = space
-        self.rcdll = rjitffi.CDLL(name, load=False)
-        try:
-            self.lib_w = W_LibHandler(self.space, name)
-        except OSError, e:
-            raise OperationError(space.w_OSError, space.wrap(str(e)))
-
-    def get_w(self, space, func, w_args_type, res_type='v'):
-        args_type_w = [ space.str_w(w_x)
-                        for w_x in space.listview(w_args_type) ]
-        try:
-            ret = W_Get(space, self.rcdll.cpu, space.wrap(self.lib_w),
-                        func, args_type_w, res_type)
-        except ValueError, e:
-            raise OperationError(space.w_ValueError, space.wrap(str(e)))
-        return space.wrap(ret)
-
-def W_CDLL___new__(space, w_type, name):
-    try:
-        return space.wrap(W_CDLL(space, name))
-    except OSError, e:
-        raise wrap_oserror(space, e)
-
-W_CDLL.typedef = TypeDef(
-        'CDLL',
-        __new__ = interp2app(W_CDLL___new__,unwrap_spec=[ObjSpace,W_Root,str]),
-        get     = interp2app(W_CDLL.get_w, unwrap_spec=['self',
-                                                        ObjSpace, str, W_Root, str]),
-        __doc__ = """ C Dynamically loaded library
-use CDLL(libname) to create a handle to a C library (the argument is processed
-the same way as dlopen processes it)."""
 )
