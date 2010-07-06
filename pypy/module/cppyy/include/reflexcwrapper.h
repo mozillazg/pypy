553cbda085b1925fc971cc175f7b9e68ca5ec87e
Carl Friedrich Bolz <cfbolz@gmx.de>
2010-07-06 10:37:23 +0000

    (antocuni, cfbolz, wlav, arigo around): start going in the direction of supporting overloading. support returning doubles from static methods.

--- pypy/module/cppyy/include/reflexcwrapper.h
+++ pypy/module/cppyy/include/reflexcwrapper.h
@@ -3,10 +3,21 @@
 #define CPPYY_REFLEXCWRAPPER
 
 extern "C" {
-    long callstatic_l(const char* class_name, const char* method_name, int numargs, void* args[]);
-    long callmethod_l(const char* class_name, const char* method_name, void* self, int numargs, void* args[]);
+    long callstatic_l(const char* class_name, int method_index, int numargs, void* args[]);
+    double callstatic_d(const char* class_name, int method_index, int numargs, void* args[]);
+    long callmethod_l(const char* class_name, int method_index, void* self, int numargs, void* args[]);
     void* construct(const char* class_name, int numargs, void* args[]);
     void destruct(const char* class_name, void* self);
+
+    int num_methods(const char* class_name);
+    char* method_name(const char* class_name, int method_index);
+    char* result_type_method(const char* class_name, int method_index);
+    int num_args_method(const char* class_name, int method_index);
+    char* arg_type_method(const char* class_name, int method_index, int index);
+    int is_constructor(const char* class_name, int method_index);
+    int is_static(const char* class_name, int method_index);
+
+    void myfree(void* ptr);
 }
 
 #endif // ifndef CPPYY_REFLEXCWRAPPER
--- pypy/module/cppyy/interp_cppyy.py
+++ pypy/module/cppyy/interp_cppyy.py
@@ -10,6 +10,8 @@ from pypy.rpython.lltypesystem import rffi, lltype
 
 from pypy.rlib.libffi import CDLL
 
+from pypy.module.cppyy import converter
+
 
 srcpath = py.path.local(__file__).dirpath().join("src")
 incpath = py.path.local(__file__).dirpath().join("include")
@@ -26,7 +28,11 @@ eci = ExternalCompilationInfo(
 
 callstatic_l = rffi.llexternal(
     "callstatic_l",
-    [rffi.CCHARP, rffi.CCHARP, rffi.INT, rffi.VOIDPP], rffi.LONG,
+    [rffi.CCHARP, rffi.INT, rffi.INT, rffi.VOIDPP], rffi.LONG,
+    compilation_info=eci)
+callstatic_d = rffi.llexternal(
+    "callstatic_d",
+    [rffi.CCHARP, rffi.INT, rffi.INT, rffi.VOIDPP], rffi.DOUBLE,
     compilation_info=eci)
 construct = rffi.llexternal(
     "construct",
@@ -34,7 +40,7 @@ construct = rffi.llexternal(
     compilation_info=eci)
 callmethod_l = rffi.llexternal(
     "callmethod_l",
-    [rffi.CCHARP, rffi.CCHARP, rffi.VOIDP, rffi.INT, rffi.VOIDPP], rffi.LONG,
+    [rffi.CCHARP, rffi.INT, rffi.VOIDP, rffi.INT, rffi.VOIDPP], rffi.LONG,
     compilation_info=eci)
 destruct = rffi.llexternal(
     "destruct",
@@ -42,19 +48,54 @@ destruct = rffi.llexternal(
     compilation_info=eci)
 
 
+num_methods = rffi.llexternal(
+    "num_methods",
+    [rffi.CCHARP], rffi.INT,
+    compilation_info=eci)
+method_name = rffi.llexternal(
+    "method_name",
+    [rffi.CCHARP, rffi.INT], rffi.CCHARP,
+    compilation_info=eci)
+result_type_method = rffi.llexternal(
+    "result_type_method",
+    [rffi.CCHARP, rffi.INT], rffi.CCHARP,
+    compilation_info=eci)
+num_args_method = rffi.llexternal(
+    "num_args_method",
+    [rffi.CCHARP, rffi.INT], rffi.INT,
+    compilation_info=eci)
+arg_type_method = rffi.llexternal(
+    "arg_type_method",
+    [rffi.CCHARP, rffi.INT, rffi.INT], rffi.CCHARP,
+    compilation_info=eci)
+is_constructor = rffi.llexternal(
+    "is_constructor",
+    [rffi.CCHARP, rffi.INT], rffi.INT,
+    compilation_info=eci)
+is_static = rffi.llexternal(
+    "is_static",
+    [rffi.CCHARP, rffi.INT], rffi.INT,
+    compilation_info=eci)
+myfree = rffi.llexternal(
+    "myfree",
+    [rffi.VOIDP], lltype.Void,
+    compilation_info=eci)
+
 
 def load_lib(space, name):
     cdll = CDLL(name)
     return W_CPPLibrary(space, cdll)
 load_lib.unwrap_spec = [ObjSpace, str]
 
-def prepare_arguments(space, args_w):
+def prepare_arguments(space, args_w, argtypes):
+    if len(args_w) != len(argtypes):
+        raise OperationError(space.w_TypeError, space.wrap("wrong number of args"))
     args = lltype.malloc(rffi.CArray(rffi.VOIDP), len(args_w), flavor='raw')
     for i in range(len(args_w)):
-        arg = space.int_w(args_w[i])
-        x = lltype.malloc(rffi.LONGP.TO, 1, flavor='raw')
-        x[0] = arg
-        args[i] = rffi.cast(rffi.VOIDP, x)
+        argtype = argtypes[i]
+        conv = converter.get_converter(argtype)
+        # XXX this can leak so far
+        args[i] = conv.convert_argument(space, args_w[i])
     return args
 
 def free_arguments(args, numargs):
@@ -75,24 +116,120 @@ W_CPPLibrary.typedef = TypeDef(
     type_byname = interp2app(W_CPPLibrary.type_byname, unwrap_spec=['self', str]),
 )
 
+class CPPMethod(object):
+    """ A concrete function after overloading has been resolved """
+    
+    def __init__(self, cpptype, method_index, result_type, arg_types):
+        self.cpptype = cpptype
+        self.space = cpptype.space
+        self.method_index = method_index
+        self.result_type = result_type
+        self.arg_types = arg_types
+
+    def call(self, cppthis, args_w):
+        args = prepare_arguments(self.space, args_w, self.arg_types)
+        result = callmethod_l(self.cpptype.name, self.method_index,
+                              cppthis, len(args_w), args)
+        free_arguments(args, len(args_w))
+        return result
+
+    def __repr__(self):
+        return "CPPFunction(%s, %s, %s, %s)" % (
+            self.cpptype, self.method_index, self.result_type, self.arg_types)
+
+class CPPFunction(CPPMethod):
+    def call(self, cppthis, args_w):
+        assert cppthis is None
+        args = prepare_arguments(self.space, args_w, self.arg_types)
+        if self.result_type == "int":
+            result = callstatic_l(self.cpptype.name, self.method_index, len(args_w), args)
+            return self.space.wrap(result)
+        if self.result_type == "double":
+            result = callstatic_d(self.cpptype.name, self.method_index, len(args_w), args)
+            return self.space.wrap(result)
+        else:
+            raise NotImplementedError
+        free_arguments(args, len(args_w))
+ 
+
+class CPPConstructor(CPPFunction):
+    def call(self, cppthis, args_w):
+        assert cppthis is None
+        args = prepare_arguments(self.space, args_w, self.arg_types)
+        result = construct(self.cpptype.name, len(args_w), args)
+        free_arguments(args, len(args_w))
+        return result
+
+
+class CPPOverload(object):
+    def __init__(self, space, func_name):
+        self.space = space
+        self.func_name = func_name
+        self.functions = []
+
+    def add_function(self, cppfunc):
+        self.functions.append(cppfunc)
+
+    def call(self, cppthis, args_w):
+        space = self.space
+        for cppyyfunc in self.functions:
+            try:
+                return cppyyfunc.call(cppthis, args_w)
+            except OperationError, e:
+                if not e.match(space, space.w_TypeError):
+                    raise
+        # XXX better error reporting
+        raise OperationError(space.w_TypeError, space.wrap("none of the overloads matched"))
+
+    def __repr__(self):
+        return "CPPOverload(%s, %s)" % (self.func_name, self.functions)
+
+def charp2str(charp):
+    string = rffi.charp2str(charp)
+    myfree(charp)
+    return string
 
 class W_CPPType(Wrappable):
     def __init__(self, cpplib, name):
         self.space = cpplib.space
         self.cpplib = cpplib
         self.name = name
+        self.function_members = {}
+        self._find_func_members()
+    
+    def _find_func_members(self):
+        num_func_members = num_methods(self.name)
+        for i in range(num_func_members):
+            func_member_name = charp2str(method_name(self.name, i))
+            cppfunction = self._make_cppfunction(i)
+            overload = self.function_members.get(func_member_name, None)
+            if overload is None:
+                overload = CPPOverload(self.space, func_member_name)
+                self.function_members[func_member_name] = overload
+            overload.add_function(cppfunction)
+
+    def _make_cppfunction(self, method_index):
+        result_type = charp2str(result_type_method(self.name, method_index))
+        num_args = num_args_method(self.name, method_index)
+        argtypes = []
+        for i in range(num_args):
+            argtype = charp2str(arg_type_method(self.name, method_index, i))
+            argtypes.append(argtype)
+        if is_constructor(self.name, method_index):
+            cls = CPPConstructor
+        elif is_static(self.name, method_index):
+            cls = CPPFunction
+        else:
+            cls = CPPMethod
+        return cls(self, method_index, result_type, argtypes)
 
     def invoke(self, name, args_w):
-        args = prepare_arguments(self.space, args_w)
-        result = callstatic_l(self.name, name, len(args_w), args)
-        free_arguments(args, len(args_w))
-        return self.space.wrap(result)
+        overload = self.function_members[name]
+        return overload.call(None, args_w)
 
     def construct(self, args_w):
-        args = prepare_arguments(self.space, args_w)
-        result = construct(self.name, len(args_w), args)
-        free_arguments(args, len(args_w))
-        return W_CPPObject(self, result)
+        overload = self.function_members[self.name]
+        return W_CPPObject(self, overload.call(None, args_w))
 
 W_CPPType.typedef = TypeDef(
     'CPPType',
@@ -109,10 +246,8 @@ class W_CPPObject(Wrappable):
         self.rawobject = rawobject
 
     def invoke(self, method_name, args_w):
-        args = prepare_arguments(self.space, args_w)
-        result = callmethod_l(self.cppclass.name, method_name,
-                              self.rawobject, len(args_w), args)
-        free_arguments(args, len(args_w))
+        overload = self.cppclass.function_members[method_name]
+        result = overload.call(self.rawobject, args_w)
         return self.space.wrap(result)
 
     def destruct(self):
