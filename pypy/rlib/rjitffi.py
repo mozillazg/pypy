c92af16ffad7cb68c6b20dad5e56397a911f4cce
Bartosz Skowron <getxsick@codespeak.net>
2010-07-06 09:10:58 +0000

    free memory to avoid memory leaks (thanks amaury)

--- pypy/rlib/rjitffi.py
+++ pypy/rlib/rjitffi.py
@@ -21,10 +21,13 @@ class CDLL(object):
 
 class _LibHandler(object):
     def __init__(self, name):
+        name_ptr = rffi.str2charp(name)
         try:
-            self.handler = rdynload.dlopen(rffi.str2charp(name))
+            self.handler = rdynload.dlopen(name_ptr)
         except rdynload.DLOpenError, e:
             raise OSError('%s: %s', name, e.msg or 'unspecified error')
+        finally:
+            rffi.free_charp(name_ptr)
 
 class _Get(object):
     def __init__(self, cpu, lib, func, args_type, res_type='v'):
