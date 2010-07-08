b14843f59866999f7751a2cd0d339aef5259cef4
Antonio Cuni <anto.cuni@gmail.com>
2010-07-08 15:08:22 +0000

    introduce two new types, cppyy_typehandle_t and cppyy_object_t, which are just void* but make the code a bit easier to understand

--- pypy/module/cppyy/include/reflexcwrapper.h
+++ pypy/module/cppyy/include/reflexcwrapper.h
@@ -5,25 +5,28 @@
 #ifdef __cplusplus
 extern "C" {
 #endif // ifdef __cplusplus
-    typedef void* (*cppyy_methptrgetter_t)(void*);
+    typedef void* cppyy_typehandle_t;
+    typedef void* cppyy_object_t;
+    typedef void* (*cppyy_methptrgetter_t)(cppyy_object_t);
 
-    void* cppyy_get_typehandle(const char* class_name);
+    cppyy_typehandle_t cppyy_get_typehandle(const char* class_name);
 
-    void* cppyy_construct(void* handle, int numargs, void* args[]);
-    long cppyy_call_l(void* handle, int method_index, void* self, int numargs, void* args[]);
-    double cppyy_call_d(void* handle, int method_index, void* self, int numargs, void* args[]);
-    void cppyy_destruct(void* handle, void* self);
-    cppyy_methptrgetter_t cppyy_get_methptr_getter(void* handle, int method_index);
+    cppyy_object_t cppyy_construct(cppyy_typehandle_t handle, int numargs, void* args[]);
+    long cppyy_call_l(cppyy_typehandle_t handle, int method_index, cppyy_object_t self, int numargs, void* args[]);
+    double cppyy_call_d(cppyy_typehandle_t handle, int method_index, cppyy_object_t self, int numargs, void* args[]);
+    void cppyy_destruct(cppyy_typehandle_t handle, cppyy_object_t self);
+    cppyy_methptrgetter_t cppyy_get_methptr_getter(cppyy_typehandle_t handle, int method_index);
 
-    int num_methods(void* handle);
-    char* method_name(void* handle, int method_index);
-    char* result_type_method(void* handle, int method_index);
-    int num_args_method(void* handle, int method_index);
-    char* arg_type_method(void* handle, int method_index, int index);
-    int is_constructor(void* handle, int method_index);
-    int is_static(void* handle, int method_index);
+    int num_methods(cppyy_typehandle_t handle);
+    char* method_name(cppyy_typehandle_t handle, int method_index);
+    char* result_type_method(cppyy_typehandle_t handle, int method_index);
+    int num_args_method(cppyy_typehandle_t handle, int method_index);
+    char* arg_type_method(cppyy_typehandle_t handle, int method_index, int index);
+    int is_constructor(cppyy_typehandle_t handle, int method_index);
+    int is_static(cppyy_typehandle_t handle, int method_index);
 
     void myfree(void* ptr);
+
 #ifdef __cplusplus
 }
 #endif // ifdef __cplusplus
