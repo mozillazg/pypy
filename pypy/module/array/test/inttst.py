3166dbb6844998e2f01d39b80ce447004de144bf
Hakan Ardo <hakan@debian.org>
2010-07-08 17:04:08 +0000

    performace tests

--- pypy/module/array/test/inttst.py
+++ pypy/module/array/test/inttst.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python
 from time import time
 
-from array import array
-#img=array('B',(0,)*640*480);
-#intimg=array('I',(0,)*640*480);
-img=array(640*480);
-intimg=array(640*480);
+from array import array #, simple_array
 
-def f():
+def f(img, intimg):
     l=0
-    for i in xrange(640,640*480):
+    i=640    
+    while i<640*480:
         l+=img[i]
         intimg[i]=intimg[i-640]+l
+        i+=1
+    return l
 
+
+
+if True:
+    img=array('d','\x00'*640*480*8)
+    intimg=array('d','\x00'*640*480*8)
+else:
+    img=simple_array(640*480)
+    intimg=simple_array(640*480)
 
 start=time()
-for l in range(500): f()
+for l in range(500): f(img, intimg)
 print time()-start
