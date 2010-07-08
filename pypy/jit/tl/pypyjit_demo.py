169325120b0cf38077112ac9ed8d4bc103c986ab
Armin Rigo <arigo@tunes.org>
2010-07-08 15:35:13 +0000

    Fixes.

--- pypy/jit/tl/pypyjit_demo.py
+++ pypy/jit/tl/pypyjit_demo.py
@@ -3,12 +3,12 @@ import cppyy
 import time
 import cppyy
 lib = cppyy.load_lib("../../module/cppyy/test/example01Dict.so")
-cls = lib.type_byname("example01")
+cls = cppyy._type_byname('example01')
 inst = cls.construct(-17)
 
 t1 = time.time()
 res = 0
-for i in range(1000000):
-    res += inst.invoke("add", i)
+for i in range(10):
+    res += inst.invoke("addDataToInt", i)
 t2 = time.time()
 print t2 - t1
