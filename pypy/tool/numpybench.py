cba0e1cd06e94f59027dc334f2a06ff12591242c
Daniel Roberts <Ademan555@gmail.com>
2010-07-16 22:41:02 +0000

    Oops, I forgot the most important part of the benchmark!

--- pypy/tool/numpybench.py
+++ pypy/tool/numpybench.py
@@ -21,13 +21,29 @@ def generate_kernel(width, height):
     return numpy.array(kernel)
 
 if __name__ == '__main__':
-    from sys import argv as args
-    width, height, kwidth, kheight = [int(x) for x in args[1:]]
+    from optparse import OptionParser
+
+    option_parser = OptionParser()
+    option_parser.add_option('--kernel-size', dest='kernel', default='3x3',
+                             help="The size of the convolution kernel, given as WxH. ie 3x3"
+                                  "Note that both dimensions must be odd.")
+    option_parser.add_option('--image-size', dest='image', default='256x256',
+                             help="The size of the image, given as WxH. ie. 256x256")
+    option_parser.add_option('--runs', '--count', dest='count', default=1000,
+                             help="The number of times to run the convolution filter")
+
+    options, args = option_parser.parse_args()
+    
+    def parse_dimension(arg):
+        return [int(s.strip()) for s in arg.split('x')]
+
+    width, height = parse_dimension(options.image)
+    kwidth, kheight = parse_dimension(options.kernel)
+    count = int(options.count)
 
     image = generate_image(width, height)
     kernel = generate_kernel(kwidth, kheight)
 
     from timeit import Timer
     convolve_timer = Timer('naive_convolve(image, kernel)', 'from convolve import naive_convolve; from __main__ import image, kernel; gc.enable()')
-    count = 100
     print "%.5f sec/pass" % (convolve_timer.timeit(number=count)/count)
