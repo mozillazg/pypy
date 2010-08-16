dce22ccdc52bd29f5d36d3d2dd02b1b233d9170d
Bartosz Skowron <getxsick@codespeak.net>
2010-08-16 16:18:33 +0000

    add some constants

--- pypy/module/_ctypes/__init__.py
+++ pypy/module/_ctypes/__init__.py
@@ -2,8 +2,24 @@ from pypy.interpreter.mixedmodule import MixedModule
 
 class Module(MixedModule):
     interpleveldefs = {
-        '__version__' : 'space.wrap("1.0.3")',
         'dlopen' : 'interp_dll.W_CDLL',
         'Test' : 'interp_test.W_Test',
     }
     appleveldefs = {}
+
+    def buildloaders(cls):
+        from pypy.module._ctypes.constants import constants
+        for constant, value in constants.iteritems():
+            Module.interpleveldefs[constant] = 'space.wrap(%r)' % value
+
+        from pypy.rlib import rjitffi
+        for name in ['FUNCFLAG_STDCALL',
+                     'FUNCFLAG_CDECL',
+                     'FUNCFLAG_PYTHONAPI',
+                    ]:
+            if hasattr(rjitffi, name):
+                Module.interpleveldefs[name] = \
+                                    "space.wrap(%r)" % getattr(rjitffi, name)
+
+        super(Module, cls).buildloaders()
+    buildloaders = classmethod(buildloaders)
