27259a88a6902e98a4aa147a5b7c1f79b5eb7d21
Bartosz Skowron <getxsick@codespeak.net>
2010-08-16 15:45:27 +0000

    set __version__ for module/_ctypes

--- pypy/module/_ctypes/__init__.py
+++ pypy/module/_ctypes/__init__.py
@@ -2,7 +2,8 @@ from pypy.interpreter.mixedmodule import MixedModule
 
 class Module(MixedModule):
     interpleveldefs = {
+        '__version__' : 'space.wrap("1.0.3")',
         'dlopen' : 'interp_dll.W_CDLL',
-        'Test'  : 'interp_test.W_Test',
+        'Test' : 'interp_test.W_Test',
     }
     appleveldefs = {}
