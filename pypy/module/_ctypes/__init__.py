4eda487705ae3eb55a84a6aaec7d302c7ee99468
Bartosz Skowron <getxsick@codespeak.net>
2010-08-16 13:22:18 +0000

    add dlopen function (it's equivalent to CDLL from module/jitffi)

--- pypy/module/_ctypes/__init__.py
+++ pypy/module/_ctypes/__init__.py
@@ -1,5 +1,8 @@
 from pypy.interpreter.mixedmodule import MixedModule
 
 class Module(MixedModule):
-    interpleveldefs = {}
+    interpleveldefs = {
+        'dlopen' : 'interp_dll.W_CDLL',
+        'Test'  : 'interp_test.W_Test',
+    }
     appleveldefs = {}
