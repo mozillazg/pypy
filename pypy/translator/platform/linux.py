7fa7643f7411ac552f5ded8da80be852627411cf
Jason Creighton <jcreigh@codespeak.net>
2010-08-25 17:06:43 +0000

    merge changes from trunk through r76731

--- pypy/translator/platform/linux.py
+++ pypy/translator/platform/linux.py
@@ -6,12 +6,13 @@ from pypy.translator.platform.posix import BasePosix
 class BaseLinux(BasePosix):
     name = "linux"
     
-    link_flags = ['-pthread', '-lrt']
-    cflags = ['-O3', '-pthread', '-fomit-frame-pointer', '-Wall', '-Wno-unused']
-    standalone_only = []
-    shared_only = ['-fPIC']
+    link_flags = ('-pthread', '-lrt')
+    cflags = ('-O3', '-pthread', '-fomit-frame-pointer',
+              '-Wall', '-Wno-unused')
+    standalone_only = ()
+    shared_only = ('-fPIC',)
     so_ext = 'so'
-    so_prefixes = ['lib', '']
+    so_prefixes = ('lib', '')
     
     def _args_for_shared(self, args):
         return ['-shared'] + args
