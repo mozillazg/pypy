a90cabc99fafa974074dfa99712f99c596412b52
Armin Rigo <arigo@tunes.org>
2010-08-29 10:24:52 +0000

    Merge branch/markcompact (up to r76779) in that branch.

--- pypy/translator/c/test/test_newgc.py
+++ pypy/translator/c/test/test_newgc.py
@@ -67,9 +67,8 @@ class TestUsingFramework(object):
             if not fullname.startswith('define'):
                 continue
             keyword = conftest.option.keyword
-            if keyword:
-                if keyword.startswith('test_'):
-                    keyword = keyword[len('test_'):]
+            if keyword.startswith('test_'):
+                keyword = keyword[len('test_'):]
                 if keyword not in fullname:
                     continue
             prefix, name = fullname.split('_', 1)
@@ -1072,10 +1071,10 @@ class TestMarkCompactGC(TestSemiSpaceGC):
     should_be_moving = True
     GC_CAN_SHRINK_ARRAY = False
 
-    def setup_class(cls):
-        py.test.skip("Disabled for now")
-
     def test_gc_set_max_heap_size(self):
+        py.test.skip("not implemented")
+
+    def test_gc_heap_stats(self):
         py.test.skip("not implemented")
 
     def test_finalizer_order(self):
@@ -1083,9 +1082,8 @@ class TestMarkCompactGC(TestSemiSpaceGC):
 
 # ____________________________________________________________________
 
-class TestHybridTaggedPointers(TestHybridGC):
+class TaggedPointersTest(object):
     taggedpointers = True
-
 
     def define_tagged(cls):
         class Unrelated(object):
@@ -1129,3 +1127,10 @@ class UnboxedObject(TaggedBase, UnboxedValue):
     __slots__ = 'smallint'
     def meth(self, x):
         return self.smallint + x + 3
+
+
+class TestHybridTaggedPointers(TaggedPointersTest, TestHybridGC):
+    pass
+
+class TestMarkCompactGCMostCompact(TaggedPointersTest, TestMarkCompactGC):
+    removetypeptr = True
