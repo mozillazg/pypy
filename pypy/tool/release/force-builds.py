ebf6f826a51ee12610b677e626fe035583b73349
Maciej Fijalkowski <fijall@gmail.com>
2010-09-09 09:38:02 +0000

    update builder name here

--- pypy/tool/release/force-builds.py
+++ pypy/tool/release/force-builds.py
@@ -23,7 +23,7 @@ BUILDERS = [
     'pypy-c-app-level-linux-x86-32',
     'pypy-c-app-level-linux-64',
     'pypy-c-stackless-app-level-linux-x86-32',
-    'pypy-c-app-level-win-32',
+    'pypy-c-app-level-win-x86-32',
     'pypy-c-jit-linux-x86-32',
 #    'pypy-c-jit-macosx-x86-32',
     'pypy-c-jit-win-x86-32',
