3b84118fec9c3ba9c04bb41c92cb4023b687edf5
Maciej Fijalkowski <fijall@gmail.com>
2010-09-22 19:46:31 +0000

    adapt test (still no assert)

--- pypy/jit/metainterp/test/test_direct_call.py
+++ pypy/jit/metainterp/test/test_direct_call.py
@@ -2,7 +2,8 @@
 import py
 from pypy.rlib.jit import JitDriver
 from pypy.jit.metainterp.test.test_basic import LLJitMixin
-from pypy.rlib.libffi import FuncPtr, CDLL, ffi_type_sint
+from pypy.rlib.clibffi import FuncPtr, CDLL, ffi_type_sint
+from pypy.rlib.libffi import IntArg, Func
 from pypy.tool.udir import udir
 from pypy.translator.tool.cbuild import ExternalCompilationInfo
 from pypy.translator.platform import platform
@@ -24,17 +25,19 @@ class TestDirectCall(LLJitMixin):
                                             standalone=False))
 
     def test_one(self):
-        driver = JitDriver(reds = ['n', 'fn'], greens = [])        
+        driver = JitDriver(reds = ['n', 'func'], greens = [])        
 
         def f(n):
             cdll = CDLL(self.lib_name)
             fn = cdll.getpointer('sum_xy', [ffi_type_sint, ffi_type_sint],
                                  ffi_type_sint)
+            func = Func(fn)
             while n < 10:
-                driver.jit_merge_point(n=n, fn=fn)
-                driver.can_enter_jit(n=n, fn=fn)
-                fn.push_arg(n)
-                fn.push_arg(1)
-                n = fn.call(lltype.Signed)
+                driver.jit_merge_point(n=n, func=func)
+                driver.can_enter_jit(n=n, func=func)
+                arg0 = IntArg(n)
+                arg1 = IntArg(1)
+                arg0.next = arg1
+                n = func.call(arg0, lltype.Signed)
             
         self.meta_interp(f, [0])
