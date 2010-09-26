22001f94fcaf48480094213c73750cf53e610a4c
Armin Rigo <arigo@tunes.org>
2010-09-26 15:14:03 +0000

    Kill these, which are mostly duplicates of each other. Helps the JIT.

--- pypy/rpython/lltypesystem/rstr.py
+++ pypy/rpython/lltypesystem/rstr.py
@@ -696,24 +696,16 @@ class LLHelpers(AbstractLLHelpers):
 
     @purefunction
     def _ll_stringslice(s1, start, stop):
-        newstr = s1.malloc(stop - start)
-        assert start >= 0
         lgt = stop - start
+        assert start >= 0
         assert lgt >= 0
+        newstr = s1.malloc(lgt)
         s1.copy_contents(s1, newstr, start, 0, lgt)
         return newstr
     _ll_stringslice.oopspec = 'stroruni.slice(s1, start, stop)'
 
     def ll_stringslice_startonly(s1, start):
-        len1 = len(s1.chars)
-        if we_are_jitted():
-            return LLHelpers._ll_stringslice(s1, start, len1)
-        newstr = s1.malloc(len1 - start)
-        lgt = len1 - start
-        assert lgt >= 0
-        assert start >= 0
-        s1.copy_contents(s1, newstr, start, 0, lgt)
-        return newstr
+        return LLHelpers._ll_stringslice(s1, start, len(s1.chars))
 
     def ll_stringslice_startstop(s1, start, stop):
         if we_are_jitted():
@@ -728,12 +720,7 @@ class LLHelpers(AbstractLLHelpers):
 
     def ll_stringslice_minusone(s1):
         newlen = len(s1.chars) - 1
-        if we_are_jitted():
-            return LLHelpers._ll_stringslice(s1, 0, newlen)
-        newstr = s1.malloc(newlen)
-        assert newlen >= 0
-        s1.copy_contents(s1, newstr, 0, 0, newlen)
-        return newstr
+        return LLHelpers._ll_stringslice(s1, 0, newlen)
 
     def ll_split_chr(LIST, s, c):
         chars = s.chars
