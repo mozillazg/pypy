df0c1fc9d87198b7680489c43e425f663861a4c1
Hakan Ardo <hakan@debian.org>
2010-09-29 17:36:45 +0000

    Made mess of trying to pass data from a loop to its bridges using VInvariantOpInfo :(

--- pypy/jit/metainterp/optimizeopt/optimizer.py
+++ pypy/jit/metainterp/optimizeopt/optimizer.py
@@ -354,6 +354,7 @@ class Optimizer(Optimization):
         assert isinstance(descr, compile.ResumeGuardDescr)
         modifier = resume.ResumeDataVirtualAdder(descr, self.resumedata_memo)
         newboxes = modifier.finish(self.values, self.pendingfields)
+        debug_print('\ndescr:', descr)
         if len(newboxes) > self.metainterp_sd.options.failargs_limit: # XXX be careful here
             compile.giveup()
         descr.store_final_boxes(op, newboxes)
