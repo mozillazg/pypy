092f446d2224201f792aa83bac74a8d8cabd8add
Antonio Cuni <anto.cuni@gmail.com>
2010-10-04 13:24:09 +0000

    cast the result to the correct type when calling a function

--- pypy/jit/metainterp/test/test_fficall.py
+++ pypy/jit/metainterp/test/test_fficall.py
@@ -2,7 +2,7 @@
 import py
 from pypy.rlib.jit import JitDriver, hint
 from pypy.jit.metainterp.test.test_basic import LLJitMixin
-from pypy.rlib.libffi import CDLL, ffi_type_sint, ffi_type_double, ArgChain, Func
+from pypy.rlib.libffi import CDLL, ffi_type_sint, ffi_type_double, ffi_type_uchar, ArgChain, Func
 from pypy.tool.udir import udir
 from pypy.translator.tool.cbuild import ExternalCompilationInfo
 from pypy.translator.platform import platform
@@ -12,7 +12,7 @@ class TestFfiCall(LLJitMixin):
     def setup_class(cls):
         # prepare C code as an example, so we can load it and call
         # it via rlib.libffi
-        c_file = udir.ensure("test_jit_direct_call", dir=1).join("xlib.c")
+        c_file = udir.ensure("test_jit_fficall", dir=1).join("xlib.c")
         c_file.write(py.code.Source('''
         int sum_xy(int x, double y)
         {
@@ -25,8 +25,13 @@ class TestFfiCall(LLJitMixin):
                 return -x;
             return x;
         }
+
+        unsigned char cast_to_uchar(int x)
+        {
+            return 200+(unsigned char)x;
+        }
         '''))
-        eci = ExternalCompilationInfo(export_symbols=['sum_xy'])
+        eci = ExternalCompilationInfo(export_symbols=[])
         cls.lib_name = str(platform.compile([c_file], eci, 'x',
                                             standalone=False))
 
@@ -77,3 +82,23 @@ class TestFfiCall(LLJitMixin):
         assert res == 9
         self.check_loops(call=1)
 
+    def test_cast_result(self):
+        driver = JitDriver(reds = ['n', 'res', 'func'], greens = [])        
+
+        def f(n):
+            cdll = CDLL(self.lib_name)
+            func = cdll.getpointer('cast_to_uchar', [ffi_type_sint],
+                                   ffi_type_uchar)
+            res = 0
+            while n < 10:
+                driver.jit_merge_point(n=n, func=func, res=res)
+                driver.can_enter_jit(n=n, func=func, res=res)
+                func = hint(func, promote=True)
+                argchain = ArgChain()
+                argchain.int(0)
+                res = func.call(argchain, rffi.UCHAR)
+                n += 1
+            return res
+            
+        res = self.meta_interp(f, [0])
+        assert res == 200
