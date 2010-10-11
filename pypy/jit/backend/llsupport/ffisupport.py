40cb6f9c82d5cf264e65add56b7319bcd16e6496
Antonio Cuni <anto.cuni@gmail.com>
2010-10-11 09:38:17 +0000

    move the logic from the jit to libffi, which is a more proper place

--- pypy/jit/backend/llsupport/ffisupport.py
+++ pypy/jit/backend/llsupport/ffisupport.py
@@ -21,43 +21,13 @@ def get_call_descr_dynamic(ffi_args, ffi_result, extrainfo=None):
         return VoidCallDescr(arg_classes, extrainfo)
     assert False
 
-
-# XXX: maybe we can turn this into a dictionary, but we need to do it at
-# runtime as libffi.types.* pointers
 def get_ffi_type_kind(ffi_type):
     from pypy.rlib.libffi import types
-    if ffi_type is types.void:
-        return history.VOID
-    elif ffi_type is types.pointer:
+    kind = types.getkind(ffi_type)
+    if kind == 'i':
         return history.INT
-    elif ffi_type is types.double:
+    elif kind == 'f':
         return history.FLOAT
-    elif ffi_type is types.uchar:
-        return history.INT
-    elif ffi_type is types.uint8:
-        return history.INT
-    elif ffi_type is types.schar:
-        return history.INT
-    elif ffi_type is types.sint8:
-        return history.INT
-    elif ffi_type is types.uint16:
-        return history.INT
-    elif ffi_type is types.ushort:
-        return history.INT
-    elif ffi_type is types.sint16:
-        return history.INT
-    elif ffi_type is types.sshort:
-        return history.INT
-    elif ffi_type is types.uint:
-        return history.INT
-    elif ffi_type is types.uint32:
-        return history.INT
-    elif ffi_type is types.sint:
-        return history.INT
-    elif ffi_type is types.sint32:
-        return history.INT
-    ## elif ffi_type is types.uint64:
-    ##     return history.INT
-    ## elif ffi_type is types.sint64:
-    ##     return history.INT
-    raise KeyError
+    elif kind == 'v':
+        return history.VOID
+    assert False, "Unsuported kind '%s'" % kind
