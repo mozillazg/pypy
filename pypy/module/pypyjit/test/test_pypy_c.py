d906d0349ee6bb9e7842a2f73249888b0a097b76
Armin Rigo <arigo@tunes.org>
2010-10-20 15:45:13 +0000

    (cfbolz, arigo) Now withmapdict is the default with the JIT.

--- pypy/module/pypyjit/test/test_pypy_c.py
+++ pypy/module/pypyjit/test/test_pypy_c.py
@@ -278,7 +278,7 @@ class PyPyCJITTests(object):
         assert len(ops) == 2
         assert not ops[0].get_opnames("call")
         assert not ops[0].get_opnames("new")
-        assert len(ops[0].get_opnames("guard")) <= 3     # we get 2 withmapdict
+        assert len(ops[0].get_opnames("guard")) <= 2
         assert not ops[1] # second LOOKUP_METHOD folded away
 
         ops = self.get_by_bytecode("CALL_METHOD")
@@ -323,8 +323,8 @@ class PyPyCJITTests(object):
         assert len(ops) == 2
         assert not ops[0].get_opnames("call")
         assert not ops[0].get_opnames("new")
-        assert len(ops[0].get_opnames("guard")) <= 3    # we get 2 withmapdict
-        assert len(ops[0].get_opnames("getfield")) <= 5 # we get <5 withmapdict
+        assert len(ops[0].get_opnames("guard")) <= 2
+        assert len(ops[0].get_opnames("getfield")) <= 4
         assert not ops[1] # second LOOKUP_METHOD folded away
 
     def test_default_and_kw(self):
