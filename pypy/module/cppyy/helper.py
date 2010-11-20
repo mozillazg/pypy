c989386542f78946617d07de54a95e15ceb63700
Wim Lavrijsen <wlav@codespeak.net>
2010-11-20 02:25:43 +0000

    fixes for rtyper: make sure iterator stop is non-neg

--- pypy/module/cppyy/helper.py
+++ pypy/module/cppyy/helper.py
@@ -2,24 +2,25 @@ from pypy.rlib import rstring
 
 def compound(name):
     name = "".join(rstring.split(name, "const")) # poor man's replace
-    if name[-1] == "]":                          # array type
+    if name.endswith("]"):                       # array type?
         return "[]"
     i = _find_qualifier_index(name)
     return "".join(name[i:].split(" "))
 
 def array_size(name):
     name = "".join(rstring.split(name, "const")) # poor man's replace
-    if name[-1] == "]":                          # array type
-        i = _find_qualifier_index(name)
-        for i in range(len(name) - 1, -1, -1):
-            c = name[i]
-            if c == "[":
-                return int(name[i+1:-1])
+    if name.endswith("]"):                       # array type?
+        idx = name.rfind("[")
+        if 0 < idx:
+            end = len(name)-1                    # len rather than -1 for rpython
+            if 0 < end and (idx+1) < end:        # guarantee non-neg for rpython
+                return int(name[idx+1:end])
     return -1
 
 def _find_qualifier_index(name):
     i = len(name)
-    for i in range(len(name) - 1, -1, -1):
+    # search from the back; note len(name) > 0 (so rtyper can use uint)
+    for i in range(len(name) - 1, 0, -1):
         c = name[i]
         if c.isalnum() or c == ">" or c == "]":
             break
@@ -29,9 +30,8 @@ def clean_type(name):
     assert name.find("const") == -1
     i = _find_qualifier_index(name)
     name = name[:i].strip(' ')
-    if name[-1] == "]":
-        for i in range(len(name) - 1, -1, -1):
-            c = name[i]
-            if c == "[":
-                return name[:i]
+    if name.endswith("]"):                       # array type?
+        idx = name.rfind("[")
+        if 0 < idx:
+            return name[:idx]
     return name
