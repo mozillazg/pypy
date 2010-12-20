f3a614594647bbf91b8a10211c9eb15016700a90
Armin Rigo <arigo@tunes.org>
2010-12-20 10:05:16 +0100

    Fix this file, which is not fully tested and not really used any more. (Fix it anyway to prevent source code rotting.)

--- pypy/objspace/std/smallintobject.py
+++ pypy/objspace/std/smallintobject.py
@@ -5,7 +5,6 @@ translated PyPy.  To enable them, see inttype.py.
 from pypy.objspace.std import intobject
 from pypy.objspace.std.model import registerimplementation, W_Object
 from pypy.objspace.std.register_all import register_all
-from pypy.objspace.std.noneobject import W_NoneObject
 from pypy.objspace.std.intobject import W_IntObject
 from pypy.rlib.objectmodel import UnboxedValue
 from pypy.tool.sourcetools import func_with_new_name
@@ -40,11 +39,22 @@ def copy_multimethods(ns):
         if "__Int" in name:
             new_name = name.replace("Int", "SmallInt")
             # Copy the function, so the annotator specializes it for
-            # W_SmallIntObject.
-            ns[new_name] = func_with_new_name(func, new_name)
+            # W_SmallIntObject.  Also replaces the func_globals with ns.
+            # The effect we get is as if the source code of the function
+            # was copied in smallintobject.py.
+            ns[new_name] = func_with_new_name(func, new_name, globals=ns)
     ns["get_integer"] = ns["pos__SmallInt"] = ns["int__SmallInt"]
     ns["get_negint"] = ns["neg__SmallInt"]
 
 copy_multimethods(globals())
+
+# extra imports needed because we switch the func_globals above
+from pypy.interpreter.error import OperationError
+from pypy.objspace.std.multimethod import FailedToImplementArgs
+from pypy.rlib.rarithmetic import ovfcheck, ovfcheck_lshift, LONG_BIT, r_uint
+from pypy.objspace.std.inttype import wrapint
+from pypy.objspace.std.intobject import _impl_int_int_pow
+from pypy.objspace.std.noneobject import W_NoneObject
+from pypy.rlib.rbigint import rbigint
 
 register_all(vars())
