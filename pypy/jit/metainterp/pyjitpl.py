54e97fa33c540922f2431130591152eeef60732f
Maciej Fijalkowski <fijall@gmail.com>
2010-12-23 10:55:03 +0200

    another oops

--- pypy/jit/metainterp/pyjitpl.py
+++ pypy/jit/metainterp/pyjitpl.py
@@ -1185,7 +1185,7 @@ class MIFrame(object):
             if resbox is not None:
                 self.make_result_of_lastop(resbox)
             self.metainterp.vable_after_residual_call()
-            if effectinfo.extraeffect >= effectinfo.EF_FORCES_JIT_INVARIANT:
+            if effectinfo is None or effectinfo.extraeffect >= effectinfo.EF_FORCES_JIT_INVARIANT:
                 self.generate_guard(rop.GUARD_NOT_INVARIANT, None)
             self.generate_guard(rop.GUARD_NOT_FORCED, None)
             self.metainterp.handle_possible_exception()
