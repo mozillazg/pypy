fc0355dfb517498a2f6bf79fb48f37f24e13123d
Justin Peel <notmuchtotell@gmail.com>
2011-08-02 19:43:26 -0600

    add bool, int64, and uint64 types

--- pypy/module/micronumpy/test/test_dtypes.py
+++ pypy/module/micronumpy/test/test_dtypes.py
@@ -8,15 +8,24 @@ class AppTestDtype(BaseNumpyAppTest):
         d = dtype('l')
         assert d.num == 7
         assert d.kind == 'i'
+    
+    def test_bool_array(self):
+        from numpy import array
+        a = array([0, 1, 2, 2.5], '?')
+        assert a[0] == False
+        for i in xrange(1, 4):
+            assert a[i] == True
 
     def test_overflow(self):
         from numpy import array
         # only one 32-bit system for now.. will change to 'i' when we can
+        assert array([3], '?')[0] == True
         assert array([128], 'b')[0] == -128
         assert array([256], 'B')[0] == 0
         assert array([32768], 'h')[0] == -32768
         assert array([65536], 'H')[0] == 0
         raises(OverflowError, "array([2147483648], 'l')")
+        raises(OverflowError, "array([9223372036854775808], 'q')")
 
     def test_int_array(self):
         from numpy import array
