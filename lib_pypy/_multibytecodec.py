4e31c83adac289528e372c195bf27b444d195b41
Armin Rigo <arigo@tunes.org>
2011-12-04 11:32:03 +0100

    Fixes.

--- lib_pypy/_multibytecodec.py
+++ lib_pypy/_multibytecodec.py
@@ -1,3 +1,4 @@
 # indirection needed; otherwise the built-in module "_multibytecodec" shadows
 # any file _multibytecodec.py that would be found in the user dirs
 from __builtin__multibytecodec import *
+from __builtin__multibytecodec import __getcodec
