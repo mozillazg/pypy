463ea07ce74a217660072e7c998a351f1a938f9c
Michal Bendowski <michal@bendowski.pl>
2012-05-10 19:57:07 +0200

    Simple jvm "util" module works and compiles.

--- pypy/module/jvm/test/test_jvm.py
+++ pypy/module/jvm/test/test_jvm.py
@@ -5,21 +5,14 @@ class AppTestJvm:
         space = gettestobjspace(usemodules=('jvm',))
         cls.space = space
 
-    def test_make_instance(self):
+    def test_new(self):
         import jvm
-        obj = jvm.make_instance('java.lang.Object')
+
+        methods = jvm.new('java.lang.Object')
+        assert isinstance(methods, list)
 
         expected_methods = {'getClass', 'notifyAll', 'equals', 'hashCode',
                             'toString', 'notify', 'wait'}
 
-        for name in expected_methods:
-            assert hasattr(obj, name)
-            assert name in getattr(obj, name)()
-
-        for name in [m for m in dir(obj) if not m.startswith('_')]:
-            assert name in expected_methods
-
-        assert obj._class_name == 'Object'
-        assert obj._full_name == 'java.lang.Object'
-
+        assert set(methods) == expected_methods
 
