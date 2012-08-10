6546127f8cd01f3a3f1b34df0ae16bac307a8cec
mattip <matti.picus@gmail.com>
2012-08-10 13:33:46 +0300

    blindly copy all *.py files from numpy ver fd15162

---
+++ lib_pypy/numpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+short_version = '1.8.0'
+version = '1.8.0'
+full_version = '1.8.0.dev-fd15162'
+git_revision = 'fd15162fbff5dd68c548284947d39bb2a2481183'
+release = False
+
+if not release:
+    version = full_version
