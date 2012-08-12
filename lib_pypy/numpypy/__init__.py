be00e36a0f7509a7a8779f4cc6725fb2e2eef36b
mattip <matti.picus@gmail.com>
2012-08-12 20:16:48 +0300

    add empty function definitions

--- lib_pypy/numpypy/__init__.py
+++ lib_pypy/numpypy/__init__.py
@@ -1,15 +1,14 @@
 #from _numpypy import *
 #from .core import *
 
-import sys, types
+import sys
 #sys.modules.setdefault('numpy', sys.modules['numpypy'])
 
-nt = types.ModuleType('numerictype','fake numerictypes module')
-setattr(nt, 'sctypeDict',{})
 import _numpypy as umath
 import multiarray
+import numerictypes
 sys.modules['numpy.core.multiarray'] = multiarray
 sys.modules['numpy.core.umath'] = umath
 
-sys.modules['numerictypes'] = nt
-sys.modules['numpy.core.numerictypes'] = nt
+sys.modules['numerictypes'] = numerictypes
+sys.modules['numpy.core.numerictypes'] = numerictypes
