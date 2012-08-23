c24ad1dcb8a1126485789af9381cf0fb23e30334
Matti Picus <matti.picus@gmail.com>
2012-08-23 14:24:37 +0300

    stub out linalg (lapack_lite), add missing files

---
+++ lib_pypy/numpypy/_compiled_base.py
@@ -0,0 +1,32 @@
+
+
+def _insert(*args, **kwargs):
+    raise NotImplementedError("not implemented yet")
+
+def add_docstring(*args, **kwargs):
+    raise NotImplementedError("not implemented yet")
+
+def bincount(*args, **kwargs):
+    raise NotImplementedError("not implemented yet")
+
+def digitize(*args, **kwargs):
+    raise NotImplementedError("not implemented yet")
+
+def interp(*args, **kwargs):
+    raise NotImplementedError("not implemented yet")
+
+def unravel_index(*args, **kwargs):
+    raise NotImplementedError("not implemented yet")
+
+def ravel_multi_index(*args, **kwargs):
+    raise NotImplementedError("not implemented yet")
+
+def add_newdoc_ufunc(*args, **kwargs):
+    raise NotImplementedError("not implemented yet")
+
+def packbits(*args, **kwargs):
+    raise NotImplementedError("not implemented yet")
+
+def unpackbits(*args, **kwargs):
+    raise NotImplementedError("not implemented yet")
+
---
+++ lib_pypy/numpypy/scalarmath.py
@@ -0,0 +1,12 @@
+
+def alter_pythonmath(*args, **kwargs):
+    raise NotImplementedError("alter_pythonmath not implemented yet")
+
+def restore_pythonmath(*args, **kwargs):
+    raise NotImplementedError("restore_pythonmath not implemented yet")
+
+def use_pythonmath(*args, **kwargs):
+    raise NotImplementedError("use_pythonmath not implemented yet")
+
+def use_scalarmath(*args, **kwargs):
+    raise NotImplementedError("use_scalarmath not implemented yet")
