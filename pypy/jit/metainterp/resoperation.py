0d3cef699bc395993dfd128d321715e5603dfc2e
Armin Rigo <arigo@tunes.org>
2012-10-15 16:34:33 +0200

    hg backout 458e381ff84d: I think we don't need KEEPALIVE any more

--- pypy/jit/metainterp/resoperation.py
+++ pypy/jit/metainterp/resoperation.py
@@ -508,7 +508,6 @@ _oplist = [
     'COPYUNICODECONTENT/5',
     'QUASIIMMUT_FIELD/1d',    # [objptr], descr=SlowMutateDescr
     'RECORD_KNOWN_CLASS/2',   # [objptr, clsptr]
-    'KEEPALIVE/1',
 
     '_CANRAISE_FIRST', # ----- start of can_raise operations -----
     '_CALL_FIRST',
