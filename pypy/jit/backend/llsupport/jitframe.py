8068698731f2c777fd1ee69cf873b4bf93370f8c
Armin Rigo <arigo@tunes.org>
2012-10-16 16:56:45 +0200

    Random changes, mostly documentation fixes

--- pypy/jit/backend/llsupport/jitframe.py
+++ pypy/jit/backend/llsupport/jitframe.py
@@ -2,9 +2,17 @@
 
 _LONGLONGARRAY = lltype.GcArray(lltype.SignedLongLong)
 
-JITFRAME = lltype.GcStruct('JITFRAME',
-               ('jf_descr', llmemory.GCREF),
-               ('jf_excvalue', llmemory.GCREF),
-               ('jf_nongcvalues', lltype.Ptr(_LONGLONGARRAY)),
-               ('jf_gcvalues', lltype.Array(llmemory.GCREF)))
+JITFRAME = lltype.GcStruct(
+    'JITFRAME',
+
+    # Once the execute_token() returns, the field 'jf_descr' stores the
+    # descr of the last executed operation (either a GUARD, or FINISH).
+    ('jf_descr', llmemory.GCREF),
+
+    # For the front-end: a GCREF for the savedata
+    ('jf_savedata', llmemory.GCREF),
+
+    # XXX
+    ('jf_nongcvalues', lltype.Ptr(_LONGLONGARRAY)),
+    ('jf_gcvalues', lltype.Array(llmemory.GCREF)))
 JITFRAMEPTR = lltype.Ptr(JITFRAME)
