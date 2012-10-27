fb25a2f32bdf7cb4893f6fc08a31f64707ce52de
Armin Rigo <arigo@tunes.org>
2012-10-27 14:34:47 +0200

    Another branch off result-in-resops: remove 'failargs' from the ResOperations.

--- pypy/jit/backend/llsupport/jitframe.py
+++ pypy/jit/backend/llsupport/jitframe.py
@@ -1,18 +1,17 @@
+from pypy.rpython.lltypesystem import lltype, llmemory
 
-
-_LONGLONGARRAY = lltype.GcArray(lltype.SignedLongLong)
 
 JITFRAME = lltype.GcStruct(
     'JITFRAME',
 
     # Once the execute_token() returns, the field 'jf_descr' stores the
     # descr of the last executed operation (either a GUARD, or FINISH).
+    # This field is also set immediately before doing CALL_MAY_FORCE.
     ('jf_descr', llmemory.GCREF),
 
     # For the front-end: a GCREF for the savedata
     ('jf_savedata', llmemory.GCREF),
 
     # XXX
-    ('jf_nongcvalues', lltype.Ptr(_LONGLONGARRAY)),
-    ('jf_gcvalues', lltype.Array(llmemory.GCREF)))
+    ('jf_values', lltype.Array(llmemory.Address)))
 JITFRAMEPTR = lltype.Ptr(JITFRAME)
