15c26e5455914ac1a8a2934b21cea747f42f3794
Maciej Fijalkowski <fijall@gmail.com>
2012-10-28 18:25:29 +0100

    fix those tests, they're still fine

--- pypy/jit/metainterp/optimizeopt/test/test_optimizebasic.py
+++ pypy/jit/metainterp/optimizeopt/test/test_optimizebasic.py
@@ -615,9 +615,9 @@ class BaseTestOptimizeBasic(BaseTestBasic):
         escape(i3)
         p1 = new_with_vtable(ConstClass(node_vtable))
         p1sub = new_with_vtable(ConstClass(node_vtable2))
-        setfield_gc(p1, i1, descr=valuedescr)
         setfield_gc(p1sub, i1, descr=valuedescr)
         setfield_gc(p1, p1sub, descr=nextdescr)
+        setfield_gc(p1, i1, descr=valuedescr)
         jump(i1, p1, p2)
         """
         # The same as test_p123_simple, but with a virtual containing another
@@ -630,10 +630,10 @@ class BaseTestOptimizeBasic(BaseTestBasic):
         p3sub = getfield_gc_r(p3, descr=nextdescr)
         i3 = getfield_gc_i(p3sub, descr=valuedescr)
         escape(i3)
-        p1 = new_with_vtable(ConstClass(node_vtable))
         p2sub = new_with_vtable(ConstClass(node_vtable2))
         setfield_gc(p2sub, i1, descr=valuedescr)
         setfield_gc(p2, p2sub, descr=nextdescr)
+        p1 = new_with_vtable(ConstClass(node_vtable))
         jump(i1, p1, p2)
         """
         # The same as test_p123_simple, but in the end the "old" p2 contains
