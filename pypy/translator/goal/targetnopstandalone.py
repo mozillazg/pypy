8dde37f3a92d27b0249cf7730d19fc0a0831debf
Maciej Fijalkowski <fijall@gmail.com>
2013-01-03 10:50:40 +0200

    see how good is rhettingers dict in rpython

--- pypy/translator/goal/targetnopstandalone.py
+++ pypy/translator/goal/targetnopstandalone.py
@@ -7,13 +7,24 @@ of input types is empty, it is meant to be a list of strings,
 actually implementing argv of the executable.
 """
 
-def debug(msg):
-    print "debug:", msg
-
 # __________  Entry point  __________
 
+class A(object):
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
 def entry_point(argv):
-    debug("hello world")
+    if len(argv) > 2:
+        d = {}
+        for i in range(int(argv[1])):
+            d[i] = A(i, i, i)
+    else:
+        from pypy.rlib import dict
+        d = dict.Dict()
+        for i in range(int(argv[1])):
+            d.__setitem__(i, A(i, i, i))
     return 0
 
 # _____ Define and setup target ___
