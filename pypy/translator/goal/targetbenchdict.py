78e5caacb7a3db2954a9e323a965ae88a59657d7
Maciej Fijalkowski <fijall@gmail.com>
2013-01-07 02:55:31 +0200

    add repeats

--- pypy/translator/goal/targetbenchdict.py
+++ pypy/translator/goal/targetbenchdict.py
@@ -19,27 +19,29 @@ class A(object):
 def entry_point(argv):
     bench_no = int(argv[1])
     bench_counter = int(argv[2])
-    if bench_no == 1:
-        d = {}
-        for i in range(bench_counter):
-            d[str(i)] = None
-    if bench_no == 2: 
-        d = {}
-        for i in range(bench_counter):
-            d[i] = A(1, 2, 3)
-    if bench_no == 3:
-        d = {}
-        a = A(1, 2, 3)
-        for i in range(bench_counter):
-            if i % 100 == 0:
-                a = A(1, 2, 3)
-            d[i] = a
-    if bench_no == 4:
-        d = {}
-        s = 0
-        for i in range(bench_counter):
-            d[i % 100] = i
-            s += d[i % 100]
+    repeats = int(argv[3])
+    for k in range(repeats):
+        if bench_no == 1:
+            d = {}
+            for i in range(bench_counter):
+                d[str(i)] = None
+        if bench_no == 2: 
+            d = {}
+            for i in range(bench_counter):
+                d[i] = A(1, 2, 3)
+        if bench_no == 3:
+            d = {}
+            a = A(1, 2, 3)
+            for i in range(bench_counter):
+                if i % 100 == 0:
+                    a = A(1, 2, 3)
+                d[i] = a
+        if bench_no == 4:
+            d = {}
+            s = 0
+            for i in range(bench_counter):
+                d[i % 100] = i
+                s += d[i % 100]
     return 0
 
 # _____ Define and setup target ___
