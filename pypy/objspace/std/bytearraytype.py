e41b756e022ebc592f7d795b19c9debfcddfb1a4
Vladimir Kryachko <v.kryachko@gmail.com>
2013-03-19 13:49:52 -0700

    Cleanup

--- pypy/objspace/std/bytearraytype.py
+++ pypy/objspace/std/bytearraytype.py
@@ -166,25 +166,6 @@ def descr_fromhex(space, w_type, w_hexstring):
 
 # ____________________________________________________________
 
-
-class ByteArrayMethods(object):
-    _mixin_ = True
-
-    def ljust(self, space, arg, fillchar=' '):
-        u_self = self._value
-        if len(fillchar) != 1:
-            raise OperationError(space.w_TypeError,
-                space.wrap("ljust() argument 2 must be a single character"))
-
-        d = arg - len(u_self)
-        if d > 0:
-            fillchar = fillchar[0]    # annotator hint: it's a single character
-            u_self += d * fillchar
-
-        return space.wrap(u_self)
-
-# ____________________________________________________________
-
 bytearray_typedef = StdTypeDef("bytearray",
     __doc__ = '''bytearray() -> an empty bytearray
 bytearray(sequence) -> bytearray initialized from sequence\'s items
