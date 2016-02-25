f781026c6218233724386269206fe5c37c64f480
Remi Meier <remi.meier@gmail.com>
2016-02-25 14:34:14 +0100

    (plan_rich,remi) faster update_free_registers

--- rpython/jit/backend/llsupport/regalloc.py
+++ rpython/jit/backend/llsupport/regalloc.py
@@ -317,7 +317,6 @@ class RegisterManager(object):
             return self.get_free_register(var, callee=False, target_reg=None)
 
     def update_free_registers(self, regs_in_use):
-        # XXX: slow?
         self._reinit_free_regs()
         for r in regs_in_use:
             self.remove_free_register(r)
@@ -342,17 +341,18 @@ class RegisterManager(object):
                reg in self.free_caller_regs
 
     def _reinit_free_regs(self):
-        self.free_callee_regs = [reg for reg in self.all_regs
-                                 if reg not in self.save_around_call_regs]
+        self.free_callee_regs = self.save_in_callee_regs[:]
         self.free_caller_regs = self.save_around_call_regs[:]
 
     def _change_regs(self, all_regs, save_around_call_regs):
         self.all_regs = all_regs
         self.save_around_call_regs = save_around_call_regs
+        self.save_in_callee_regs = [reg for reg in all_regs
+                                    if reg not in save_around_call_regs]
         self._reinit_free_regs()
         self.is_callee_lookup = [True] * max(
             [r.value + 1 for r in self.all_regs])
-        for reg in save_around_call_regs:
+        for reg in self.save_around_call_regs:
             self.is_callee_lookup[reg.value] = False
 
     def __init__(self, live_ranges, frame_manager=None, assembler=None):
